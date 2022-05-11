16fd0e0ecd1ae34fae3b26b9624d80f7d5469f78
bun913 <imanau03@gmail.com>
2022-05-11 18:35:16 +0900

    feat: ABC096のC問題を解答

--- abc096/c/main.py
+++ abc096/c/main.py
@@ -19,3 +19,31 @@ l = list(map(int, input().split()))
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
 
+H, W = list(map(int, input().split()))
+grids = []
+for _a in range(H):
+    l = input()
+    grids.append(l)
+# 上下左右4方向を確認するための配列
+dxy = [
+    (-1, 0), (1, 0), (0, -1), (0,1)
+]
+ans = "Yes"
+for y in range(H):
+    for x in range(W):
+        now_col = grids[y][x]
+        # 白に塗るべきマスは飛ばす
+        if now_col == ".":
+            continue
+        count = 0
+        for dx, dy in dxy:
+            if x + dx < 0 or x + dx > W - 1 or y + dy < 0 or y + dy > H - 1:
+                continue
+            target_col = grids[y+dy][x+dx]
+            if target_col == "#":
+                count += 1
+        # 上下左右に黒マスがなければ白に色を塗ってしまうことになるため失敗
+        if count == 0:
+            ans = "No"
+            break
+print(ans)
