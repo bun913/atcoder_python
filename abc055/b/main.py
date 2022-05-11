1dcc098855bda1307caf2f2b9dad9aaec88ecc9c
bun913 <imanau03@gmail.com>
2022-05-12 08:36:08 +0900

    feat: ABC055のB問題を解答

--- abc055/b/main.py
+++ abc055/b/main.py
@@ -18,4 +18,13 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
-
+# 非常に大きな数同士の計算についての問題
+# 今回で言えばあまりを求めるわけだが階乗を続けていくと非常に大きな値になる
+# それを 10^9 + 7 という非常に大きな数と計算しようとするとオーバーフローする
+# https://math.nakaken88.com/textbook/cp-remainder/
+n = int(input())
+c = 10**9 + 7
+ans = 1
+for i in range(1, n+1):
+    ans = (ans * i) % c
+print(ans)
