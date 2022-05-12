ce87276f9e1fe10a6c2bb02db6b1261276ebd1df
bun913 <imanau03@gmail.com>
2022-05-12 19:02:09 +0900

    feat: abc046のB問題を追加

--- abc046/b/main.py
+++ abc046/b/main.py
@@ -18,4 +18,11 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
-
+N, K = list(map(int, input().split()))
+ans = 1
+for i in range(N):
+    if i == 0:
+        ans *= K
+    else:
+        ans *= (K-1)
+print(ans)
