deab51814adbced945c02bf8a1a2d6a6a43ffabf
bun913 <imanau03@gmail.com>
2022-05-14 05:58:12 +0900

    feat: ABC048のB問題を解答

--- abc048/b/main.py
+++ abc048/b/main.py
@@ -18,4 +18,17 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+# 1..nまでの整数でmで割り切れるものの数は n/m個
+# mが何回登場するかって考えでしょ、じゃあn//mじゃん
 
+a, b, x = list(map(int, input().split()))
+
+def count_of_div(n, m):
+    return n // m
+
+# 1以上b以下までの個数から
+# 1以上a-1以下までの個数
+# を引けば良い
+a_count = count_of_div(a-1,x)
+b_count = count_of_div(b,x)
+print(b_count-a_count)
