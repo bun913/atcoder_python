bdea3b83d02a1fd7107e1276892d0ad5743eb5cc
bun913 <imanau03@gmail.com>
2022-05-20 04:59:42 +0900

    feat: ABC199のB問題を追加

--- abc199/b/main.py
+++ abc199/b/main.py
@@ -18,4 +18,22 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+n = int(input())
+a_list = list(map(int, input().split()))
+b_list = list(map(int, input().split()))
+ans_dic = {}
 
+for i in range(n):
+    a = a_list[i]
+    b = b_list[i]
+    for x in range(a, b+1):
+        if x not in ans_dic:
+            ans_dic[x] = 1
+        else:
+            ans_dic[x] += 1
+ans = 0
+
+for v in ans_dic.values():
+    if v == n:
+        ans += 1
+print(ans)
