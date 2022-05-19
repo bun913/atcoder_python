297c64e72b0024034056dac09e923d0b6f23d255
bun913 <imanau03@gmail.com>
2022-05-20 04:42:07 +0900

    feat: ABC197のB問題を追加

--- abc197/b/main.py
+++ abc197/b/main.py
@@ -19,3 +19,27 @@ l = list(map(int, input().split()))
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
 
+h, w, x, y = list(map(int, input().split()))
+cols = []
+for _ in range(h):
+    l = list(input())
+    cols.append(l)
+x -= 1
+y -= 1
+dxy = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+ans = 1
+# X, Yの左右上下のマスを見続けていく。左からみ初めて#が見つかったら次は右をずっと見ていく。その次は下、その次は上
+for dx, dy in dxy:
+    curx = x+dx
+    cury = y+dy
+    while True:
+        # インデックスが上下左右に過ぎたら次の方向を見る
+        if curx < 0 or curx > h-1 or cury < 0 or cury > w-1:
+            break
+        # #が見つかったら次の方向を見る
+        if cols[curx][cury] == "#":
+            break
+        ans += 1
+        curx += dx
+        cury += dy
+print(ans)
