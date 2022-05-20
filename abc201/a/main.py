f9a0f9fb086822a5ca009eaf457a102fa1825d63
bun913 <imanau03@gmail.com>
2022-05-21 05:35:37 +0900

    feat: solved ABC201 B

--- abc201/a/main.py
+++ abc201/a/main.py
@@ -19,3 +19,9 @@ l = list(map(int, input().split()))
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
 
+a, b, c = sorted(list(map(int, input().split())))
+
+ans = "No"
+if c - b == b - a:
+    ans = "Yes"
+print(ans)
