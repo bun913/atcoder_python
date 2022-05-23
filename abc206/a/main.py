801d1b52f063d2bcc94d2a58abd4087f63d8a44e
bun913 <imanau03@gmail.com>
2022-05-24 05:02:19 +0900

    feat: solve abc206 B

--- abc206/a/main.py
+++ abc206/a/main.py
@@ -18,4 +18,12 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
-
+import math
+n = int(input())
+cost = math.floor((1.08 * n))
+ans = 'Yay!'
+if cost == 206:
+    ans = "so-so"
+elif cost > 206:
+    ans = ":("
+print(ans)
