025bd4f9dafc08b6b20856c2df367f9e405137e2
bun913 <imanau03@gmail.com>
2022-05-27 05:10:33 +0900

    feat: solve abc211 b

--- abc211/b/main.py
+++ abc211/b/main.py
@@ -18,4 +18,14 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+l = [input() for _ in range(4)]
+is_h = l.count("H") == 1
+is_2h = l.count("2B") == 1
+is_3h = l.count("3B") == 1
+is_hr = l.count("HR") == 1
+is_all = all([is_h, is_2h, is_3h, is_hr])
 
+ans = "No"
+if is_all:
+    ans = "Yes"
+print(ans)
