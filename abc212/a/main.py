71d4fec79c67f42a19c811f27e80d607907be403
bun913 <imanau03@gmail.com>
2022-05-28 08:12:00 +0900

    feat: solve abc212 a

--- abc212/a/main.py
+++ abc212/a/main.py
@@ -18,4 +18,11 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+ans = "Alloy"
+a, b = list(map(int, input().split()))
 
+if 0 < a and b == 0:
+    ans = "Gold"
+elif a == 0 and 0 < b:
+    ans = "Silver"
+print(ans)
