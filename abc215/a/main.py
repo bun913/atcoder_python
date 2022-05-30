0a933776400b595f119fdbd38f9b119bab857a58
bun913 <imanau03@gmail.com>
2022-05-31 05:22:14 +0900

    feat: add abc215 a

--- abc215/a/main.py
+++ abc215/a/main.py
@@ -18,4 +18,8 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
-
+s = input()
+if s == "Hello,World!":
+    print("AC")
+    exit()
+print("WA")
