df36b598dc841c993e794575a24664931d5bca1d
bun913 <imanau03@gmail.com>
2022-06-03 16:57:15 +0900

    feat: solve mat019(combinations)

--- math-and-algorithm/019/main.py
+++ math-and-algorithm/019/main.py
@@ -18,4 +18,22 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
-
+n = int(input())
+l = list(map(int, input().split(" ")))
+c1 = 0
+c2 = 0
+c3 = 0
+for i in l:
+    if i == 1:
+        c1 += 1
+    elif i == 2:
+        c2 += 1
+    else:
+        c3 += 1
+ans = 0
+for c in [c1, c2, c3]:
+    if c <= 1:
+        continue
+    s = c * (c-1) // 2
+    ans += s
+print(ans)
