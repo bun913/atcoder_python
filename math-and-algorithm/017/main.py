b36e3d8359871cf9c562d5c8be1434e6486b2353
bun913 <imanau03@gmail.com>
2022-06-03 15:08:39 +0900

    feat: solve mat017(mcl)

--- math-and-algorithm/017/main.py
+++ math-and-algorithm/017/main.py
@@ -18,4 +18,34 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+import copy
 
+
+def gcd(a: int, b: int) -> int:
+    if a == 0 or b == 0:
+        return max(a, b)
+    big = max(a, b)
+    small = min(a, b)
+    mod = big % small
+    return gcd(mod, small)
+
+
+def mcl(a: int, b: int) -> int:
+    g = gcd(a, b)
+    ans = g * (a // g) * (b // g)
+    return ans
+
+
+n = int(input())
+l = list(map(int, input().split(" ")))
+
+ans = 1
+while True:
+    if len(l) == 1:
+        break
+    a = l.pop()
+    b = l.pop()
+    m = mcl(a, b)
+    l.append(m)
+
+print(l[0])
