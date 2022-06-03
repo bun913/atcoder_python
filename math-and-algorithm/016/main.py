2850c749309e00361b0f0250cb85d219855d3372
bun913 <imanau03@gmail.com>
2022-06-03 14:37:46 +0900

    feat: solve mat016(multi numbers gcd)

--- math-and-algorithm/016/main.py
+++ math-and-algorithm/016/main.py
@@ -18,4 +18,25 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+n = int(input())
+l = list(map(int, input().split(" ")))
 
+
+def gcd(a: int, b: int) -> int:
+    if a == 0 or b == 0:
+        return max(a, b)
+    big = max(a, b)
+    small = min(a, b)
+    mod = big % small
+    return gcd(mod, small)
+
+
+# 順番はどうでも良いのでスタックっぽく出し入れを行う
+while True:
+    if len(l) <= 1:
+        print(l[0])
+        exit()
+    a = l.pop()
+    b = l.pop()
+    g = gcd(a, b)
+    l.append(g)
