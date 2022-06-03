51b19b36d64c9f6ab57ae6cb2840628f21ef56f3
bun913 <imanau03@gmail.com>
2022-06-03 14:23:09 +0900

    feat: solve mat015(Euclidean algorithm)

--- math-and-algorithm/015/main.py
+++ math-and-algorithm/015/main.py
@@ -18,4 +18,18 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+a, b = list(map(int, input().split(" ")))
+# ユークリッドの互助法を使って一から実装する
+# Pythonだったら math.gcdで求められるが理解促進のため、車輪をもう一回作ってみる
 
+
+def gcd(a: int, b: int) -> int:
+    if a == 0 or b == 0:
+        return max(a, b)
+    big = max(a, b)
+    small = min(a, b)
+    mod = big % small
+    return gcd(mod, small)
+
+
+print(gcd(a, b))
