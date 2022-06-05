cba2ec6c4084b1e3732d39a7be5311f96c6e1e79
bun913 <imanau03@gmail.com>
2022-06-05 11:18:08 +0900

    feat: solve mat025

--- math-and-algorithm/025/main.py
+++ math-and-algorithm/025/main.py
@@ -18,4 +18,14 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+n = int(input())
+al = list(map(int, input().split()))
+bl = list(map(int, input().split()))
 
+ans = 0.0
+for i in range(n):
+    # 和の期待値を合計すると答えになる
+    a = al[i]
+    b = bl[i]
+    ans += (a * 1/3) + (b * 2/3)
+print(ans)
