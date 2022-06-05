c96032b2b1b2aeea8a8e4cf7087edca074d6bd56
bun913 <imanau03@gmail.com>
2022-06-05 10:07:29 +0900

    feat: solve mat022

--- math-and-algorithm/022/main.py
+++ math-and-algorithm/022/main.py
@@ -18,4 +18,23 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
-
+n = int(input())
+l = list(map(int, input().split(" ")))
+# 組み合わせ列挙のため、1~99999までの数を数えておく
+values = [0 for _ in range(1, 100000)]
+keys = [i for i in range(1, 100000)]
+dic = dict(zip(keys, values))
+# 数え上げ
+for i in l:
+    dic[i] += 1
+# (1,99999), (2,99998) と数え上げていく
+ans = 0
+for i in range(1, 50001):
+    conbi = (dic[i], dic[100000-i])
+    if i == 50000:
+        # 50,000だけ同じ数なのでnC2で計算
+        n = (conbi[0] * (conbi[0] - 1)) // 2
+        ans += n
+        break
+    ans += (conbi[0] * conbi[1])
+print(ans)
