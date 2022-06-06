5ea2a359d889885d582aaa9d2d55977b73614c25
bun913 <imanau03@gmail.com>
2022-06-07 06:03:07 +0900

    feat: solve abc224 b

--- abc224/b/main.py
+++ abc224/b/main.py
@@ -1,45 +1,12 @@
 # -*- coding: utf-8 -*-
-"""
-Atcoderの問題解く用
+H, W = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(H)]
 
-1行1列データ
-
-#str型で受け取るとき
-s = input()
-#int型で受け取るとき
-s = int(input())
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-"""
-from functools import reduce
-from itertools import combinations
-import math
-
-# 素数判定
-
-
-def is_prime(n: int) -> bool:
-    for i in range(2, int(math.sqrt(n))+1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def permutation(n, r):
-    return math.factorial(n) // math.factorial(n-r)
-
-
-def combination(n, r):
-    return permutation(n, r) // math.factorial(r)
+for i1 in range(H - 1):
+    for i2 in range(i1 + 1, H):
+        for j1 in range(W - 1):
+            for j2 in range(j1 + 1, W):
+                if not(A[i1][j1] + A[i2][j2] <= A[i2][j1] + A[i1][j2]):
+                    print("No")
+                    exit()
+print("Yes")
