9f20cafa1d8a4518408f71a062589b3b61564190
bun913 <imanau03@gmail.com>
2022-06-13 06:20:25 +0900

    feat: solve mat031(DP)

--- math-and-algorithm/031/main.py
+++ math-and-algorithm/031/main.py
@@ -18,4 +18,19 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
-
+N = int(input())
+A = list(map(int, input().split()))
+dp_study = [0 for _ in range(N+1)]
+dp_no_study = [0 for _ in range(N+1)]
+# i日目に勉強した場合と勉強しない場合を分けて考える
+# これでi日目までの最大値を求める
+for i in range(1, N+1):
+    # i日目に勉強する場合の最大値
+    # 2日続けて勉強はできないので、昨日勉強しなかった場合の実力のアップ値を使う
+    dp_study[i] = dp_no_study[i-1] + A[i-1]  # iは1始まりなので-1
+    # i日目に勉強しない場合の最大値
+    # 単純に昨日までの勉強しない場合と、勉強する場合の最大値を比べれば良い
+    no_study = dp_no_study[i-1]
+    study = dp_study[i-1]
+    dp_no_study[i] = max(no_study, study)
+print(max(dp_study[-1], dp_no_study[-1]))
