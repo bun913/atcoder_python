6319a7676d98eaf2b0bd4f38c4d1035f5c519623
bun913 <imanau03@gmail.com>
2022-06-13 04:45:59 +0900

    feat: solve mat009 with DP

--- math-and-algorithm/009/main.py
+++ math-and-algorithm/009/main.py
@@ -18,4 +18,29 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+N, S = list(map(int, input().split()))
+A = list(map(int, input().split()))
+# DPで解くため1行あたり10,000までの値を持つ二次元配列を用意
+limit = S+1
+dp = [[None] * (S + 1) for i in range(N + 1)]
+dp[0][0] = True
 
+# 数字がvになるような値が作れるか作れないか見れば良いだけ
+# 今引いているカードが
+for i in range(1, N+1):
+    for j in range(0, limit):
+        now = A[i-1]
+        # 現在のカードを使えない時(vが現在引いているカードの値より低い)
+        if j < now:
+            dp[i][j] = dp[i-1][j]
+        else:
+            no_use = dp[i-1][j]
+            use = dp[i-1][j-now]
+            if no_use is True or use is True:
+                dp[i][j] = True
+            else:
+                dp[i][j] = False
+ans = 'No'
+if dp[N][S] is True:
+    ans = 'Yes'
+print(ans)
