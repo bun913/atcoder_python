0cc64bef558df2d83a7d2a20368962eff90644c9
bun913 <imanau03@gmail.com>
2022-06-14 05:53:59 +0900

    feat: resolve abc070 b

--- abc070/b/main.py
+++ abc070/b/main.py
@@ -18,13 +18,9 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
-
-a, b, c, d = list(map(int, input().split()))
-_max = max(a,c)
-_min = min(b, d)
-dif = _min - _max
-
-ans = 0
-if dif >= 0:
-    ans = dif
+A, B, C, D = list(map(int, input().split()))
+# 重なりを求めるAが先,Bが先で条件分岐ではなく最大値を使う
+ans = min(B, D) - max(A, C)
+if ans < 0:
+    ans = 0
 print(ans)
