537e556e9edc2034619fdbfdd7d9734123e4e364
bun913 <imanau03@gmail.com>
2022-06-13 13:10:03 +0900

    feat: resolve abc096c(Grid)

--- abc096/c/main.py
+++ abc096/c/main.py
@@ -18,32 +18,21 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
-
 H, W = list(map(int, input().split()))
-grids = []
-for _a in range(H):
-    l = input()
-    grids.append(l)
-# 上下左右4方向を確認するための配列
-dxy = [
-    (-1, 0), (1, 0), (0, -1), (0,1)
-]
-ans = "Yes"
-for y in range(H):
-    for x in range(W):
-        now_col = grids[y][x]
-        # 白に塗るべきマスは飛ばす
-        if now_col == ".":
-            continue
+S = [list(input()) for _ in range(H)]
+
+dxy = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+for i in range(H):
+    for j in range(W):
         count = 0
         for dx, dy in dxy:
-            if x + dx < 0 or x + dx > W - 1 or y + dy < 0 or y + dy > H - 1:
+            plusi = i + dx
+            plusj = j + dy
+            if plusi < 0 or plusi >= H or plusj < 0 or plusj >= W:
                 continue
-            target_col = grids[y+dy][x+dx]
-            if target_col == "#":
+            if S[plusi][plusj] == '#':
                 count += 1
-        # 上下左右に黒マスがなければ白に色を塗ってしまうことになるため失敗
-        if count == 0:
-            ans = "No"
-            break
-print(ans)
+        if S[i][j] == '#' and count == 0:
+            print("No")
+            exit()
+print("Yes")
