f190a1a7c55f77afc11d0d332bc3539283da2b08
bun913 <imanau03@gmail.com>
2022-06-13 17:24:42 +0900

    feat: solve abc234 a

--- abc234/a/main.py
+++ abc234/a/main.py
@@ -73,3 +73,12 @@ def derot_n(s, n) -> str:
         answer += chr(ord('Z') - (ord('Z')-ord(letter)+n) % 26)
 
     return answer
+
+
+def f(t):
+    return (t**2) + (2*t) + 3
+
+
+t = int(input())
+ans = f(f(f(t)+t)+f(f(t)))
+print(ans)
