a8779bcbbb80a53ccb7999d4e3038de7b080ddfe
bun913 <imanau03@gmail.com>
2022-06-14 18:23:32 +0900

    feat: resolve mat019

--- math-and-algorithm/019/main.py
+++ math-and-algorithm/019/main.py
@@ -18,22 +18,35 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
-n = int(input())
-l = list(map(int, input().split(" ")))
+from operator import mul
+from functools import reduce
+
+
+def cmb(n, r):
+    r = min(n-r, r)
+    if r == 0:
+        return 1
+    over = reduce(mul, range(n, n - r, -1))
+    under = reduce(mul, range(1, r + 1))
+    return over // under
+
+
+N = int(input())
+A = list(map(int, input().split()))
+
 c1 = 0
 c2 = 0
 c3 = 0
-for i in l:
-    if i == 1:
+
+for a in A:
+    if a == 1:
         c1 += 1
-    elif i == 2:
+    elif a == 2:
         c2 += 1
     else:
         c3 += 1
-ans = 0
-for c in [c1, c2, c3]:
-    if c <= 1:
-        continue
-    s = c * (c-1) // 2
-    ans += s
-print(ans)
+
+cmb1 = cmb(c1, 2) if c1 >= 2 else 0
+cmb2 = cmb(c2, 2) if c2 >= 2 else 0
+cmb3 = cmb(c3, 2) if c3 >= 2 else 0
+print(cmb1 + cmb2 + cmb3)
