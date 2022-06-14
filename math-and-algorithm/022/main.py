1d8f6b8e1712049955007d2bb2f457d3e0055852
bun913 <imanau03@gmail.com>
2022-06-14 19:09:43 +0900

    feat: resolve mat022

--- math-and-algorithm/022/main.py
+++ math-and-algorithm/022/main.py
@@ -18,23 +18,40 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
-n = int(input())
-l = list(map(int, input().split(" ")))
-# 組み合わせ列挙のため、1~99999までの数を数えておく
-values = [0 for _ in range(1, 100000)]
-keys = [i for i in range(1, 100000)]
-dic = dict(zip(keys, values))
-# 数え上げ
-for i in l:
-    dic[i] += 1
-# (1,99999), (2,99998) と数え上げていく
+from operator import mul
+from functools import reduce
+
+
+def cmb(n, r):
+    r = min(n-r, r)
+    if r == 0:
+        return 1
+    elif r < 0:
+        return 0
+    over = reduce(mul, range(n, n - r, -1))
+    under = reduce(mul, range(1, r + 1))
+    return over // under
+
+
+N = int(input())
+A = list(map(int, input().split()))
+
+memo = dict([(k, 0) for k in range(1, 100001)])
+
+for a in A:
+    memo[a] += 1
+
 ans = 0
+
 for i in range(1, 50001):
-    conbi = (dic[i], dic[100000-i])
+    left = memo[i]
+    right = memo[100000-i]
+    # print(i, left, right)
+    conb = 0
     if i == 50000:
-        # 50,000だけ同じ数なのでnC2で計算
-        n = (conbi[0] * (conbi[0] - 1)) // 2
-        ans += n
-        break
-    ans += (conbi[0] * conbi[1])
+        if left >= 2:
+            conb = cmb(left, 2)
+    else:
+        conb = left * right
+    ans += conb
 print(ans)
