96a273cc13efb0b7e326bd6f367cb2336b6dcf4d
bun913 <imanau03@gmail.com>
2022-06-15 16:28:03 +0900

    feat: solve mat059

--- math-and-algorithm/059/main.py
+++ math-and-algorithm/059/main.py
@@ -18,4 +18,17 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+N = int(input())
+if N == 0:
+    print(1)
+    exit(1)
 
+mod = N % 4
+if mod == 1:
+    print(2)
+elif mod == 2:
+    print(4)
+elif mod == 3:
+    print(8)
+else:
+    print(6)
