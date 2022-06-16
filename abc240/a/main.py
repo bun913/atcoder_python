b86c3c3caed6561d6e09db0958cdd411ec02693e
bun913 <imanau03@gmail.com>
2022-06-17 05:20:45 +0900

    feat: solve abc240 a

--- abc240/a/main.py
+++ abc240/a/main.py
@@ -12,3 +12,13 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+a, b = list(map(int, input().split()))
+ad, bd = (a % 10, b % 10)
+
+ans = 'No'
+if abs(ad - bd) == 1:
+    ans = 'Yes'
+elif a == 9 and b == 10:
+    ans = 'Yes'
+print(ans)
