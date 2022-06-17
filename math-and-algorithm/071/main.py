a2d362c7b0ae468dcdce7b42a01f1709accf7628
bun913 <imanau03@gmail.com>
2022-06-17 16:12:19 +0900

    chore: can't solve mat071

--- math-and-algorithm/071/main.py
+++ math-and-algorithm/071/main.py
@@ -18,4 +18,6 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
-
+N = int(input())
+ABC = [list(map(int, input().split())) for _ in range(N)]
+_max = -1
