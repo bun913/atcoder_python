0989b496660baca13f6db2e32f52dad1aac91276
bun913 <imanau03@gmail.com>
2022-06-17 16:41:32 +0900

    feat: solve abc243 a

--- abc243/a/main.py
+++ abc243/a/main.py
@@ -12,3 +12,22 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+v, a, b, c = list(map(int, input().split()))
+l = [a, b, c]
+i = 0
+while True:
+    h = l[i]
+    if v - h < 0:
+        if i == 0:
+            print('F')
+        elif i == 1:
+            print('M')
+        else:
+            print('T')
+        exit()
+    v -= h
+    if i == 2:
+        i = 0
+    else:
+        i += 1
