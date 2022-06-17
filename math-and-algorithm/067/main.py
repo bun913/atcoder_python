6673f5d67ca96e58cc47efa33dfcef72b7ec5414
bun913 <imanau03@gmail.com>
2022-06-17 10:37:49 +0900

    feat: solve mat067

--- math-and-algorithm/067/main.py
+++ math-and-algorithm/067/main.py
@@ -18,4 +18,22 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
-
+H, W = list(map(int, input().split()))
+A = [list(map(int, input().split())) for _ in range(H)]
+# 行ごとの和と列ごとの和を計算しておく
+row_sum = [sum(row) for row in A]
+col_sum = []
+zipped = list(zip(*A))
+for col in zipped:
+    col_sum.append(sum(list(col)))
+# 計算した結果を元に重複部分（自分）を除いた結果を出力する
+ans = []
+for i in range(H):
+    l = []
+    for j in range(W):
+        s = row_sum[i] + col_sum[j] - A[i][j]
+        l.append(str(s))
+    ans.append(l)
+# 答えの出力
+for i in range(H):
+    print(' '.join(ans[i]))
