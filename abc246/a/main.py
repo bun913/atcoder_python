06a53d9709fc46fc1cfd7a4fb7973be8098f0de3
bun913 <imanau03@gmail.com>
2022-06-18 05:11:59 +0900

    feat: refactor abc246a

--- abc246/a/main.py
+++ abc246/a/main.py
@@ -19,21 +19,8 @@ x3, y3 = list(map(int, input().split()))
 S = [(x1, y1), (x2, y2), (x3, y3)]
 x4, y4 = 101, 101
 
-xl = [x1, x2, x3]
-yl = [y1, y2, y3]
-
-if xl.count(x1) == 1:
-    x4 = x1
-elif xl.count(x2) == 1:
-    x4 = x2
-elif xl.count(x3) == 1:
-    x4 = x3
-
-if yl.count(y1) == 1:
-    y4 = y1
-elif yl.count(y2) == 1:
-    y4 = y2
-elif yl.count(y3) == 1:
-    y4 = y3
+# 排他的論理和を使えば、一発で仲間外れを見つけることができる
+x4 = x1 ^ x2 ^ x3
+y4 = y1 ^ y2 ^ y3
 
 print("{} {}".format(x4, y4))
