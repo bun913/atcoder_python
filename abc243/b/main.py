bc5ccffc700b216d6564003cbf017ea289bff76e
bun913 <imanau03@gmail.com>
2022-06-17 16:46:10 +0900

    feat: solve abc243 b

--- abc243/b/main.py
+++ abc243/b/main.py
@@ -12,3 +12,20 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+eq = 0
+dif = 0
+for i in range(N):
+    for j in range(N):
+        if A[i] == B[j]:
+            if i == j:
+                eq += 1
+            else:
+                dif += 1
+print(eq)
+print(dif)
