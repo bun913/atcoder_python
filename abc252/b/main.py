afb589abd6ad9d6fdcec6f40f6c2bc4a95485ef1
bun913 <imanau03@gmail.com>
2022-06-20 05:57:07 +0900

    feat: solve abc252 b

--- abc252/b/main.py
+++ abc252/b/main.py
@@ -12,3 +12,17 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+N, K = list(map(int, input().split()))
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+
+_max = max(A)
+ans = 'No'
+for i in range(N):
+    a = A[i]
+    if a == _max and i+1 in B:
+        ans = 'Yes'
+        break
+print(ans)
