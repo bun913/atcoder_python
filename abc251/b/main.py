c26f5f54eedca03c752b335ab76362733b56a3f4
bun913 <imanau03@gmail.com>
2022-06-19 20:23:35 +0900

    feat: add abc251 first solvement(tle)

--- abc251/b/main.py
+++ abc251/b/main.py
@@ -12,3 +12,49 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+# 単純に100から3種類選んでも10**6 * 5種類(3種類からどれか最低でも一つを使う)
+N, W = list(map(int, input().split()))
+A = list(map(int, input().split()))
+ans = set()
+
+for a in range(N):
+    # 1つだけの場合
+    if N == 1:
+        if A[a] <= W:
+            print(1)
+            exit()
+    for b in range(a+1, N):
+        # 2つだけの場合
+        if N == 2:
+            if A[a] <= W:
+                ans.add(A[a])
+            if A[b] <= W:
+                ans.add(A[b])
+            if A[a] + A[b] <= W:
+                ans.add(A[a] + A[b])
+            print(len(ans))
+            exit()
+        for c in range(b+1, N):
+            # ここから3種類をbit全探索
+            for i in range(8):
+                uses = []
+                for j in range(3):
+                    if ((i >> j) & 1):
+                        uses.append(True)
+                    else:
+                        uses.append(False)
+                # 全く使わないパターンは想定しない
+                if True not in uses:
+                    continue
+                s = 0
+                if uses[0]:
+                    s += A[a]
+                if uses[1]:
+                    s += A[b]
+                if uses[2]:
+                    s += A[c]
+                if s <= W:
+                    ans.add(s)
+# print(ans)
+print(len(ans))
