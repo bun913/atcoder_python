f56c9d3e669fe29534d7f6625c7617594bad7e35
bun913 <imanau03@gmail.com>
2022-06-19 16:49:35 +0900

    feat: solve abc249 b

--- abc249/b/main.py
+++ abc249/b/main.py
@@ -12,3 +12,23 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+S = input()
+
+# 大文字が含まれるか判定
+# 小文字が含まれるか判定
+is_upper = False
+is_lower = False
+for s in S:
+    if s.isupper():
+        is_upper = True
+    elif s.islower():
+        is_lower = True
+# 全てユニークな文字列か判定
+is_unique = False
+if len(set(list(S))) == len(S):
+    is_unique = True
+if all([is_lower, is_upper, is_unique]):
+    print('Yes')
+    exit()
+print("No")
