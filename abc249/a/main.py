daeecca7f18792175f07051093c4afd3bdf78d0e
bun913 <imanau03@gmail.com>
2022-06-19 16:42:33 +0900

    feat: solve abc249 a

--- abc249/a/main.py
+++ abc249/a/main.py
@@ -12,3 +12,31 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+A, B, C, D, E, F, X = list(map(int, input().split()))
+
+t = 0
+a = 0
+
+# 何セット歩けるか
+t_set = X // (A+C)
+a_set = X // (D+F)
+# あまりが何秒あるか
+t_mod = X % (A+C)
+a_mod = X % (D+F)
+
+# まずはセット数で進んだメートルを計算
+t = (B * A) * t_set
+a = (E * D) * a_set
+
+# あまりで進める文をたす
+t += (min(t_mod, A) * B)
+a += (min(a_mod, D) * E)
+
+
+if t > a:
+    print('Takahashi')
+elif a > t:
+    print('Aoki')
+elif t == a:
+    print('Draw')
