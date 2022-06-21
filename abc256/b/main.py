0afe70579b32dca19927e2a1d2708906de924b2d
bun913 <imanau03@gmail.com>
2022-06-21 15:38:35 +0900

    feat: solve abc256b

--- abc256/b/main.py
+++ abc256/b/main.py
@@ -12,3 +12,23 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+N = int(input())
+A = list(map(int, input().split()))
+P = 0
+# 大きめのlistを用意してはみ出した文をポイントにする
+B = [0 for _ in range(8)]
+B[0] = 1
+
+for i in range(N):
+    a = A[i]
+    B[0] = 1
+    for j in range(3, -1, -1):
+        if B[j] == 1:
+            B[j+a] = 1
+            B[j] = 0
+    # ４以上は足し上げて0に戻す
+    P += sum(B[4:])
+    B[4:] = [0, 0, 0, 0]
+
+print(P)
