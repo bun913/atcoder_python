79f0d7cedd53505ca3a85c7bb945be77388e1a00
bun913 <imanau03@gmail.com>
2022-06-21 13:52:01 +0900

    feat: solve abc255 b

--- abc255/b/main.py
+++ abc255/b/main.py
@@ -12,3 +12,31 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+N, K = list(map(int, input().split()))
+A = list(map(int, input().split()))
+xl = []
+yl = []
+for _ in range(N):
+    x, y = list(map(int, input().split()))
+    xl.append(x)
+    yl.append(y)
+
+min_dis = []
+
+for i in range(N):
+    # まず全ての点（人)にとって一番近い点を見つける
+    # そこの点との距離をとっていき、それの最大値が答え
+    near_dis = float('INF')
+    x = xl[i]
+    y = yl[i]
+    for j in range(K):
+        k = A[j] - 1
+        kx = xl[k]
+        ky = yl[k]
+        ab = (x-kx) ** 2 + (y-ky) ** 2
+        near_dis = min(near_dis, ab)
+    min_dis.append(near_dis)
+# 平方根で絶対距離の計算に戻す
+ans = math.sqrt(max(min_dis))
+print(ans)
