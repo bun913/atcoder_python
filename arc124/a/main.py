64e1a976c455a95a222567556f96e953e0ecc1f6
bun913 <imanau03@gmail.com>
2022-06-23 14:19:17 +0900

    feat: solve arc124a

--- arc124/a/main.py
+++ arc124/a/main.py
@@ -1,54 +1,29 @@
 # -*- coding: utf-8 -*-
-"""
-Atcoderの問題解く用
+N, K = map(int, input().split())
+constraints = [0] * (N + 1)
+can_num = 0
+for _ in range(K):
+    c, k = input().split()
+    constraints[int(k)] = c
+    if c == "R":
+        can_num += 1
+MOD = 998244353
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+ans = 1
+# カードを左端から右へ向けて順に見ていく
+for i in range(1, N + 1):
+    # 今のカードのインデックスに制約があるか？無いか？をチェック
+    if constraints[i]:
+        # 制約があるなら今のカードのインデックスの数は既に指定されている(候補が１つしか無い)
+        # 加えて候補となる数の種類が変化する
+        if constraints[i] == "L":
+            can_num += 1
+        else:
+            can_num -= 1
 
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
-"""
-from functools import reduce
-from itertools import combinations
-import math
-
-# 同じカードの枠に制約がつくことはない
-N, K = list(map(int, input().split()))
-# 残りの枠の個数を保持
-frames = [K for _ in range(N)]
-
-
-def decre(n: int):
-    if n > 1:
-        return n-1
+    # 今のカードのインデックスでの制約が無いなら候補数を掛け算する
     else:
-        return n
+        ans *= can_num
+        ans %= MOD
 
-
-for i in range(1, K+1):
-    c, k = list(map(str, input().split()))
-    k = int(k)
-    left = []
-    right = []
-    middle = [1]
-    # print('frames:', frames)
-    if c == 'L':
-        left = list(map(decre, frames[:k-1]))
-        right = frames[k:]
-    else:
-        left = frames[:-k]
-        right = list(map(decre, frames[-k+1:]))
-    print('---------------------------')
-    print('条件: {}から{}番目が{}'.format(c, k, i))
-    print('frameの長さ:{}'.format(len(frames)))
-    print('fram:{}'.format((frames)))
-    print('left:{}'.format(left))
-    print('midl:{}'.format(middle))
-    print('righ:{}'.format(right))
-    frames = left + middle + right
-    print('afte:{}'.format(frames))
-# 最後にあまりを求めるととんでもない数になる
-mod = reduce(lambda total, current: total * (current % 998244353), frames, 1)
-ans = mod % 998244353
 print(ans)
