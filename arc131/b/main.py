d8538706da3c318a8e0a2fc32141fa36d5877f3e
bun913 <imanau03@gmail.com>
2022-06-23 17:06:30 +0900

    feat: solve arc131b

--- arc131/b/main.py
+++ arc131/b/main.py
@@ -12,3 +12,29 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+H, W = list(map(int, input().split()))
+dxy = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+full_colors = set(['1', '2', '3', '4', '5', ])
+C = [list(input()) for _ in range(H)]
+for i in range(H):
+    for j in range(W):
+        s = C[i][j]
+        if s != '.':
+            continue
+        # 上下左右を見てない色を選ぶ
+        colors = set()
+        for dx, dy in dxy:
+            a = i + dx
+            b = j + dy
+            if a < 0 or a > H-1 or b < 0 or b > W-1:
+                continue
+            colors.add(C[a][b])
+        # 入っていない色を適当に選ぶ
+        dif = full_colors.difference(colors)
+        # 前後左右に何もない場合がある
+        if len(dif) == 0:
+            C[i][j] = '.'
+        else:
+            C[i][j] = list(dif)[0]
+    print(*C[i], sep='')
