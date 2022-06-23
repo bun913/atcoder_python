5d6229f31f7a1e8e6724cfe20d36dbb15df64428
bun913 <imanau03@gmail.com>
2022-06-23 14:08:36 +0900

    chore: wip: arc124a

--- arc124/a/main.py
+++ arc124/a/main.py
@@ -12,3 +12,43 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+# 同じカードの枠に制約がつくことはない
+N, K = list(map(int, input().split()))
+# 残りの枠の個数を保持
+frames = [K for _ in range(N)]
+
+
+def decre(n: int):
+    if n > 1:
+        return n-1
+    else:
+        return n
+
+
+for i in range(1, K+1):
+    c, k = list(map(str, input().split()))
+    k = int(k)
+    left = []
+    right = []
+    middle = [1]
+    # print('frames:', frames)
+    if c == 'L':
+        left = list(map(decre, frames[:k-1]))
+        right = frames[k:]
+    else:
+        left = frames[:-k]
+        right = list(map(decre, frames[-k+1:]))
+    print('---------------------------')
+    print('条件: {}から{}番目が{}'.format(c, k, i))
+    print('frameの長さ:{}'.format(len(frames)))
+    print('fram:{}'.format((frames)))
+    print('left:{}'.format(left))
+    print('midl:{}'.format(middle))
+    print('righ:{}'.format(right))
+    frames = left + middle + right
+    print('afte:{}'.format(frames))
+# 最後にあまりを求めるととんでもない数になる
+mod = reduce(lambda total, current: total * (current % 998244353), frames, 1)
+ans = mod % 998244353
+print(ans)
