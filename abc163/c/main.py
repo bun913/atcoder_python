9bc54a269851e42444ff2f89a2f98e7d13b2aef7
bun913 <imanau03@gmail.com>
2022-06-24 17:06:45 +0900

    feat: solve abc163c

--- abc163/c/main.py
+++ abc163/c/main.py
@@ -12,3 +12,21 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+# N人の社員がいる
+# 社員番号が1の人がルート(上司がいない)
+# 自分より社員番号が小さい直属の上司が一人だけいる
+# 親が一つの木構造と言える
+# 社員番号iの直属の上司の社員番号がAiで与えられる
+# 各社員について直属の社員が何人いるか求めること
+# 直属だけであることに注意
+# 単純に出現回数じゃね
+
+N = int(input())
+A = list(map(int, input().split()))
+dic = dict([(i, 0) for i in range(1, N+1)])
+
+for a in A:
+    dic[a] += 1
+for i in range(1, N+1):
+    print(dic[i])
