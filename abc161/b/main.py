d9df5b7b33fc6de4b53592ed93c60368a4af516e
bun913 <imanau03@gmail.com>
2022-06-25 07:31:48 +0900

    feat: solve abc161b

--- abc161/b/main.py
+++ abc161/b/main.py
@@ -12,3 +12,21 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+# N種類から人気の商品をM選ぶ
+# 得票数が 1/4M未満の商品を選べない
+# これでM個選べるかどうかだけ
+
+N, M = list(map(int, input().split()))
+A = list(map(int, input().split()))
+AD = sorted(A, reverse=True)
+s = sum(A)
+
+ans = 'Yes'
+for i in range(M):
+    a = AD[i]
+    t = (1 / (4*M))
+    if a < t*s:
+        ans = 'No'
+        break
+print(ans)
