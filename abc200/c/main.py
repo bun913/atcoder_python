d32c73c85a125a1475bc1fc90d59f4e9207113a1
bun913 <imanau03@gmail.com>
2022-06-24 16:31:00 +0900

    feat: solve abc200 c

--- abc200/c/main.py
+++ abc200/c/main.py
@@ -18,4 +18,25 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+# N個の正の整数からなる数列Aが与えられる
+# iとjの組の個数を求める
+# Ai- Ajが200の倍数となる
+# Nが2 * 10 **5なので全組み合わせ列挙は無理
+# 今回はあまりの性質を利用して全ての数の200での余りを求めていけばO(N)ですみそう
 
+N = int(input())
+A = list(map(int, input().split()))
+# 全てのAの要素の余りを求めてここから逆算していく
+# 辞書の初期化
+dic = dict([(i, 0) for i in range(200)])
+for a in A:
+    mod = a % 200
+    dic[mod] += 1
+# 数え上げ
+ans = 0
+for i in range(200):
+    i_cnt = dic[i]
+    # あまりが同じになるものから2つ選ぶ
+    if i_cnt >= 2:
+        ans += (i_cnt * (i_cnt-1)) // 2
+print(ans)
