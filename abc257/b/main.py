6b2b205023b8d1c7de94c382bc97d79102cda223
bun913 <imanau03@gmail.com>
2022-06-26 11:52:59 +0900

    feat: solve abc257b

--- abc257/b/main.py
+++ abc257/b/main.py
@@ -12,3 +12,15 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+n, k, q = map(int, input().split())
+a = list(map(int, input().split()))
+l = list(map(int, input().split()))
+for i in range(q):
+    if(a[l[i]-1] == n):
+        continue
+    elif(l[i] == k):
+        a[l[i]-1] += 1
+    elif(a[l[i]-1]+1 < a[l[i]]):
+        a[l[i]-1] += 1
+print(*a)
