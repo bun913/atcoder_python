f6f1920b175a5f1de75222007086044a7093d36d
bun913 <imanau03@gmail.com>
2022-06-28 05:01:00 +0900

    feat: solve abc162b

--- abc162/b/main.py
+++ abc162/b/main.py
@@ -12,3 +12,16 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+from decimal import Decimal
+
+N = int(input())
+ans = Decimal('0')
+i = 1
+
+while i < N+1:
+    if i % 15 == 0 or i % 5 == 0 or i % 3 == 0:
+        i += 1
+        continue
+    ans += Decimal(str(i))
+    i += 1
+print(ans)
