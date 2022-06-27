c69f45306d2347140f4d469db22ea8c6f10bb6b8
bun913 <imanau03@gmail.com>
2022-06-27 13:49:27 +0900

    feat: solve abc239c myself

--- abc239/c/main.py
+++ abc239/c/main.py
@@ -12,3 +12,29 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+x1, y1, x2, y2 = list(map(int, input().split()))
+a_cand = []
+b_cand = []
+
+# 求める点はa,bの2点からそれぞれ絶対距離が3と慣れば良い(xかyの方向に合計3進む)
+for dx in range(-2, 3):
+    if dx == 0:
+        continue
+    dys = []
+    a = 3 - abs(dx)
+    dys.append(a)
+    dys.append(-a)
+    for dy in dys:
+        ax = x1 + dx
+        ay = y1 + dy
+        bx = x2 + dx
+        by = y2 + dy
+        a_cand.append((ax, ay))
+        b_cand.append((bx, by))
+ans = "No"
+for a in a_cand:
+    for b in b_cand:
+        if a == b:
+            ans = 'Yes'
+print(ans)
