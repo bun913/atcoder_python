03db5316f8b4a86ad26378d77d4691e3e2a48c58
bun913 <imanau03@gmail.com>
2022-06-27 12:49:08 +0900

    feat: solve m-soluction2020 c

--- m-solutions2020/c/main.py
+++ m-solutions2020/c/main.py
@@ -10,5 +10,18 @@ bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
 from functools import reduce
-from itertools import combinations
-import math
+
+N, K = list(map(int, input().split()))
+A = list(map(int, input().split()))
+
+# 成績を計算
+# 例えばKが３の時
+# K学期の評点は、１個以外掛け合わせる数は同じ
+# だからA[i]とA[i-k]を比較してA[i]が大きければ掛け算の合計は必ずおおきくなる
+for i in range(K, N):
+    bef = A[i-K]
+    cur = A[i]
+    if cur > bef:
+        print('Yes')
+        continue
+    print('No')
