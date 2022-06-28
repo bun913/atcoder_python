098a8dd636072c60d693550f624743f0d2ed0b55
bun913 <imanau03@gmail.com>
2022-06-28 13:33:18 +0900

    feat: solve abc202c(myself)

--- abc202/c/main.py
+++ abc202/c/main.py
@@ -19,3 +19,38 @@ l = list(map(int, input().split()))
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
 
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+C = list(map(int, input().split()))
+
+a_dic = {}
+c_dic = {}
+
+# 辞書の初期化
+for i in range(N+1):
+    a_dic[i] = 0
+    c_dic[i] = 0
+
+# AとCを辞書にして数をメモ
+for i in range(N):
+    a = A[i]
+    c = C[i]
+    if a in a_dic:
+        a_dic[a] += 1
+    if c in c_dic:
+        c_dic[c] += 1
+
+ans = 0
+
+# Bをループで回していき、AとBが同じになるもののうちCから選べるものを数え上げる
+for j in range(N):
+    jd = j+1
+    b = B[j]
+    if b in a_dic:
+        # Cの中にjdがあるか
+        if jd in c_dic:
+            cnt = a_dic[b] * c_dic[jd]
+            ans += cnt
+
+print(ans)
