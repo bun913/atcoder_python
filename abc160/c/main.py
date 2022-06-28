11453558fe38286f37d315ee4a349135c21983a4
bun913 <imanau03@gmail.com>
2022-06-29 06:35:17 +0900

    feat: solve abc160c(myself)

--- abc160/c/main.py
+++ abc160/c/main.py
@@ -16,19 +16,16 @@ import math
 K, N = list(map(int, input().split()))
 A = list(map(int, input().split()))
 
-# この手の問題を円環の問題と言うらしい
-# 円環の問題は一直線の数直線で表すことができれば楽
-# 例えば点間の距離だとKを２枚することで、最後の点から最初の点の距離とかも扱いやすくなる
+# 公式解説Youtubeで教えてもらった解法
+# 2Kまで回ることで各区間の距離を求めることができる
+# それにより一番長い区間を回らない経路を取れば良いと言う方法
+# (今回の場合2kまでしなくても、最後の点から最初の点までの距離だけ求めればよい)
+l = A + [A[0]]
+longest = -1
 
-# 今回で言えば点の最小距離は普通にどこかの点から時計回りに回って得られる
-
-# 単純に最後の点から最初の点までの距離を求めておく
-ans = abs(A[0] - A[N-1])
-
-# あとはiからスタート地点までの距離、スタート地点からiまでの距離を合算しておけば良い
-for i in range(N):
-    i_to_start = K - A[i]
-    # i-1までの距離を取ることで全部の家に訪問する
-    start_to_i = A[i-1]
-    ans = min(ans, i_to_start + start_to_i)
-print(ans)
+for i in range(1, N+1):
+    dis = l[i] - l[i-1]
+    if dis < 0:
+        dis += K
+    longest = max(longest, dis)
+print(K-longest)
