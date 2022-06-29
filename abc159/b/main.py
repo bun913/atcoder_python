3241aa5b8e17069061a4dfef216bb8e3ba2db4ea
bun913 <imanau03@gmail.com>
2022-06-30 04:57:31 +0900

    feat: solve abc159b

--- abc159/b/main.py
+++ abc159/b/main.py
@@ -12,3 +12,20 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+S = input()
+N = len(S)
+
+cond1 = S == S[::-1]
+ind1 = ((N-1) // (2) - 1)
+cond2 = S[:ind1+1] == S[:ind1+1][::-1]
+ind2 = ((N+3) // (2) - 1)
+cond3 = S[ind2:N] == S[ind2:N][::-1]
+
+ans = 'No'
+
+all_cond = all([cond1, cond2, cond3])
+# print([cond1, cond2, cond3])
+if all_cond is True:
+    ans = 'Yes'
+print(ans)
