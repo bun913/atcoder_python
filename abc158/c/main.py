1c041e2ee0d20504cb80080d617758b590a49f6a
bun913 <imanau03@gmail.com>
2022-06-30 17:12:53 +0900

    feat: solve abc158c

--- abc158/c/main.py
+++ abc158/c/main.py
@@ -12,3 +12,15 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+A, B = list(map(int, input().split()))
+
+# A,Bが100以下なので、せいぜいxは100倍くらいまでなのでそれを全探索する
+
+for i in range(min(A, B), 10000):
+    a = i * 8 // 100
+    b = i * 10 // 100
+    if (a == A) and (b == B):
+        print(i)
+        exit()
+print(-1)
