bb7e324916d926920d5863c036ceb9fe8f636b1d
bun913 <imanau03@gmail.com>
2022-07-02 05:45:15 +0900

    feat: solve abc156 c

--- abc156/c/main.py
+++ abc156/c/main.py
@@ -12,3 +12,17 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+# xiが1以上、100以下でNもたかだか100なので全探索で間に合いそう
+N = int(input())
+X = list(map(int, input().split()))
+
+ans = float('inf')
+
+for i in range(1, 101):
+    sum_hp = 0
+    for x in X:
+        hp = (x-i) ** 2
+        sum_hp += hp
+    ans = min(ans, sum_hp)
+print(ans)
