5ca02e38801c03f5d8f263b3dcc49cb5c940c4ad
bun913 <imanau03@gmail.com>
2022-07-03 05:39:15 +0900

    feat: solve abc155c

--- abc155/c/main.py
+++ abc155/c/main.py
@@ -12,3 +12,23 @@ if ((i >> j) & 1)
 from functools import reduce, lru_cache
 from itertools import combinations
 import math
+
+# Nは最大で@*10**5なのでループはネストできない
+# 辞書にメモで1回、辞書からの取り出しで1回、並び替えでnlogn
+
+N = int(input())
+dic = {}
+_max = -1
+# 辞書にメモ
+for i in range(N):
+    s = input()
+    if s in dic:
+        dic[s] += 1
+    else:
+        dic[s] = 1
+    _max = max(_max,  dic[s])
+# 辞書から_maxの要素を取得して並び替え
+keys = [k for k, v in dic.items() if v == _max]
+_sorted = sorted(keys)
+
+print(*_sorted, sep='\n')
