b3263b17e3f4784b327dbc8d48648f96fd186c52
bun913 <imanau03@gmail.com>
2022-07-02 21:33:12 +0900

    refactor: use_lru_cache abc247c

--- abc247/c/main.py
+++ abc247/c/main.py
@@ -9,19 +9,17 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce
+from functools import reduce, lru_cache
 from itertools import combinations
 import math
 
 N = int(input())
 
-memo = {}
 
-
+# Pythonのメモ化を簡単にしてくれるlru_cacheを使った場合
+@lru_cache(maxsize=None)
 def calc_s(n: int):
     # メモ化再起にしてみたバージョン
-    if n in memo:
-        return memo[n]
     if n == 1:
         return [1]
     return calc_s(n-1) + [n] + calc_s(n-1)
