dd4101b32c50ec892888a56fcfdf276dbe5b418e
bun913 <imanau03@gmail.com>
2022-07-02 21:25:26 +0900

    feat: solve abc247c

--- abc247/c/main.py
+++ abc247/c/main.py
@@ -12,3 +12,17 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+
+# Nはたかだか16でたかだか65,536文字にしかならないから全部一から計算しても問題なさそう
+
+N = int(input())
+
+
+def calc_s(n: int):
+    if n == 1:
+        return [1]
+    return calc_s(n-1) + [n] + calc_s(n-1)
+
+
+S = calc_s(N)
+print(*S)
