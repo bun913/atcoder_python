f34f79dd5df8894c524992c92693ebcec879b050
bun913 <imanau03@gmail.com>
2022-07-03 16:50:50 +0900

    feat: solve abc251c

--- abc251/c/main.py
+++ abc251/c/main.py
@@ -12,3 +12,29 @@ if ((i >> j) & 1)
 from functools import reduce
 from itertools import combinations
 import math
+from collections import OrderedDict
+
+# 同じ文字列でも特典が違う可能性がある
+# N回の提出のうち、ようするにその文字列の中で一番早く出したものがオリジナル
+# そのオリジナルの中から、最も特典が高いものを最優秀とする
+# さらに得点が被るなら提出が早いものを最油臭とする
+point_dic = OrderedDict()
+index_dic = {}
+
+N = int(input())
+
+_max = -1
+# dictにメモをする
+for i in range(N):
+    tmp = list(input().split())
+    s = tmp[0]
+    t = int(tmp[1])
+    if s not in point_dic:
+        point_dic[s] = t
+        index_dic[s] = i + 1
+        _max = max(t, _max)
+
+for k, v in point_dic.items():
+    if v == _max:
+        print(index_dic[k])
+        exit()
