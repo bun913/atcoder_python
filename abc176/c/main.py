e55c8b63ab5735d0b0b22d0c84df3586237be22c
bun913 <imanau03@gmail.com>
2022-07-05 05:23:17 +0900

    feat: solve abc176c

--- abc176/c/main.py
+++ abc176/c/main.py
@@ -12,3 +12,17 @@ if ((i >> j) & 1)
 from functools import reduce, lru_cache
 from itertools import combinations
 import math
+
+N = int(input())
+A = list(map(int, input().split()))
+# それぞれの足元に0以上の踏み台を設置
+# 全ての人が、踏み台を込めて身長を比較した時、自分より前に自分より背の高い人が存在しない
+# 基本的に前の人との差分をとっていくわけだが、それまでに見つけた最大値と高い方を比べないといけない
+ans = 0
+_max = A[0]
+for i in range(1, N):
+    cur = A[i]
+    if _max > cur:
+        ans += _max - cur
+    _max = max(cur, _max)
+print(ans)
