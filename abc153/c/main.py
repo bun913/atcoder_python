539f50eab1e663202ffaabeab80b5448d81a9d0c
bun913 <imanau03@gmail.com>
2022-07-05 05:14:10 +0900

    feat: solve abc153c

--- abc153/c/main.py
+++ abc153/c/main.py
@@ -12,3 +12,15 @@ if ((i >> j) & 1)
 from functools import reduce, lru_cache
 from itertools import combinations
 import math
+
+N, K = list(map(int, input().split()))
+H = list(map(int, input().split()))
+
+# 必殺技を全員に使える
+if K >= N:
+    print(0)
+    exit()
+_sorted = sorted(H, reverse=True)
+# 体力が大きい順に技を使う
+ans = sum(_sorted[K:])
+print(ans)
