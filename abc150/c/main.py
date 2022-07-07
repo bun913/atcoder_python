764fc0fabf5305451ecf5ece295eefb3723b73e0
bun913 <imanau03@gmail.com>
2022-07-08 05:24:41 +0900

    feat: solve abc150c

--- abc150/c/main.py
+++ abc150/c/main.py
@@ -10,5 +10,24 @@ bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
 from functools import reduce, lru_cache
-from itertools import combinations
+from itertools import combinations, permutations
 import math
+
+# Nの順列を考えて、与えられる順列P,Qがその順列たちの中で何番目か考える
+# その差の絶対値を出せばOK
+# 幸いにもNが8の階乗なので全列挙でも余裕
+# pythonはpermutationsがあるから余裕
+
+N = int(input())
+P = list(input().split())
+Q = list(input().split())
+
+perm = list(permutations([i for i in range(1, N+1)]))
+ps = ''.join(P)
+qs = ''.join(Q)
+
+_sorted = sorted([''.join(map(str, p)) for p in perm])
+a = _sorted.index(ps) + 1
+b = _sorted.index(qs) + 1
+
+print(abs(a-b))
