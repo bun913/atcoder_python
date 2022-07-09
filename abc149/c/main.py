fa396889065d439524ffba48034abe28bf8dca44
bun913 <imanau03@gmail.com>
2022-07-09 09:37:47 +0900

    feat: solve abc149c

--- abc149/c/main.py
+++ abc149/c/main.py
@@ -12,3 +12,26 @@ if ((i >> j) & 1)
 from functools import reduce, lru_cache
 from itertools import combinations
 import math
+
+# X以上の素数で最小のものを求める
+# LogNの式で素数判定すればいつかたどり着くのでは・・・・
+
+
+def is_prime(n: int) -> bool:
+    """
+    素数判定(LogN)
+    """
+    for i in range(2, int(math.sqrt(n))+1):
+        if n % i == 0:
+            return False
+    return True
+
+
+X = int(input())
+cur = X
+
+while True:
+    if is_prime(cur) is True:
+        print(cur)
+        exit()
+    cur += 1
