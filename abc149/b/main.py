691db6b300b1c05e4e912da330523ffe47592326
bun913 <imanau03@gmail.com>
2022-07-09 09:26:02 +0900

    feat: solve abc149b

--- abc149/b/main.py
+++ abc149/b/main.py
@@ -12,3 +12,18 @@ if ((i >> j) & 1)
 from functools import reduce, lru_cache
 from itertools import combinations
 import math
+
+# A >=1 a--
+# B >=1 b--
+# 10**12あるのでKはループできない
+
+A, B, K = list(map(int, input().split()))
+# 高橋のか食べる
+if A >= K:
+    print(A-K, B)
+    exit()
+# 青木のを食べる
+if B >= (K-A):
+    print(0, B-(K-A))
+    exit()
+print(0, 0)
