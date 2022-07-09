48018a281e501a7ca3565dd1381f871795aa04dd
bun913 <imanau03@gmail.com>
2022-07-09 20:26:41 +0900

    feat: solve abc258b

--- abc258/b/main.py
+++ abc258/b/main.py
@@ -12,3 +12,39 @@ if ((i >> j) & 1)
 from functools import reduce, lru_cache
 from itertools import combinations
 import math
+
+N = int(input())
+A = [list(map(int, list(input()))) for _ in range(N)]
+# 最初は二次元配列を上下左右プラス斜めの方向に同じものをたすことを考えて
+# 取り扱いが面倒そう
+# インデックスが+にはみ出るときは % N をとって
+# インデックスが-にはみ出るときはNを足してやればうまくいくのではないかと考え直した
+
+dxy = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+
+_max = -1
+for i in range(N):
+    for j in range(N):
+        # 各方向に移動
+        for dx, dy in dxy:
+            # N-1回だけ移動する
+            s = ''
+            curx = i
+            cury = j
+            for _ in range(N):
+                # マイナスの場合の補正(Nを足してあげる)
+                if curx < 0:
+                    curx += N
+                if cury < 0:
+                    cury += N
+                # プラスにはみ出る場合の補正(Nを引く)
+                if curx > N - 1:
+                    curx -= N
+                if cury > N-1:
+                    cury -= N
+                s += str(A[curx][cury])
+                curx -= dx
+                cury -= - dy
+            num = int(s)
+            _max = max(num, _max)
+print(_max)
