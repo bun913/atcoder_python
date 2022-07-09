16fe8a92332a71b4daf0b4248c0946a6ec5aa027
bun913 <imanau03@gmail.com>
2022-07-09 14:22:01 +0900

    feat: solve abc148d

--- abc148/d/main.py
+++ abc148/d/main.py
@@ -12,3 +12,39 @@ if ((i >> j) & 1)
 from functools import reduce, lru_cache
 from itertools import combinations
 import math
+
+# Nが最大200,000なのでループは1回はできる
+# 単純に最後に[1,2,3...]と初項1,差分1の等差数列になればOK
+
+N = int(input())
+A = list(map(int, input().split()))
+B = A[:]
+
+_next = 1
+ans = 0
+
+for i in range(N):
+    a = A[i]
+    if a != _next:
+        # 要素を実際に抜くと面倒なため-1を砕いた印とする
+        B[i] = -1
+        ans += 1
+        continue
+    _next += 1
+
+expcted = 1
+removed = [b for b in B if b != -1]
+
+# -1しかなかった場合
+if len(removed) == 0:
+    print(-1)
+    exit()
+
+for b in removed:
+    if expcted == b:
+        expcted += 1
+        continue
+    # 等差数列じゃなかった
+    print(-1)
+    exit()
+print(ans)
