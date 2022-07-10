24d4c786053460b6d34dd53f8b84b74bc91dd1ff
bun913 <imanau03@gmail.com>
2022-07-11 06:09:01 +0900

    feat: solve abc146c

--- abc146/c/main.py
+++ abc146/c/main.py
@@ -12,3 +12,24 @@ if ((i >> j) & 1)
 from functools import reduce, lru_cache
 from itertools import combinations
 import math
+
+"""
+考えたこと
+二部探索でいけばlogの計算で間に合うのでは？
+"""
+
+A, B, X = list(map(int, input().split()))
+
+# 2分探
+# 一番小さいのはまず0から
+# 最初に考えられる右端は10**9+1としておく
+l = 0
+r = 10 ** 9 + 1
+while r - l > 1:
+    c = (l + r) // 2
+    calced = A * c + B * len(str(c))
+    if calced <= X:
+        l = c
+    else:
+        r = c
+print(l)
