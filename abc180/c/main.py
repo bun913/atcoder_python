d40314b8b937f8452e1172fbc46e54701e751c21
bun913 <imanau03@gmail.com>
2022-07-11 18:23:38 +0900

    feat: solve abc180c

--- abc180/c/main.py
+++ abc180/c/main.py
@@ -12,3 +12,19 @@ if ((i >> j) & 1)
 from functools import reduce, lru_cache
 from itertools import combinations
 import math
+
+# 約数列挙の問題
+# Nが10 ** 12なので全部列挙は間に合わない
+# 約数は平方根まで計算すれば十分である性質を利用すれば余裕
+
+N = int(input())
+sqr = math.sqrt(N)
+divs = set()
+
+for i in range(1, int(sqr) + 1):
+    if N % i == 0:
+        divs.add(i)
+        divs.add(N // i)
+
+ans_list = sorted(list(divs))
+print(*ans_list, sep="\n")
