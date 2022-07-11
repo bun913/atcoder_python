a03cad1ff121a4b64097b26c5536108daa09e1c8
bun913 <imanau03@gmail.com>
2022-07-12 05:35:36 +0900

    feat: solve abc145b

--- abc145/b/main.py
+++ abc145/b/main.py
@@ -9,6 +9,15 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+N = int(input())
+if N % 2 != 0:
+    print("No")
+    exit()
+S = input()
+left = S[:N//2]
+right = S[N//2:]
+
+ans = 'No'
+if left == right:
+    ans = 'Yes'
+print(ans)
