21f03a4de83905c6c6be423cb0e4cf92f0b5ecf0
bun913 <imanau03@gmail.com>
2022-07-12 09:11:51 +0900

    feat: solve abc143c

--- abc143/c/main.py
+++ abc143/c/main.py
@@ -10,5 +10,13 @@ bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
 from functools import reduce, lru_cache
-from itertools import combinations
+from itertools import groupby
 import math
+
+# 同じ色をもつ隣接スライムは融合する
+# 要するに隣接した文字をグループにまとめて終えば良い
+# ラングレス圧縮の考え方でまとめて仕舞えば良い
+N = int(input())
+S = input()
+grouped = groupby(list(S))
+print(len(list(grouped)))
