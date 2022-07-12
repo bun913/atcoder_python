bec4971757507914bea362c242e49bbddd1036b6
bun913 <imanau03@gmail.com>
2022-07-12 19:00:01 +0900

    feat: solve abc144b

--- abc144/b/main.py
+++ abc144/b/main.py
@@ -9,6 +9,12 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+
+N = int(input())
+
+for i in range(1, 10):
+    for j in range(1, 10):
+        if i * j == N:
+            print('Yes')
+            exit()
+print('No')
