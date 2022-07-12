55df5181e6b0c70cc9594c4544ba3c4a0b9e8a57
bun913 <imanau03@gmail.com>
2022-07-13 05:18:39 +0900

    feat: solve abc142a

--- abc142/a/main.py
+++ abc142/a/main.py
@@ -9,6 +9,9 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+N = int(input())
+cnt = N // 2
+
+if N % 2 == 1:
+    cnt += 1
+print(cnt / N)
