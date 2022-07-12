b25a795411ee70465c18d107dcb74a39dbde7ddc
bun913 <imanau03@gmail.com>
2022-07-12 19:28:58 +0900

    feat: solve abc144c

--- abc144/c/main.py
+++ abc144/c/main.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+取り掛かる前のメモ
+横か縦に1づつしか動けないなら、ユークリッド距離が答えになるはず
+ただ、問題は無限に広がる空間ということので、探索はできない
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+約数を列挙して、その中から最も合計が小さい組み合わせを出せば良さげ
+Nが10 **12 なのでlogNなら十分間に合うはず
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+
+N = int(input())
+_min = float('inf')
+
+for d in range(1, int(N ** 0.5) + 1):
+    if N % d != 0:
+        continue
+    q = N // d
+    dis = (d-1) + (q-1)
+    _min = min(_min, dis)
+print(_min)
