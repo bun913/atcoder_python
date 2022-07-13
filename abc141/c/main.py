457b068dc69a5365d6034f64e917e16f9aab7526
bun913 <imanau03@gmail.com>
2022-07-13 18:50:08 +0900

    feat: solve abc141c

--- abc141/c/main.py
+++ abc141/c/main.py
@@ -1,14 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+解答前のメモ
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+正解した人以外の点が減るというのがポイント
+辞書とかでそれぞれのポイントを持って、それを減らしていけば
+毎回最大で10**5の繰り返しが入るので間に合わない。
 
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+まぁ、でも実際にポイントを減らしていく必要はなく
+単純に自分いがいの奴が何回正解したかがわかれば良いだけ
+(Q-自分の正解数)が減点される数字
+なので、それをKから引いて0以下ならNoと、逆ならYesと出すだけだろうと思う
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+
+N, K, Q = list(map(int, input().split()))
+correct_ans_dic = dict([(i, 0) for i in range(N)])
+
+for i in range(Q):
+    a = int(input())
+    correct_ans_dic[a-1] += 1
+
+for i in range(N):
+    own_correnct_ans = correct_ans_dic[i]
+    decrese_point = (Q-own_correnct_ans)
+    if max(0, K-decrese_point) == 0:
+        print('No')
+        continue
+    print('Yes')
