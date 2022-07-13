b71297f862599814866da5d1d814f1d3022d5393
bun913 <imanau03@gmail.com>
2022-07-14 08:45:43 +0900

    feat: solve abc235c

--- abc235/c/main.py
+++ abc235/c/main.py
@@ -1,75 +1,37 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+数xi がki回目に登場するのはAの前から何番目の要素をみた時かを出力
 
-1行1列データ
-
-#str型で受け取るとき
-s = input()
-#int型で受け取るとき
-s = int(input())
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+普通に考えれば2重ループで検索すれば良いが,Nが2 * 10**5 なので間に合わない
+まぁでもあらかじめaの数がそれぞれ何回、何番目に登場したかメモしておけば良いというだけ
+どういう持ち方をするか {1: set(1番目, 4番目, 5番目)}とすれば辞書を引くのも1,setから要素を見つけるのも計算量1でいけそう
 """
-from functools import reduce
-from itertools import combinations
-import math
 
-# 素数判定
+N, Q = list(map(int, input().split()))
+A = list(map(int, input().split()))
 
+# 各数字が出現した回数をメモ
+displaeyd_memo = dict([(a, 0) for a in A])
+# 各数字の出現回数と位置をメモ
+memo = dict([(a, {}) for a in A])
+for i in range(N):
+    cnt = i + 1
+    a = A[i]
+    dic = memo[a]
+    k = displaeyd_memo[a] + 1
+    dic[k] = cnt
+    displaeyd_memo[a] += 1
 
-def is_prime(n: int) -> bool:
-    """
-    素数判定(LogN)
-    """
-    for i in range(2, int(math.sqrt(n))+1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def permutation(n, r) -> int:
-    """
-    順列の計算
-    """
-    return math.factorial(n) // math.factorial(n-r)
-
-
-def combination(n, r) -> int:
-    """
-    組み合わせ数の計算
-    """
-    return permutation(n, r) // math.factorial(r)
-
-
-def rot_n(s, n) -> str:
-    """
-    アルファベットsをn文字分後にずらす
-    """
-    answer = ''
-    for letter in s:
-        answer += chr(ord('A') + (ord(letter)-ord('A')+n) % 26)
-    return answer
-
-
-def derot_n(s, n) -> str:
-    """
-    アルファベットsをn文字分戻す
-    """
-    answer = ''
-    for letter in s:
-        answer += chr(ord('Z') - (ord('Z')-ord(letter)+n) % 26)
-
-    return answer
+for q in range(Q):
+    x, k = list(map(int, input().split()))
+    # memoに要素が存在しない
+    if x not in memo:
+        print(-1)
+        continue
+    d = memo[x]
+    # 要素があるけどK個要素がない
+    if k not in d:
+        print(-1)
+        continue
+    ans = d[k]
+    print(ans)
