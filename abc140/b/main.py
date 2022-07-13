4c6fb4b1b0efd08222ad43ff58a161097cc694d7
bun913 <imanau03@gmail.com>
2022-07-14 05:44:14 +0900

    feat: solve abc140b

--- abc140/b/main.py
+++ abc140/b/main.py
@@ -9,6 +9,26 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+C = list(map(int, input().split()))
+
+s = 0
+bef = -1
+
+for i in range(N):
+    a = A[i]
+    b = B[a-1]
+    s += b
+    if i == 0:
+        bef = a
+        continue
+    # 満足度を満たす場合
+    if bef + 1 == a:
+        extra = C[bef-1]
+        s += extra
+    bef = a
+print(s)
