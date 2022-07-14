bdd3f4ae45c32b5bfb29ad9e6160522508931bcc
bun913 <imanau03@gmail.com>
2022-07-15 05:31:18 +0900

    feat: solve abc139c

--- abc139/c/main.py
+++ abc139/c/main.py
@@ -1,14 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+解く前のメモ
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+普通に考えると二重ループで全探索できるが
+それだと最大で10**10となるため間に合わない
 
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+問題を小さく切り分けて、各要素が自分の1個右だけをみて、その結果を記録しておく
+その結果,True(自分より右が自分以下だったマーク)が一番連続している箇所の数を抜き出せば良い
+pythonだとgroupby、ランレングス圧縮の考えを利用すれば良さげ
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from itertools import groupby
+
+N = int(input())
+H = list(map(int, input().split()))
+
+memo = []
+
+# 自分より隣の数が自分以下かをメモ
+for i in range(N-1):
+    cur = H[i]
+    nex = H[i+1]
+    if nex <= cur:
+        memo.append(1)
+        continue
+    memo.append(0)
+
+# グループ化
+grouped = [(k, list(g)) for k, g in groupby(memo)]
+ans = 0
+for key, elms in grouped:
+    if key == 0:
+        continue
+    ans = max(ans, len(elms))
+print(ans)
