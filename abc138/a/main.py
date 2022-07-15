46c1262314727cb98d8c61f5303822027c2a6ee4
bun913 <imanau03@gmail.com>
2022-07-16 05:59:30 +0900

    feat: solve abc138a

--- abc138/a/main.py
+++ abc138/a/main.py
@@ -9,6 +9,8 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+a = int(input())
+s = input()
+if a < 3200:
+    s = 'red'
+print(s)
