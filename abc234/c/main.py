7fb2145ca1a03770060dc1f50c0459a06d5b1577
bun913 <imanau03@gmail.com>
2022-07-17 05:28:22 +0900

    feat: solve abc234c

--- abc234/c/main.py
+++ abc234/c/main.py
@@ -1,75 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input()
-#int型で受け取るとき
-s = int(input())
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+問題を解く前のメモ
+0,2で現した正の整数のうちK番目に小さいもの
+2進数で1が2になっているバージョン
+１回単純に10進数でkを出して、それを2進数に戻す->1を2に置き換える
+とすれば良いのでは
 """
-from functools import reduce
-from itertools import combinations
-import math
-
-# 素数判定
-
-
-def is_prime(n: int) -> bool:
-    """
-    素数判定(LogN)
-    """
-    for i in range(2, int(math.sqrt(n))+1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def permutation(n, r) -> int:
-    """
-    順列の計算
-    """
-    return math.factorial(n) // math.factorial(n-r)
-
-
-def combination(n, r) -> int:
-    """
-    組み合わせ数の計算
-    """
-    return permutation(n, r) // math.factorial(r)
-
-
-def rot_n(s, n) -> str:
-    """
-    アルファベットsをn文字分後にずらす
-    """
-    answer = ''
-    for letter in s:
-        answer += chr(ord('A') + (ord(letter)-ord('A')+n) % 26)
-    return answer
-
-
-def derot_n(s, n) -> str:
-    """
-    アルファベットsをn文字分戻す
-    """
-    answer = ''
-    for letter in s:
-        answer += chr(ord('Z') - (ord('Z')-ord(letter)+n) % 26)
-
-    return answer
+K = int(input())
+bn = bin(K)
+to_s = str(bn)[2:]
+ans = to_s.replace('1', '2')
+print(ans)
