94d4a16ae181a34dfea542113f135c276fc73ad1
bun913 <imanau03@gmail.com>
2022-07-19 06:02:49 +0900

    feat: solve abc132c

--- abc132/c/main.py
+++ abc132/c/main.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+解く前のメモ
+Nが10**5で、dが1-**5が最大なのでやろうと思えば、1から数え上げても間に合いそう
+でも色々と制限を見ていたたら、必ずNが偶数なので、ソート済みの配列を半分に割って
+左側の配列の最後の要素より大きく、右側の配列の最初の要素以下の数を数えれば良さそう
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+
+N = int(input())
+D = list(map(int, input().split()))
+dr = sorted(D)
+
+left = dr[:N//2]
+right = dr[N//2:]
+
+le = left[-1]
+re = right[0]
+
+ans = max(0, re-le)
+print(ans)
