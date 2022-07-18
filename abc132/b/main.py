bac9bf8107c52dc9eeb548a451350c1664a9f957
bun913 <imanau03@gmail.com>
2022-07-19 05:53:46 +0900

    feat: solve abc132b

--- abc132/b/main.py
+++ abc132/b/main.py
@@ -9,6 +9,16 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+
+n = int(input())
+P = list(map(int, input().split()))
+
+ans = 0
+
+for i in range(1, n-1):
+    bef = P[i-1]
+    cur = P[i]
+    aft = P[i+1]
+    if sorted([bef, cur, aft])[1] == cur:
+        ans += 1
+print(ans)
