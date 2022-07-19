d3cbfd32caa8f2b480c26a50bfbf7aee146d35c5
bun913 <imanau03@gmail.com>
2022-07-20 05:02:25 +0900

    feat: solve abc133b

--- abc133/b/main.py
+++ abc133/b/main.py
@@ -9,6 +9,23 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from math import sqrt
+
+N, D = list(map(int, input().split()))
+X = [list(map(int, input().split())) for _ in range(N)]
+
+ans = 0
+# １個目の点
+for i in range(N):
+    # ２個目の点
+    for j in range(i+1, N):
+        s = 0
+        # 行
+        for d in range(D):
+            a = X[i][d]
+            b = X[j][d]
+            s += (a-b) ** 2
+        sqr = sqrt(s)
+        if sqr.is_integer() is True:
+            ans += 1
+print(ans)
