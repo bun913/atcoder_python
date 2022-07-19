8ab7b923d26c28f9fd4f8a0e93ad1c69a136415e
bun913 <imanau03@gmail.com>
2022-07-20 05:44:30 +0900

    feat: solve abc131c

--- abc131/c/main.py
+++ abc131/c/main.py
@@ -1,14 +1,33 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+解く前のメモ
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+A以上B以下の整数のうちCでもDでも割り切れないものの個数を求める
+普通ならA以上B以下のの数を1こづつ列挙して、割り切れるか試していくが
+今回はA,Bが最大で10**18なのでlog2にしても間に合わないという（約数列挙かと思った)
 
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+ベン図を書けば一発だった。
+A以上B以下の数を集合の全体として
+(c or bで割れるもの -  c and d で割れるもの)
+↑を全体から引いてやれば残りの数が答え
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from math import gcd
+
+
+def lcm(a, b):
+    return (a * b) // gcd(a, b)
+
+
+A, B, C, D = list(map(int, input().split()))
+
+
+# 集合を求める
+set_all = (B-A) + 1
+# CとDでたまたま同じ数が出ることがある
+set_c = (B // C) - ((A-1) // C)
+set_d = (B // D) - ((A-1) // D)
+lc = lcm(C, D)
+set_c_and_d = (B // lc) - ((A-1) // lc)
+
+ans = set_all - (set_c + set_d - set_c_and_d)
+print(ans)
