242635f30825d238de5ec4d8e18cc0508956914e
bun913 <imanau03@gmail.com>
2022-07-19 19:02:12 +0900

    feat: solve abc220c

--- abc220/c/main.py
+++ abc220/c/main.py
@@ -1,45 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+Aを10 ** 100回連結した数列を数列Bとする
+Bを前から順に足した時和が初めてXを超えるのはなんこうまで足した時か
 
-1行1列データ
-
-#str型で受け取るとき
-s = input()
-#int型で受け取るとき
-s = int(input())
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+これも長さがNって決まっているから
+和とあまりを使えば簡単に求められそう
 """
-from functools import reduce
-from itertools import combinations
-import math
 
-# 素数判定
+N = int(input())
+A = list(map(int, input().split()))
+X = int(input())
 
+one_sum = sum(A)
+q = X // one_sum
+m = X % one_sum
 
-def is_prime(n: int) -> bool:
-    for i in range(2, int(math.sqrt(n))+1):
-        if n % i == 0:
-            return False
-    return True
+ans = q * N
+cur = 0
 
-
-def permutation(n, r):
-    return math.factorial(n) // math.factorial(n-r)
-
-
-def combination(n, r):
-    return permutation(n, r) // math.factorial(r)
+for i in range(N):
+    a = A[i]
+    cur += a
+    ans += 1
+    if cur > m:
+        print(ans)
+        exit()
