6cf469fb354d272565a43a54cb492b6c8b4e114b
bun913 <imanau03@gmail.com>
2022-07-20 04:52:00 +0900

    feat: solve abc133a

---
+++ abc133/a/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Atcoderの問題解く用
+
+全ての組み合わせを列挙する方法
+list(0...8)から2つを抜き出す
+list(combinations(l, 2))
+
+bit全探索でフラグが立っているかチェックする
+if ((i >> j) & 1)
+"""
+N, A, B = list(map(int, input().split()))
+train = A * N
+taxi = B
+print(min(train, taxi))
