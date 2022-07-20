6653a30cd8559f7740c40d504085652312240516
bun913 <imanau03@gmail.com>
2022-07-21 05:26:31 +0900

    chore: solve abc130a

--- abc130/a/main.py
+++ abc130/a/main.py
@@ -9,6 +9,9 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+X, A = list(map(int, input().split()))
+
+if X < A:
+    print(0)
+else:
+    print(10)
