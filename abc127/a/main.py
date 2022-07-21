d79475bf88e728120473f0a488b2efbbb9db9ea2
bun913 <imanau03@gmail.com>
2022-07-22 05:57:34 +0900

    feat: solve abfc127a

--- abc127/a/main.py
+++ abc127/a/main.py
@@ -9,6 +9,11 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+A, B = list(map(int, input().split()))
+ans = B
+
+if 6 <= A <= 12:
+    ans = B // 2
+elif A <= 5:
+    ans = 0
+print(ans)
