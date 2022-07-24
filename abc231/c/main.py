ec55598f23f98f55fdb9cf2cc9e39fcef2c2aa27
bun913 <imanau03@gmail.com>
2022-07-25 05:45:51 +0900

    chore: add abc125ploblems

--- abc231/c/main.py
+++ abc231/c/main.py
@@ -1,21 +1,40 @@
 # -*- coding: utf-8 -*-
 """
-N人のうち身長がxj以上の生徒は何人か
+めぐる式二部探索で組み直したバージョン
+今回はxj以上のもので、最小のindexの値が分かれば良い
+なので、okの条件をx以上としたうえで、条件を満たす最小のindexを取れば良い
 
-普通に考えるならばQ回のループの中で、それぞれN人をfilterにかけて何人いるか数える方法
-ただ、これだとN,Qがそれぞれ最大で2*10**5であるため二重ループにしたら間に合わないと思われる
-
-なのであらかじめAをソートしておく
-あとは目的となる値を二部探索で求めれば良いだけ
-pythonならbisectというライブラリがある
+※他にもいくつかアプローチが考えられそうで、例えばxより大きいをOKとして
+最終的に出力されるNGを使うというてこともできそう
 """
-from bisect import bisect_left
 
 N, Q = list(map(int, input().split()))
 A = list(map(int, input().split()))
 AD = sorted(A)
 
+
+def is_ok(x: int, v):
+    if v >= x:
+        return True
+    return False
+
+
+def meguru_bisect(ng, ok, x):
+    '''
+    今回はx以上のものをOKとして使う
+    そうすればOKがx以上のものの一番左側となるため、x以上となる最初のindexがわかる
+    '''
+    while (abs(ok - ng) > 1):
+        mid = (ok + ng) // 2
+        if is_ok(x, AD[mid]):
+            ok = mid
+        else:
+            ng = mid
+    return ok
+
+
 for j in range(Q):
     x = int(input())
-    i = bisect_left(AD, x)
-    print(N-i)
+    i = meguru_bisect(-1, len(AD), x)
+    ans = N - i
+    print(ans)
