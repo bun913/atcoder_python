a2a1f018dab27a90a5d70ff05482da458fcd2ae3
bun913 <imanau03@gmail.com>
2022-07-25 06:20:09 +0900

    feat: solve abc125a

--- abc125/a/main.py
+++ abc125/a/main.py
@@ -9,6 +9,7 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+A, B, T = list(map(int, input().split()))
+s = int((T + 0.5) // A)
+ans = s * B
+print(ans)
