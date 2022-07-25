6a4fd9b90f7981dff090bbe0e1e76eafcc5e969b
bun913 <imanau03@gmail.com>
2022-07-26 05:24:53 +0900

    feat: solve abc229c

--- abc229/c/main.py
+++ abc229/c/main.py
@@ -1,45 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+DPの典型みたいな問題だと思った
 
-1行1列データ
-
-#str型で受け取るとき
-s = input()
-#int型で受け取るとき
-s = int(input())
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+が、どうも考えてみたら罠っぽかった
+というのもチーズは普通にどれを何グラム使っても良いわけなのでコスパの良い順に並べて余った文を付け足していけば良いだけ
 """
-from functools import reduce
-from itertools import combinations
-import math
+from operator import itemgetter
 
-# 素数判定
+N, W = list(map(int, input().split()))
+memo = []
+for i in range(N):
+    a, b = list(map(int, input().split()))
+    dic = {'value': a, 'weight': b, 'cosp': a / b}
+    memo.append(dic)
 
+_sorted = sorted(memo, key=itemgetter('value'), reverse=True)
+ans = 0
 
-def is_prime(n: int) -> bool:
-    for i in range(2, int(math.sqrt(n))+1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def permutation(n, r):
-    return math.factorial(n) // math.factorial(n-r)
-
-
-def combination(n, r):
-    return permutation(n, r) // math.factorial(r)
+for dic in _sorted:
+    if W == 0:
+        break
+    a = dic['value']
+    b = dic['weight']
+    # bとWの残りの最小値文だけ入れる
+    g = min(b, W)
+    ans += a * g
+    W -= g
+print(ans)
