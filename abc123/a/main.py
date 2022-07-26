39a5d6a1413d057e0dfaa1665f40f88ea457c89c
bun913 <imanau03@gmail.com>
2022-07-27 05:09:33 +0900

    feat: solve abc123a

--- abc123/a/main.py
+++ abc123/a/main.py
@@ -9,6 +9,16 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+A = [int(input()) for _ in range(5)]
+k = int(input())
+
+ans = 'Yay!'
+
+for i in range(5):
+    for j in range(i+1, 5):
+        x = A[i]
+        y = A[j]
+        if abs(x-y) > k:
+            ans = ':('
+            break
+print(ans)
