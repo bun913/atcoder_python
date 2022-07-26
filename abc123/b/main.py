a66ab5e9109bac69c9c161cad3a18b4f9b4e2f53
bun913 <imanau03@gmail.com>
2022-07-27 05:25:14 +0900

    feat: solve abc123b

--- abc123/b/main.py
+++ abc123/b/main.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+解く前のメモ
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+今回はA~Eしかないのがわかっているため、全ての並び替えのパターンを試して間に合う
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from itertools import permutations
+
+X = [int(input()) for _ in range(5)]
+# 順列を取得
+perm = list(permutations(X))
+
+ans = 100000 * 5
+
+for x in perm:
+    _sum = 0
+    for i in range(5):
+        a = x[i]
+        t = a
+        # 最後の注文の場合は10の倍数の考慮不要
+        if i == 4:
+            _sum += t
+            continue
+        # それ以外の場合10の倍数を考慮
+        if t % 10 == 0:
+            _sum += t
+            continue
+        rest = t % 10
+        _sum += t + (10 - rest)
+    ans = min(ans, _sum)
+print(ans)
