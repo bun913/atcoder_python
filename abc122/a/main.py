5f6b2c400f79e3727ff1229e5cac23a987646318
bun913 <imanau03@gmail.com>
2022-07-28 05:52:34 +0900

    feat: solve abc122a

--- abc122/a/main.py
+++ abc122/a/main.py
@@ -9,6 +9,7 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+b = input()
+
+base = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+print(base[b])
