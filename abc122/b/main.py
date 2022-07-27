7f63f8a612d52f45e5012f01ed1c50643aca8ddb
bun913 <imanau03@gmail.com>
2022-07-28 05:52:41 +0900

    feat: solve abc122b

--- abc122/b/main.py
+++ abc122/b/main.py
@@ -9,6 +9,21 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+S = input()
+ans = 0
+
+
+def is_acgt(s: str) -> bool:
+    if s in set(['A', 'C', 'G', 'T']):
+        return True
+    return False
+
+
+c_cnt = 0
+for s in S:
+    if is_acgt(s) is False:
+        c_cnt = 0
+        continue
+    c_cnt += 1
+    ans = max(c_cnt, ans)
+print(ans)
