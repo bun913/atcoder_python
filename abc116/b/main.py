2e00fecf89e0d0a3d30cb5d769b0ee58040bc824
bun913 <imanau03@gmail.com>
2022-07-29 06:25:05 +0900

    feat: solve abc116b

--- abc116/b/main.py
+++ abc116/b/main.py
@@ -9,6 +9,28 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from functools import lru_cache
+
+s = int(input())
+
+
+def f(n: int) -> int:
+    if n % 2 == 0:
+        return n // 2
+    return 3 * n + 1
+
+
+@lru_cache(maxsize=None)
+def recursion(i: int) -> int:
+    if i == 0:
+        return s
+    return f(recursion(i-1))
+
+
+_set = set()
+for i in range(0, 1000001):
+    ans = recursion(i)
+    if ans in _set:
+        print(i+1)
+        exit()
+    _set.add(ans)
