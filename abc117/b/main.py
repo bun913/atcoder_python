1603e788550b388315efbc8d954b5385ab50f811
bun913 <imanau03@gmail.com>
2022-07-31 07:11:42 +0900

    feat: solve abc117b

--- abc117/b/main.py
+++ abc117/b/main.py
@@ -9,6 +9,12 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+N = int(input())
+L = list(map(int, input().split()))
+_max = max(L)
+s = sum(L) - _max
+
+if _max < s:
+    print('Yes')
+    exit()
+print('No')
