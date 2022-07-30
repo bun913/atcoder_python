ed4c9675d227ebff5089e04fe1396725cb509fd7
bun913 <imanau03@gmail.com>
2022-07-30 16:47:22 +0900

    feat: solve abc116a

--- abc114/a/main.py
+++ abc114/a/main.py
@@ -9,6 +9,9 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+X = int(input())
+
+if X in {7, 5, 3}:
+    print('YES')
+    exit()
+print('NO')
