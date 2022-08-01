f47d489bca70b4427d82ba86f48ab48f36a8e195
bun913 <imanau03@gmail.com>
2022-08-01 19:05:14 +0900

    feat: solve abc262c

--- abc262/c/main.py
+++ abc262/c/main.py
@@ -3,31 +3,36 @@
 解く前のメモ
 
 普通にやるならiとjをそれぞれ二重ループで全探索
-だが、Nが10**5まであるのでi<jとなる組を数えるとTLE
+だが、Nが10**5まであるのでi<jとなる組を数えるとTLEとなってしまう
+
+以下解説AC
+ここでどのような状態の時に条件を満たすか考えてみる
+1: ai = i and aj = j
+2: ai = j and aj = i
+
+1は競技中に自分が考えていたものと同じ。つまりi==aiとなっているものの数を数える
+そこから2つを選ぶ組み合わせの数になるよね
+
+2が気づけなかった。
+iが一つの通りになればjは固定される分けだからiのループで十分だよねっていう話か
 """
 
 N = int(input())
 A = list(map(int, input().split()))
 
-# まず全てのくみをみてみる
-for i in range(N):
-    for j in range(i+1, N):
-        a = A[i]
-        b = A[j]
-        if min(a, b) == i+1 and max(a, b) == j + 1:
-            print(i+1, j+1)
+ans = 0
 
-# asc_set = set()
-# desc_set = set()
+# まず1のパターン
+n = 0
+for i, x in enumerate(A):
+    if i+1 == x:
+        n += 1
 
-# AR = A[::-1]
+# 添字と値が同じになっているものから2個組み合わせ
+ans += (n * (n-1) // 2)
 
-# for i in range(N):
-#     if i+1 == A[i]:
-#         asc_set.add(i+1)
-
-# for i in range(N):
-#     if i + 1 == AR[i]:
-#         desc_set.add(i+1)
-# print(asc_set)
-# print(desc_set)
+# パターン2の探索
+for i, j in enumerate(A):
+    if i+1 < j and A[j-1] == i+1:
+        ans += 1
+print(ans)
