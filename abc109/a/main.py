80265272371229da2f267b51c1a929af73307bbd
bun913 <imanau03@gmail.com>
2022-08-03 06:20:50 +0900

    feat: solve abc109a

--- abc109/a/main.py
+++ abc109/a/main.py
@@ -9,6 +9,8 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+A, B = list(map(int, input().split()))
+if A % 2 != 0 and B % 2 != 0:
+    print('Yes')
+    exit()
+print('No')
