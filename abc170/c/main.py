5ac2791ebaaa4f7eb33162780d2a9ebce9946ebc
bun913 <imanau03@gmail.com>
2022-08-05 05:48:52 +0900

    feat: solve abc170c

--- abc170/c/main.py
+++ abc170/c/main.py
@@ -5,26 +5,14 @@
 実はX,pが全て100以下の数字であるので全部数えても間に合う
 """
 X, N = list(map(int, input().split()))
-
-if N == 0:
-    print(100)
-    exit()
-
 P = list(map(int, input().split()))
 p_set = set(P)
 
-_min = 101
-
-for i in range(-200, 201):
-    if i in p_set:
-        continue
-    dif = abs(X-i)
-    _min = min(_min, dif)
-
-for i in range(-200, 201):
-    if i in p_set:
-        continue
-    dif = abs(X-i)
-    if dif == _min:
-        print(i)
-        exit()
+ans = 105
+diff = 800
+for y in range(0, 102):
+    d = abs(y-X)
+    if d < diff and y not in p_set:
+        ans = y
+        diff = d
+print(ans)
