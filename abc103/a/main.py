14965afa78db220b35944dd2d135b2a2f88460e9
bun913 <imanau03@gmail.com>
2022-08-06 13:41:48 +0900

    feat: solve abc103a

--- abc103/a/main.py
+++ abc103/a/main.py
@@ -9,6 +9,14 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+
+A = list(map(int, input().split()))
+S = sorted(A)
+
+ans = 0
+for i in range(3):
+    if i == 0:
+        continue
+    cost = abs(S[i] - S[i-1])
+    ans += cost
+print(ans)
