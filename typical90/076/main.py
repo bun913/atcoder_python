2d6336d4d2dda75a39f2e444ea290ad0a8dc4973
bun913 <imanau03@gmail.com>
2022-08-07 11:54:38 +0900

    feat: solve typ90 076

--- typical90/076/main.py
+++ typical90/076/main.py
@@ -1,14 +1,45 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+解く前のメモ
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+循環するリストの扱いの練習ぽい問題
+ケーキのある連続するピースを選ぶ方法で、選んだぶぶが全体の大きさのちょうど10分の1になるものが存在するか
 
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+ただ円形なので用意するリストは2個繋げて、最後の1個だけpopしておけば良い
+
+↓ここからTLEになるので解説見たり
+開始位置は全探索で求める
+終わり位置はsort済みの配列を渡して2分探索で求めると間に合う
+累積和を求めておけばスムーズ
+
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from itertools import accumulate
+from bisect import bisect_left
+
+N = int(input())
+A = list(map(int, input().split()))
+
+if sum(A) % 10 != 0:
+    print('No')
+    exit()
+
+target = sum(A) // 10
+
+
+# 2個繋げて最後の要素だけ不要(最後のピースから1週できるようにする)
+cakes = A + A
+
+c_sum = list(accumulate(cakes))
+
+for l in range(N):
+    if cakes[l] == target:
+        print('Yes')
+        exit()
+    # 2分探索でrを決める
+    # 要は累積話の配列からどこが一番近い数値となるか探している
+    # target + これまでの長さとすることで、どの位置に挿入すべきかわかる
+    r = bisect_left(c_sum, target + c_sum[l])
+    if c_sum[r] - c_sum[l] == target:
+        print('Yes')
+        exit()
+print('No')
