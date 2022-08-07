f7a61512f91ba9416eca2cb064f16240bda779cc
bun913 <imanau03@gmail.com>
2022-08-07 16:31:57 +0900

    feat: solve abc206c

--- abc206/c/main.py
+++ abc206/c/main.py
@@ -1,21 +1,14 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+from collections import Counter
 
+N = int(input())
+A = list(map(int, input().split()))
+
+memo = Counter(A)
+
+# 全部の組み合わせから条件を満たさないものを引いていく
+ans = N * (N-1) // 2
+for i in memo.values():
+    ans -= (i * (i-1) // 2)
+print(ans)
