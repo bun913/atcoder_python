d8c8d03362cef192e36ba7613b0951491e09fed8
bun913 <imanau03@gmail.com>
2022-08-08 05:49:31 +0900

    feat: solve abc019b

--- abc019/b/main.py
+++ abc019/b/main.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+解く前のメモ
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+まじでランレングス圧縮するだけ
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from itertools import groupby
+
+s = input()
+compressed = [k + str(len(list(g))) for k, g in groupby(s)]
+print(*compressed, sep='')
