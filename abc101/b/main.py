d9dce8cf4d9fb2adc083aece96109a188aaf0e3d
bun913 <imanau03@gmail.com>
2022-08-08 05:55:26 +0900

    feat: solve abc101b

--- abc101/b/main.py
+++ abc101/b/main.py
@@ -9,6 +9,11 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+N = input()
+n = int(N)
+ns = sum(map(int, N))
+
+if n % ns == 0:
+    print('Yes')
+    exit()
+print('No')
