f7f63f3f12a40f2ddea96a7f7bebe5f1f9faf4e4
bun913 <imanau03@gmail.com>
2022-08-09 06:18:11 +0900

    feat: solve abc100c

--- abc100/c/main.py
+++ abc100/c/main.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+解く前のメモ
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+全てのiに対して3倍はできず、どれか一つは2で割らないと操作ができないということ
+つまり2で割れる回数がボトルネックになる
+例えば何回3で掛け算をしたとしても2で割れる数が増えることはない
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+
+N = int(input())
+A = list(map(int, input().split()))
+
+ans = 0
+
+for i in range(N):
+    while A[i] % 2 == 0:
+        ans += 1
+        A[i] = A[i] // 2
+print(ans)
