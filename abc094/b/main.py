1600b88d4c08872965abfcb2d2c85a9f4d3b8651
bun913 <imanau03@gmail.com>
2022-08-11 06:57:53 +0900

    feat: solve abc094b

--- abc094/b/main.py
+++ abc094/b/main.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+Xから1番左、もしくは一番右にいくまでにかかる最小のコスト
+単純にNが100しかないのでどっちのパターンも試せば良いだけ
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+N, M, X = list(map(int, input().split()))
+A = list(map(int, input().split()))
+AD = set(A)
+
+# 右にいくパターン
+right_cost = 0
+for i in range(X, N+1):
+    if i in AD:
+        right_cost += 1
+
+# 左にいくパターン
+left_cost = 0
+for i in range(X, -1, -1):
+    if i in AD:
+        left_cost += 1
+ans = min(left_cost, right_cost)
+print(ans)
