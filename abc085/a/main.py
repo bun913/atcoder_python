fbe275d993fe76cd4e4f6fe0038be21b5746388c
bun913 <imanau03@gmail.com>
2022-08-12 05:45:18 +0900

    feat: solve abc085a

--- abc085/a/main.py
+++ abc085/a/main.py
@@ -9,6 +9,8 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+S = input()
+list_s = list(S)
+list_s[3] = '8'
+
+print(*list_s, sep='')
