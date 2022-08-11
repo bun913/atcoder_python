bf65ceb728225bc34d99364909d40970b633c9a8
bun913 <imanau03@gmail.com>
2022-08-12 06:14:38 +0900

    feat: solve abc212c

--- abc212/c/main.py
+++ abc212/c/main.py
@@ -1,21 +1,28 @@
 """
-Atcoderの問題解く用
+解く前のメモ
 
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+N,Mが最大で10**5なので全探索はできない
+正の整数というのがポイントな気がする
+Aの方は固定にして、片方は二部探索で探せば間に合うのでは
 """
+from bisect import bisect_left
 
+N, M = list(map(int, input().split()))
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+bd = sorted(B)
+
+ans = float('inf')
+
+for i in range(N):
+    a = A[i]
+    ind1 = bisect_left(bd, a)
+    # 手前の要素があれば比べる
+    if ind1-1 >= 0:
+        cand = abs(a-bd[ind1-1])
+        ans = min(ans, cand)
+    # 挿入位置の要素があれば比べる
+    if ind1 < M:
+        cand = abs(a-bd[ind1])
+        ans = min(ans, cand)
+print(ans)
