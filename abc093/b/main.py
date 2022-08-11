822bc10ebf14a5d0ec46924dddc1075dd3628ef8
bun913 <imanau03@gmail.com>
2022-08-12 05:42:36 +0900

    feat: solve abc093b

--- abc093/b/main.py
+++ abc093/b/main.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-Atcoderの問題解く用
+A, B, K = list(map(int, input().split()))
+cand1 = set([i for i in range(A, A+K) if i <= B])
+cand2 = set([i for i in range(B, B-K, -1) if A <= i])
+ans = sorted(list(cand1.union(cand2)))
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
-"""
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+for a in ans:
+    print(a)
