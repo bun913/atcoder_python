63e0e5c2e966a570a667b3f2cee9cd09e774df1b
bun913 <imanau03@gmail.com>
2022-08-11 16:42:16 +0900

    feat: solve abc093c

--- abc093/c/main.py
+++ abc093/c/main.py
@@ -1,14 +1,39 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+解く前のメモ
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+いずれの方法でも動かせる数の合計値は2だけ
+全ての数の偶奇を合わせて、あとは2ずつ増やしていけば終わりでは
+しかも合わせる基準は一番大きい数に合わせればOK
+(2ずつ足せば偶奇は絶対に崩れないのであとは数の差だけ)
 
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+しかも数が3つなので絶対に1回で偶奇は合わせれるよね
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+A, B, C = list(map(int, input().split()))
+ld = sorted([A, B, C])
+is_all_even = all([True if elm % 2 != 0 else False for elm in [A, B, C]])
+is_all_odd = all([True if elm % 2 == 0 else False for elm in [A, B, C]])
+ans = 0
+if is_all_even is False or is_all_even is False:
+    odd_count = 0
+    for elm in ld:
+        if elm % 2 != 0:
+            odd_count += 1
+    # 奇数が1つなら偶数2つの方を奇数に合わせる
+    if odd_count == 1:
+        for i in range(3):
+            elm = ld[i]
+            if elm % 2 == 0:
+                ld[i] += 1
+        ans += 1
+    elif odd_count == 2:
+        for i in range(3):
+            elm = ld[i]
+            if elm % 2 != 0:
+                ld[i] += 1
+        ans += 1
+ld = sorted(ld)
+# 全部の偶奇が揃っているならシンプル
+ans += (ld[2] - ld[1]) // 2
+ans += (ld[2] - ld[0]) // 2
+print(ans)
