6bf150ac5549e5cc5889e18fa7a7974c100f32c2
bun913 <imanau03@gmail.com>
2022-08-12 16:24:05 +0900

    feat: solve abc083c

--- abc083/c/main.py
+++ abc083/c/main.py
@@ -1,14 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+解く前のメモ
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+最大まで数列に数を詰めてあげたいのであれば、2をかけ続ければ良いのでは
+2で何回かけらえるか考えれば良さそう
 
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+最初はlogの計算でやってたけどWA出した
+よく考えれば2倍ずつ増えていくんだから普通に数え上げればよかった
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from math import log2, floor
+
+X, Y = list(map(int, input().split()))
+# div = Y // X
+# ans = floor(log2(div)) + 1
+# print(ans)
+ans = 0
+mul = 0
+cur = X
+while True:
+    n = cur * (2 ** mul)
+    if n <= Y:
+        ans += 1
+        mul += 1
+        continue
+    break
+print(ans)
