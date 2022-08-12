4b8d55b50a5dfbce0b1292118b1955c630ebeb3d
bun913 <imanau03@gmail.com>
2022-08-13 08:09:25 +0900

    feat: solve abc091b

--- abc091/b/main.py
+++ abc091/b/main.py
@@ -1,14 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+解く前のメモ
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+文字列を1言って、青いカードにその文字列が書かれていれば1円もらえる
+赤いカードに書かれていれば1円失う(これらは文字列の完全一致のみ)
+最大で何円もらうことができるか
 
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+dictに何を言えばいくら儲かるか持たせておけばそれが一番大きいものを言えば良さそう
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from collections import defaultdict
+
+N = int(input())
+memo = defaultdict(int)
+
+for _ in range(N):
+    s = input()
+    memo[s] += 1
+
+M = int(input())
+for _ in range(M):
+    s = input()
+    memo[s] -= 1
+
+sorted_memo = sorted(memo.items(), key=lambda kv: kv[1], reverse=True)
+most_big = sorted_memo[0][1]
+ans = max(most_big, 0)
+
+print(ans)
