266484dd57d1d905ca3dbe340a97fe5e51f48132
bun913 <imanau03@gmail.com>
2022-08-12 17:08:30 +0900

    feat: solve abc203c

--- abc203/c/main.py
+++ abc203/c/main.py
@@ -1,21 +1,31 @@
 """
-Atcoderの問題解く用
+解く前のメモ
 
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+村が10**100という膨大すぎる数がある
 """
+from collections import defaultdict
 
+N, K = list(map(int, input().split()))
+memo = defaultdict(int)
+
+for i in range(N):
+    A, B = list(map(int, input().split()))
+    memo[A] += B
+
+sorted_memo = sorted(memo.items())
+cur = 0
+rest = K
+
+for villege, point in sorted_memo:
+    if cur + rest < villege:
+        # 次の村に辿りつけない場合
+        print(cur+rest)
+        exit()
+    # restを減らす
+    rest = rest - (villege - cur)
+    rest += point
+    cur = villege
+    # 友達からポイントをもらう
+# 友達からポイントをもらい切った場合
+ans = cur + rest
+print(min(ans, (10 ** 100) + 1))
