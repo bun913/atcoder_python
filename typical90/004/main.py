93645e467749777fcea710a47c539f84a93c5aaa
bun913 <imanau03@gmail.com>
2022-08-14 07:50:16 +0900

    feat: solvea typ90 004

--- typical90/004/main.py
+++ typical90/004/main.py
@@ -1,14 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+解く前のメモ
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+HWが小さければそれぞれのセルに対して、毎回縦方向・横方向にみていけば良い
+が、今回はH,Wが最大で2000なのでTLEになることが予想される
 
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+そのため、あらかじめ各行・各列の和を算出しておき、それらを足し合わせた数から各セルの重複分を除けばよし
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+H, W = list(map(int, input().split()))
+rows = [list(map(int, input().split())) for _ in range(H)]
+cols = [i for i in zip(*rows)]
+
+row_sums = [sum(A) for A in rows]
+col_sums = [sum(B) for B in cols]
+
+ans = []
+for i in range(H):
+    row = []
+    for j in range(W):
+        cur = rows[i][j]
+        row_sum = row_sums[i]
+        col_sum = col_sums[j]
+        row.append(row_sum + col_sum-cur)
+    ans.append(row)
+
+for a in ans:
+    print(*a)
