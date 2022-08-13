58c37aea0c7eafe243c6b067987294380949ab2d
bun913 <imanau03@gmail.com>
2022-08-14 07:36:13 +0900

    feat: solve abc264a

---
+++ abc264/a/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Atcoderの問題解く用
+
+全ての組み合わせを列挙する方法
+list(0...8)から2つを抜き出す
+list(combinations(l, 2))
+
+bit全探索でフラグが立っているかチェックする
+if ((i >> j) & 1)
+"""
+L, R = list(map(int, input().split()))
+S = 'atcoder'
+
+ans = S[L-1:R]
+print(ans)
