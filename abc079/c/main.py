9dc25038c4a895e34c7c594dca644c3016491021
bun913 <imanau03@gmail.com>
2022-08-14 07:35:43 +0900

    feat: refactor abc079c

--- abc079/c/main.py
+++ abc079/c/main.py
@@ -4,9 +4,9 @@ Atcoderの問題解く用
 1行1列データ
 
 #str型で受け取るとき
-s = input() 
+s = input()
 #int型で受け取るとき
-s = int(input()) 
+s = int(input())
 #float型　(小数)で受け取るとき
 s = float(input())
 
@@ -18,17 +18,35 @@ l = list(map(int, input().split()))
 その他
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+from itertools import product
+
+# product使ったバージョン
 A, B, C, D = list(map(int, list(input())))
-for i in range(2 ** 3):
+flags = product([True, False], repeat=3)
+for flag in flags:
     O = []
-    # bitのフラグが立っているのを+とする
-    for j in range(3):
-        ope = '-'
-        if ((i >> j) & 1):
-            ope = '+'
-        O.append(ope)
+    for i in range(3):
+        if flag[i] is True:
+            O.append('+')
+            continue
+        O.append('-')
     formula = "{}{}{}{}{}{}{}".format(A, O[0], B, O[1], C, O[2], D)
     if eval(formula) == 7:
         ans = formula + '=7'
         print(ans)
         exit()
+
+
+# for i in range(2 ** 3):
+#     O = []
+#     # bitのフラグが立っているのを+とする
+#     for j in range(3):
+#         ope = '-'
+#         if ((i >> j) & 1):
+#             ope = '+'
+#         O.append(ope)
+#     formula = "{}{}{}{}{}{}{}".format(A, O[0], B, O[1], C, O[2], D)
+#     if eval(formula) == 7:
+#         ans = formula + '=7'
+#         print(ans)
+#         exit()
