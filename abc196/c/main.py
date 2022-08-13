472f1011ca31f8b5def53835044d9fba1e142279
bun913 <imanau03@gmail.com>
2022-08-13 17:33:39 +0900

    feat: solve abc196c

--- abc196/c/main.py
+++ abc196/c/main.py
@@ -1,21 +1,14 @@
 """
-Atcoderの問題解く用
+解く前のメモ
 
-1行1列データ
+普通にやるなら1から順に数えるけど数が大きすぎるので無理
 
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+と思ったけど半分に分けたら10**6なので間に合うという
 """
+N = input()
 
+for i in range(1, 1000001):
+    s = str(i) + str(i)
+    if int(s) > int(N):
+        print(i-1)
+        exit()
