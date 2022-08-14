dcb237ad1392e2cc52ec184c98daa9faaececb45
bun913 <imanau03@gmail.com>
2022-08-15 06:19:38 +0900

    feat:solve typ90 055

--- typical90/055/main.py
+++ typical90/055/main.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+解く前のメモ
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+Nが100なので5選ぶのを列挙しても間に合いそう
+ただAが非常に大きな数である可能性があるので、計算には工夫が必要と思われる
+全てのあまりを出して、掛け合わせて、また最後にあまりを出す必要がある
 """
-from functools import reduce, lru_cache
 from itertools import combinations
-import math
+
+N, P, Q = list(map(int, input().split()))
+A = list(map(int, input().split()))
+
+ans = 0
+
+for a, b, c, d, e in combinations(A, 5):
+    if a % P * b % P * c % P * d % P * e % P % P == Q:
+        ans += 1
+print(ans)
