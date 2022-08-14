f3585fb186edd840309d4cd556c8c01d4c863173
bun913 <imanau03@gmail.com>
2022-08-14 13:37:33 +0900

    feat: solve typ90 022

--- typical90/022/main.py
+++ typical90/022/main.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+解く前のメモ
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+見た感じ最大公約数とか最小公倍数・あまりが関わっていそうな気がする
+1辺が最大公約数の立方体ができる
 
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+切る回数は((A//G) -1)
+※ ここで少し混乱したが考えてみれば当たり前だった・・・
+だって、12の長さの棒を4の長さに切り分けるための操作回数は2回だろう
+最後に残る部分は切らないで良いのでということだった。単純乙。
+(12 // 4)- 1
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from math import gcd
+
+A, B, C = list(map(int, input().split()))
+G = gcd(A, B)
+G = gcd(G, C)
+
+a_cnt = (A // G) - 1
+b_cnt = (B // G) - 1
+c_cnt = (C // G) - 1
+ans = a_cnt + b_cnt + c_cnt
+print(ans)
