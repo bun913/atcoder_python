c7c3431193312202e307b5ce9ba8c505f1feb707
bun913 <imanau03@gmail.com>
2022-08-16 06:13:27 +0900

    feat: solve typ90 061

--- typical90/061/main.py
+++ typical90/061/main.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+解く前のメモ
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+Q個の操作
+t=3のときxi番目のカードに書かれた数を紙に書き出す
+t=3で書き出された整数を操作順に出力する
 
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+特に配列を探す部分もないのでPythonのlistなら問題なし
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+
+Q = int(input())
+X = []
+numbers = []
+
+for _ in range(Q):
+    t, x = list(map(int, input().split()))
+    if t == 3:
+        numbers.append(X[x-1])
+    if t == 1:
+        X.insert(0, x)
+    if t == 2:
+        X.append(x)
+
+for number in numbers:
+    print(number)
