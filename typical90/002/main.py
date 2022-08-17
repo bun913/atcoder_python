6faff87dd545102726d37279b7135a760663a6f7
bun913 <imanau03@gmail.com>
2022-08-18 06:20:51 +0900

    feat: solve typ90 002

--- typical90/002/main.py
+++ typical90/002/main.py
@@ -6,45 +6,29 @@
 実はNが20なので全列挙でも行けそうではある
 正しいカッコ列の判定は?
 常に何番目の文字においても(の出現回数が)の出現回数以上となっている状態かな
-
-(を1,)を2として考えてNこまでの組み合わせを全て列挙する
 """
-from itertools import permutations
-
-
-def is_valid(S: str) -> bool:
-    count1 = 0
-    count2 = 0
-    for s in S:
-        if s == '1':
-            count1 += 1
-        else:
-            count2 += 1
-        if count2 > count1:
-            return False
-    return True
-
+from itertools import product
 
 N = int(input())
 if N % 2 == 1:
     print('')
     exit()
 
-a = ['1' for _ in range(N // 2)]
-b = ['2' for _ in range(N // 2)]
-a.extend(b)
-cands = list(permutations(a, N))
-
-ans_set = set()
-for t in cands:
-    S = ''.join(list(t))
-    if is_valid(S) is False:
+for bits in product(['(', ')'], repeat=N):
+    count0 = 0
+    count1 = 0
+    is_valid = True
+    for bit in bits:
+        if bit == '(':
+            count0 += 1
+        else:
+            count1 += 1
+        if count1 > count0:
+            is_valid = False
+            break
+    if count0 != count1:
+        is_valid = False
+    if is_valid is False:
         continue
-    ans_set.add(S)
-
-ansers = sorted(list(ans_set))
-
-for S in ansers:
-    ans = S.replace('1', '(')
-    ans = ans.replace('2', ')')
+    ans = ''.join(bits)
     print(ans)
