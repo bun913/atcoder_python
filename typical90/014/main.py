7f3c785a805e251d1f0138c2fa50df06bc464cfa
bun913 <imanau03@gmail.com>
2022-08-19 05:41:43 +0900

    feat: solve typ90 014

--- typical90/014/main.py
+++ typical90/014/main.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+解く前のメモ
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+全員が別の学校に通うようにしたい
+家から学校までの距離をEiおして不便さは距離の総和
+どの生徒も学校に通う時の不便さとして考えられる最小値を求める
 
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+Nが最大100,000なのでbit全探索では無理
+ソートっぽい
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+ans = 0
+AD = sorted(A)
+BD = sorted(B)
+for i in range(N):
+    a = AD[i]
+    b = BD[i]
+    ans += abs(a-b)
+print(ans)
