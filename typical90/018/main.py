56efbcf73b6cd197213d92c24cb95522eff769e0
bun913 <imanau03@gmail.com>
2022-08-19 19:27:13 +0900

    chore: (not own self)typ90 018

--- typical90/018/main.py
+++ typical90/018/main.py
@@ -1,14 +1,21 @@
-# -*- coding: utf-8 -*-
-"""
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
-"""
-from functools import reduce, lru_cache
-from itertools import combinations
 import math
+
+
+def main():
+
+    T = int(input())
+    L, X, Y = map(int, input().split())
+    Q = int(input())
+    xx = X ** 2
+    for _ in range(Q):
+        e = int(input())
+        rad = (e % T) / T * math.pi * 2
+        yt = - L / 2 * math.sin(rad)
+        zt = L / 2 * (1 - math.cos(rad))
+        yy = xx + (yt - Y) ** 2
+        zz = yy + zt ** 2
+        print(math.degrees(math.acos(math.sqrt(yy) / math.sqrt(zz))))
+
+
+if __name__ == '__main__':
+    main()
