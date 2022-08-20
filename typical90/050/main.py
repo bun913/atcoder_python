b344d0c7929797c4fd3815db36a09c4a75eae24e
bun913 <imanau03@gmail.com>
2022-08-21 07:10:28 +0900

    feat: solve typ90 050

--- typical90/050/main.py
+++ typical90/050/main.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+解く前のメモ
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+N段を1歩、またはLずつ登れる
+移動方法が何通りあるか 10**9 + 7で割ったあまりを求める
 
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+以下解説AC
+数学的にパッとわからない問題なら、一旦DPで考えてみるのが良いかも
+1段を積むか、L段を積むかっていう問題なら積み上げていくだけだし
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+N, L = list(map(int, input().split()))
+DP = [0 for _ in range(N+1)]
+DP[0] = 1
+mod = 10 ** 9 + 7
+
+for i in range(1, N+1):
+    if i < L:
+        DP[i] = 1
+    else:
+        DP[i] = DP[i-L] + DP[i-1]
+    DP[i] = DP[i] % mod
+print(DP[-1] % mod)
