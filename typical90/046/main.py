4e121e33bcc44ec7cebe72a1855341e9c1271d86
bun913 <imanau03@gmail.com>
2022-08-20 15:45:47 +0900

    feat: solve typ90 046

--- typical90/046/main.py
+++ typical90/046/main.py
@@ -1,14 +1,38 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+解く前のメモ
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+Nが10**5まであるので、とても全部をforで回すのは無理だよねという話
+AとBが決まればCは二部探索で・・・みたいなことも厳しい
+選び方の総数というのがポイントな気がする
 
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+以下解説を見て分かった回答方法
+46の倍数になる組み合わせというのがポイント
+46の倍数になるということは各配列を46で割ったあまりを求める
+とすれば、0の数,1の数...45の数と求められる
+あとは足して46となるように全ての組み合わせを全探索すれば良いだけ
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+C = list(map(int, input().split()))
+
+ad = list(map(lambda n: n % 46, A))
+bd = list(map(lambda n: n % 46, B))
+cd = list(map(lambda n: n % 46, C))
+
+a_counts = [ad.count(n) for n in range(46)]
+b_counts = [bd.count(n) for n in range(46)]
+c_counts = [cd.count(n) for n in range(46)]
+
+ans = 0
+for a in range(46):
+    for b in range(46):
+        for c in range(46):
+            if (a+b+c) % 46 == 0:
+                a_cnt = a_counts[a]
+                b_cnt = b_counts[b]
+                c_cnt = c_counts[c]
+                ans += a_cnt * b_cnt * c_cnt
+print(ans)
