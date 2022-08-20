14e38157825e37c58d8989a30e47d291bb583dc2
bun913 <imanau03@gmail.com>
2022-08-20 15:20:41 +0900

    feat: solve typ90 044

--- typical90/044/main.py
+++ typical90/044/main.py
@@ -1,14 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+言われた取りにやるだけじゃね？
+と思ったけど、素直に右シフトさせるとdequeとかだと計算が大きすぎるのか
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+以下解説AC
+結局何回シフトさせたかメモしておけば実際にシフトさせなくてもその数が出せるよねというお話
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+N, Q = list(map(int, input().split()))
+A = list(map(int, input().split()))
+L = A[:]
+
+shift = 0
+for i in range(Q):
+    q, x, y = list(map(int, input().split()))
+    gx = (x - shift-1) % N
+    gy = (y - shift-1) % N
+
+    # 1の場合
+    if q == 1:
+        befx = L[gx]
+        befy = L[gy]
+        L[gx] = befy
+        L[gy] = befx
+        continue
+    # 2の場合右シフト
+    if q == 2:
+        shift += 1
+        continue
+    print(L[gx])
