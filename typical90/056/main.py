600297466b5d6a0c256943881e454c2d0f7d6fd9
bun913 <imanau03@gmail.com>
2022-08-23 09:26:09 +0900

    chore: typ90 056

--- typical90/056/main.py
+++ typical90/056/main.py
@@ -6,45 +6,38 @@ Nが100なのでbit全探索は無理
 
 DPでいけそう。横軸の最大値をSとして2パターンを考えていく
 """
-N, S = list(map(int, input().split()))
-DP = [[False for _ in range(S+1)] for _ in range(N)]
-AB = []
 
-for i in range(N):
-    A, B = list(map(int, input().split()))
-    AB.append((A, B))
-    for j in range(S+1):
-        # 最初の行のメモ
-        if i == 0:
-            DP[i][A] = True
-            DP[i][B] = True
-            break
-        # 上の行で商品を足せていないマスは飛ばす
-        if DP[i-1][j] is False:
-            continue
-        # 今のA・Bを足した金額がS以下ならTrueと記憶する
-        if j + A <= S:
-            DP[i][j+A] = True
-        if j + B <= S:
-            DP[i][j+B] = True
-if DP[-1][-1] is False:
-    print('Impossible')
-    exit()
-# DPを下から見て、答えを復元する必要がある
-ans = []
-cur = S
-for i in range(N-1, 0, -1):
-    A, B = AB[i]
-    if DP[i-1][cur-A] is True:
-        cur -= A
-        ans.append('A')
-        continue
-    if DP[i-1][cur-B] is True:
-        cur -= B
-        ans.append('B')
-        continue
-if cur == AB[0][0]:
-    ans.append('A')
-else:
-    ans.append('B')
-print(*reversed(ans), sep='')
+
+def main():
+    n, s = map(int, input().split())
+    dp = [[0]*(s+1) for _ in range(n+1)]
+    dp[0][0] = 1
+    ab = []
+    for i in range(n):
+        a, b = map(int, input().split())
+        ab.append((a, b))
+        for j in range(s+1):
+            if dp[i][j]:
+                if j+a <= s:
+                    dp[i+1][j+a] = 1
+                if j+b <= s:
+                    dp[i+1][j+b] = 1
+    if dp[n][s] == 0:
+        print('Impossible')
+        exit()
+    tate = n
+    yoko = s
+    ans = ''
+    while tate > 0:
+        a, b = ab[tate-1]
+        if yoko-a >= 0 and dp[tate-1][yoko-a]:
+            ans += 'A'
+            yoko -= a
+        else:
+            ans += 'B'
+            yoko -= b
+        tate -= 1
+    print(ans[::-1])
+
+
+main()
