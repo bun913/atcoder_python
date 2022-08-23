74d2439d3b54737ff40f252afe56c3910d88ac4c
bun913 <imanau03@gmail.com>
2022-08-24 06:17:48 +0900

    feat: solve typ90 064

--- typical90/064/main.py
+++ typical90/064/main.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+N個の区間。標高はAi
+地殻変動で標高がQ回変わるLからRまでV変わる
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+Nが最大で10**5で、LR間も最大同程度ある
+毎回リストの要素を全て変更していたら間に合わないのではないかと思われる
 
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+数列の階差を考える
+変わっていくのは実はL-1とRだけ
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+N, Q = list(map(int, input().split()))
+A = list(map(int, input().split()))
+s = 0
+
+B = [A[i+1] - A[i] for i in range(N-1)]
+B.append(0)
+ans = sum(map(abs, B))
+
+for i in range(Q):
+    L, R, V = list(map(int, input().split()))
+    L -= 1
+    R -= 1
+    before = abs(B[L-1]) + abs(B[R])
+    if L > 0:
+        B[L-1] += V
+    if R < N-1:
+        B[R] -= V
+    after = abs(B[L-1]) + abs(B[R])
+    ans += after-before
+    print(ans)
