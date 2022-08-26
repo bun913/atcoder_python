918dddbf7e8cee12c657741355df47d15fb57603
bun913 <imanau03@gmail.com>
2022-08-26 16:25:46 +0900

    feat: solve abc080a

--- abc080/a/main.py
+++ abc080/a/main.py
@@ -9,6 +9,6 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+N, A, B = list(map(int, input().split()))
+ans = min(B, A*N)
+print(ans)
