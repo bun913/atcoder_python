f2e87e1a62d0fb04793f52e1a4892c7be0f2582d
bun913 <imanau03@gmail.com>
2022-08-29 05:47:40 +0900

    feat: solve typ90 075

--- typical90/075/main.py
+++ typical90/075/main.py
@@ -1,14 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+xが素数でない場合: 叩かれたボールを消滅 aとbが書かれたボールを追加する
+a,bは a>=2かつb>=2から自由に選ぶ
+全てのボールに書かれている数を素数にしたい
+素因数分解だよね
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from math import log2, ceil
+
+
+def prime_factorize(n):
+    a = []
+    while n % 2 == 0:
+        a.append(2)
+        n //= 2
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            a.append(f)
+            n //= f
+        else:
+            f += 2
+    if n != 1:
+        a.append(n)
+    return a
+
+
+N = int(input())
+f = prime_factorize(N)
+L = len(f)
+ans = ceil(log2(L))
+print(ans)
