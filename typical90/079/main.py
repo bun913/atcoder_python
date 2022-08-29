a0e849d2474534f2b7710671a550967033b6d0ce
bun913 <imanau03@gmail.com>
2022-08-30 06:16:04 +0900

    feat: solve typ90 079

--- typical90/079/main.py
+++ typical90/079/main.py
@@ -1,14 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+x,yを自分,一個右,一個上,一個右斜め上を増やすか減らすか
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+AをBに一致させることができるか
+最小の操作回数も
 
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+以下解説AC
+一個のますずつ合わせていく。そうすると2回目にそのますを含むような範囲でも
+それ以上はそのマスの数を変えられないはず。AとBの一致するますを一致させるように数をこなしていくことで
+答えが一致するはず
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+H, W = list(map(int, input().split()))
+
+A = [list(map(int, input().split())) for _ in range(H)]
+B = [list(map(int, input().split())) for _ in range(H)]
+
+cnt = 0
+for i in range(0, H-1):
+    for j in range(0, W-1):
+        t = B[i][j] - A[i][j]
+        cnt += abs(t)
+        A[i][j] += t
+        A[i+1][j] += t
+        A[i][j+1] += t
+        A[i+1][j+1] += t
+
+if A == B:
+    print('Yes')
+    print(cnt)
+    exit()
+print('No')
