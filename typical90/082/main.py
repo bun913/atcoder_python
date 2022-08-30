673c2252dbb6be56617ea1b0a7ddafde96209860
bun913 <imanau03@gmail.com>
2022-08-31 05:57:09 +0900

    chore: (not myself)typ90 082

--- typical90/082/main.py
+++ typical90/082/main.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+黒板に整数xをx回書く
+黒板以下かれている文字の個数を数える(桁数)
+3..5なら3を3回,4を4回,5を5回合計12桁の文字
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+L,Rが10**18なので、とてもforでは間に合わない
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+L, R = list(map(int, input().split()))
+mod = 10 ** 9 + 7
+
+
+def count_number(number: int) -> int:
+    digit, cnt = 1, 0
+    while True:
+        under = 10 ** (digit - 1) - 1
+        top = min(number, 10 ** digit - 1)
+        cnt += ((top * (top + 1) - under * (under+1)) // 2) * digit
+
+        if number <= 10 ** digit - 1:
+            return cnt
+        digit += 1
+
+
+print((count_number(R) - count_number(L-1)) % (10 ** 9 + 7))
