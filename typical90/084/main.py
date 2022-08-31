5c236317179098521193b9e5534a946cbad7f111
bun913 <imanau03@gmail.com>
2022-09-01 06:05:44 +0900

    feat: solve typ90 084

--- typical90/084/main.py
+++ typical90/084/main.py
@@ -1,14 +1,33 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+l文字目からr文字目の区間に○とxが両方含まれる
+lは決めうちにして、lの文字に応じてrを2分探索で探せばよいのでは
+○と罰の位置をあらかじめメモしておけばl移行に初めて出てくるo(またはx)の位置をすぐ出せそう
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from bisect import bisect_left
+
+N = int(input())
+S = list(input())
+o_list = []
+x_list = []
+
+for i, s in enumerate(S):
+    if s == 'o':
+        o_list.append(i)
+        continue
+    x_list.append(i)
+
+ans = 0
+for left in range(N-1):
+    cur = S[left]
+    search_list = o_list
+    if cur == 'o':
+        search_list = x_list
+    r_ind = bisect_left(search_list, left)
+    # 一番右側だったらoかxが見つからなかった
+    if r_ind == len(search_list):
+        continue
+    right = search_list[r_ind]
+    cnt = (N-1) - right + 1
+    ans += cnt
+print(ans)
