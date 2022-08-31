249f40926f41f23dc76c6ef9b89f29e952bc7554
bun913 <imanau03@gmail.com>
2022-08-31 19:23:49 +0900

    feat: solve nik2019 a

--- nikkei2019-final/a/main.py
+++ nikkei2019-final/a/main.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+連続するK個の区画を選んだときそれら区画の資源の総和
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+累積和の問題っぽい
+累積和を出しておく
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from itertools import accumulate
+
+N = int(input())
+A = list(map(int, input().split()))
+accum = list(accumulate(A))
+accum.insert(0, 0)
+
+max_list = [0 for _ in range(N)]
+# iが連続する何個の区画をみるかを示す
+for i in range(1, N+1):
+    ans = 0
+    # jが実際に右端を決めるものとなっている
+    for j in range(i, N+1):
+        ans = max(ans, accum[j]-accum[j-i])
+    print(ans)
