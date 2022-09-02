b0d14069fe475e1f72ee4d4b559462a496bae43c
bun913 <imanau03@gmail.com>
2022-09-03 07:06:26 +0900

    feat: solve abc168c

--- abc168/c/main.py
+++ abc168/c/main.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+H時で何度動いているか
+2辺の長さと成す角で求める余弦定理かな
 """
-from functools import reduce
-from itertools import combinations
-import math
+from math import radians, cos, sqrt
+
+A, B, H, M = list(map(int, input().split()))
+short_deg = H * 30
+long_deg = M * 5.5
+rad = radians(max(long_deg, short_deg) - min(long_deg, short_deg))
+ans = sqrt(A**2 + B**2 - (2 * A * B * cos(rad)))
+print(ans)
