19a61e80211520fd5cc3a73968519272e844300a
bun913 <imanau03@gmail.com>
2022-09-02 16:59:22 +0900

    feat: solve abc129c

--- abc129/c/main.py
+++ abc129/c/main.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+Nまで辿り着ける移動方法は何通りか
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+N, M = list(map(int, input().split()))
+A = set()
+[A.add(int(input())) for _ in range(M)]
+
+DP = [1] + list(range(N))
+mod = 1000000007
+
+for i in range(1, N + 1):
+    if i in A:
+        DP[i] = 0
+        continue
+    # 1段前
+    cur = 0
+    if (i - 1) not in A and DP[i - 1] != 0:
+        cur = DP[i - 1]
+    # 2段前からも来れる
+    if (i - 2) >= 0 and (i - 2) not in A and DP[i - 2] != 0:
+        cur += DP[i - 2]
+    DP[i] = cur % mod
+print(DP[-1] % mod)
