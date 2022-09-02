3455b330534cc57bff9366312cfd76ade3f38537
bun913 <imanau03@gmail.com>
2022-09-02 14:26:07 +0900

    feat: solve abc103c

--- abc103/c/main.py
+++ abc103/c/main.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+最小公倍数-1が答えじゃないか
+最小公倍数の時に全てのあまりが0になる
+てことはそこから-1すれば各要素とのあまりが最大になりそう
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+RE・・・
+よく考えればaが10**5なので最悪LCMがとんでもない数になる
+この考え方だけ使って、あとはai-1を足し合わせた方が良さそう
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from functools import reduce
+
+N = int(input())
+A = list(map(int, input().split()))
+
+ans = reduce(lambda bef, cur: bef + (cur - 1), A, 0)
+print(ans)
