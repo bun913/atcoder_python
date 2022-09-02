d26a9e2a2011aef6633a6f74bce101bb530906db
bun913 <imanau03@gmail.com>
2022-09-02 11:06:05 +0900

    feat: solve arc113b

--- arc113/b/main.py
+++ arc113/b/main.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+まずBとCだけ計算してAを何回階乗するかということを求めるか
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+1の位だけ見てやれば良いAの1のくらいは1~9
+1~9の規則性をあらかじめメモしておけば良いのでは
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+A, B, C = list(map(int, input().split()))
+period_memo = dict([(i, []) for i in range(1, 10)])
+# 1~9の階乗した結果の1のくらいの規則性をメモ
+for i in range(1, 10):
+    for j in range(1, 11):
+        s = str(pow(i, j))
+        about1 = int(s[-1])
+        if about1 in period_memo[i]:
+            break
+        period_memo[i].append(about1)
+a_about1 = int(str(A)[-1])
+if a_about1 == 0:
+    print(0)
+    exit()
+mod = len(period_memo[a_about1])
+period = pow(B, C, mod)
+ans = period_memo[a_about1][period - 1]
+print(ans)
