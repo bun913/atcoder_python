ec1a84856290ba368b7e88a226243ee5b235a0e0
bun913 <imanau03@gmail.com>
2022-09-04 06:51:19 +0900

    feat: solve abc267c

--- abc267/c/main.py
+++ abc267/c/main.py
@@ -1,31 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-かける数は1からMって決まっているから全てのパターンを用意できないかな
+普通にやるとTLEになるのは分かっている
+前の計算結果とかぶっている区間が多いからそれを利用して解くっぽい
+ということまでわかるが実装(式変形)がわからない
 
-まず普通に実装して答えがどんな感じで決まるのか見てみよう
-とりあえず最初の要素と最後の要素の差が大きいところだけ見てみようか
+以下解説AC
+- 前の区間と今の区間を引き算してあげると式が見えてくる(等比数列の和の出し方みたいだなと思った)
 """
+from itertools import accumulate
+from functools import reduce
+
 N, M = list(map(int, input().split()))
 A = list(map(int, input().split()))
+# N項までの累積和を用意
+T = [0] + list(accumulate(A))
+# 前の答えを利用するので最初の答えだけ手で出しておく
+s1 = 0
+for i, v in enumerate(A[:M]):
+    s1 += (i + 1) * v
+ans_list = [s1]
 
-ans = -(10**17)
-max_left_ind = 0
-max_dif = -(10**17)
-bef_left = A[0]
-for i in range(N):
-    # オーバーするなら終わり
-    if i + M - 1 > N - 1:
+ans = s1
+for i in range(1, N + 1):
+    # M先まで見れなくなったら終了
+    if i + M > N:
         break
-    tmp = A[i : i + M]
-    right_ind = i + M - 1
-    right = A[i + M - 1]
-    if right_ind - M > 0:
-        bef_left = A[right_ind - M]
-    max_dif = max(max_dif, right - bef_left)
-    if max_dif == right - bef_left:
-        max_left_ind = i
-tmp = A[max_left_ind : max_left_ind + M]
-ans = 0
-for i, v in enumerate(tmp):
-    ans += (i + 1) * v
+    bef_ans = ans_list[i - 1]
+    cur_total = T[i + M - 1] - T[i - 1]
+    # 前の答え - (N+M-1項までの和) + (今回右端に出てきた数*)が今回の求める値となる
+    cur_ans = bef_ans + (A[i + M - 1] * M) - cur_total
+    ans_list.append(cur_ans)
+    ans = max(ans, cur_ans)
 print(ans)
