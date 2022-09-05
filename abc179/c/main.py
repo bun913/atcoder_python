eec75369ed73d51d5bc93d3c38d8d292e3a44b73
bun913 <imanau03@gmail.com>
2022-09-06 06:35:45 +0900

    feat: solve abc179c

--- abc179/c/main.py
+++ abc179/c/main.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+一見A*B+Cを全て探索しないようにいけないように感じる
+A,B,Cは全て正の整数の組なのでA*B=Nとなるような約数以外を列挙すれば良いのでは
+約数列挙の別バージョンで約数じゃ無い時に数え上げればよし
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from math import sqrt
+
+N = int(input())
+
+ans = 0
+for A in range(1, N):
+    # AxBは確実にAの倍数になる
+    # つまりAの倍数個分だけ答えがある
+    cnt = (N - 1) // A
+    ans += cnt
+print(ans)
