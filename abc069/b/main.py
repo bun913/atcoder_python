1ab7a83d246b53b566605116eb66f8f1317a1e28
bun913 <imanau03@gmail.com>
2022-09-07 07:59:29 +0900

    feat: solve abc069b

--- abc069/b/main.py
+++ abc069/b/main.py
@@ -9,6 +9,6 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+S = input()
+tmp = S[1:-1]
+print("{}{}{}".format(S[0], len(tmp), S[-1]))
