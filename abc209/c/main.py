56e9e3c9d2251bb90835fbf0716375fc3fe7e50f
bun913 <imanau03@gmail.com>
2022-09-07 07:56:12 +0900

    feat: solve abc209c

--- abc209/c/main.py
+++ abc209/c/main.py
@@ -1,21 +1,9 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
-
+N = int(input())
+C = list(map(int, input().split()))
+CD = sorted(C)
+ans = 1
+for i in range(N):
+    ans = ans * max(0, CD[i] - i) % 1000000007
+print(ans)
