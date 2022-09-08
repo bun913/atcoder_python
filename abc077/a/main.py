3a0602d8b823ffcb303427ef0cf6896a0085f21f
bun913 <imanau03@gmail.com>
2022-09-08 18:58:12 +0900

    feat: add abc077a

--- abc077/a/main.py
+++ abc077/a/main.py
@@ -9,6 +9,12 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+
+top = input()
+under = input()
+
+ans = top[::-1] == under
+if ans is True:
+    print("YES")
+    exit()
+print("NO")
