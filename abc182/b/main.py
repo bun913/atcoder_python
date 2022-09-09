7e04d3d569a2c5430911f49bffe1c0273058cb65
bun913 <imanau03@gmail.com>
2022-09-09 10:47:14 +0900

    feat: solve abc182b

--- abc182/b/main.py
+++ abc182/b/main.py
@@ -9,6 +9,18 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+# Aがたかだか1000以下と決まっているので全部調べれば良い
+N = int(input())
+A = list(map(int, input().split()))
+
+max_cnt = 0
+ans = 0
+for k in range(2, 1001):
+    cnt = 0
+    for a in A:
+        if a % k == 0:
+            cnt += 1
+    if cnt >= max_cnt:
+        max_cnt = cnt
+        ans = k
+print(ans)
