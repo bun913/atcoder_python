75612d186209d6ade6c292cef2d31102a90657aa
bun913 <imanau03@gmail.com>
2022-09-10 10:45:33 +0900

    feat: solve abc181c

--- abc181/c/main.py
+++ abc181/c/main.py
@@ -2,13 +2,30 @@
 """
 Atcoderの問題解く用
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+かたむきが同じか判定する
+y1/x1 = y2/x2
+x2*y1 = x1*y2
 """
-from functools import reduce, lru_cache
 from itertools import combinations
-import math
+
+
+def is_on_line(a, b, c):
+    ax, ay = a[0], a[1]
+    bx, by = b[0], b[1]
+    cx, cy = c[0], c[1]
+    fx, fy = bx - ax, by - ay
+    sx, sy = cx - ax, cy - ay
+    if fx * sy == sx * fy:
+        return True
+    return False
+
+
+N = int(input())
+XY = [list(map(int, input().split())) for _ in range(N)]
+
+for a, b, c in combinations([i for i in range(N)], 3):
+    # 3点が同じか判定する
+    if is_on_line(XY[a], XY[b], XY[c]) is True:
+        print("Yes")
+        exit()
+print("No")
