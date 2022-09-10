d9cec50562d27359fa26784a70354ec0dc08bd6a
bun913 <imanau03@gmail.com>
2022-09-10 11:22:30 +0900

    feat: solve abc012c

--- abc012/c/main.py
+++ abc012/c/main.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+九九を計算して、それぞれの結果を辞書に入れておけば？
+一発で求められそう
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from collections import defaultdict
+
+N = int(input())
+
+memo = defaultdict(list)
+
+for l in range(1, 10):
+    for r in range(1, 10):
+        k = l * r
+        v = "{} x {}".format(l, r)
+        memo[k].append(v)
+key = 2025 - N
+
+for v in memo[key]:
+    print(v)
