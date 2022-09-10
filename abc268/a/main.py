84338de8c198cb2ca378dde6b6921ea5db7c3eba
bun913 <imanau03@gmail.com>
2022-09-11 05:56:34 +0900

    feat: solve abc268a

---
+++ abc268/a/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Atcoderの問題解く用
+
+全ての組み合わせを列挙する方法
+list(0...8)から2つを抜き出す
+list(combinations(l, 2))
+
+bit全探索でフラグが立っているかチェックする
+if ((i >> j) & 1)
+"""
+L = list(map(int, input().split()))
+print(len(set(L)))
