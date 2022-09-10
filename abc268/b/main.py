554c93c1c2a1f633c78f445bd52b7b32fdd30642
bun913 <imanau03@gmail.com>
2022-09-11 05:56:44 +0900

    feat: solve abc268b

---
+++ abc268/b/main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Atcoderの問題解く用
+
+全ての組み合わせを列挙する方法
+list(0...8)から2つを抜き出す
+list(combinations(l, 2))
+
+bit全探索でフラグが立っているかチェックする
+if ((i >> j) & 1)
+"""
+S = input()
+T = input()
+is_start = T.startswith(S)
+
+if is_start is True:
+    print("Yes")
+    exit()
+print("No")
