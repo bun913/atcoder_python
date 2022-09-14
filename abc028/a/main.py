1c8d8dcf2bfb3616f2a0191196e7cecfbc432834
bun913 <imanau03@gmail.com>
2022-09-15 05:33:31 +0900

    feat: solve abc028a

--- abc028/a/main.py
+++ abc028/a/main.py
@@ -9,6 +9,13 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+N = int(input())
+
+if N <= 59:
+    print("Bad")
+elif N <= 89:
+    print("Good")
+elif N <= 99:
+    print("Great")
+else:
+    print("Perfect")
