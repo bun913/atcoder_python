377b9107b0a4c9cbdac9883e342d9a0b1e6553d2
bun913 <imanau03@gmail.com>
2022-09-15 06:11:43 +0900

    feat: solve abc223c

--- abc223/c/main.py
+++ abc223/c/main.py
@@ -1,45 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+N本の導火線を一直線に繋げている
+左端と右端から火をつけて左から何センチのところで火がぶつかるか
 
-1行1列データ
-
-#str型で受け取るとき
-s = input()
-#int型で受け取るとき
-s = int(input())
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+二つの火がぶつかる時間は片側から火をつけて燃え尽きるまでの時間の半分になる
 """
-from functools import reduce
-from itertools import combinations
-import math
 
-# 素数判定
+N = int(input())
+A = []
+B = []
+for _ in range(N):
+    a, b = list(map(int, input().split()))
+    A.append(a)
+    B.append(b)
+t = 0.0
 
+for i in range(N):
+    t += A[i] / B[i]
+t /= 2
 
-def is_prime(n: int) -> bool:
-    for i in range(2, int(math.sqrt(n))+1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def permutation(n, r):
-    return math.factorial(n) // math.factorial(n-r)
-
-
-def combination(n, r):
-    return permutation(n, r) // math.factorial(r)
+ans = 0
+for i in range(N):
+    if A[i] / B[i] <= t:
+        ans += A[i]
+        t -= A[i] / B[i]
+    else:
+        ans += B[i] * t
+        break
+print(ans)
