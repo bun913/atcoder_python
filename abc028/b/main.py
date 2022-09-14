567c9ea951e3487af475f1176307a0dcf604b4ab
bun913 <imanau03@gmail.com>
2022-09-15 05:33:39 +0900

    feat: solvea abc028b

--- abc028/b/main.py
+++ abc028/b/main.py
@@ -9,6 +9,9 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from collections import Counter
+
+S = input()
+c = Counter(S)
+
+print(c["A"], c["B"], c["C"], c["D"], c["E"], c["F"], sep=" ", end="\n")
