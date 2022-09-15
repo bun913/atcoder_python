8de77365816a40afcec8586fd7d7d58db89b6da8
bun913 <imanau03@gmail.com>
2022-09-16 05:34:53 +0900

    feat: solve abc060c

--- abc060/c/main.py
+++ abc060/c/main.py
@@ -1,21 +1,14 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+N, T = list(map(int, input().split()))
+L = list(map(int, input().split()))
 
+ans = 0
+
+for i in range(N - 1):
+    t1 = L[i]
+    t2 = L[i + 1]
+    dif = t2 - t1
+    ans += min(dif, T)
+
+print(ans + T)
