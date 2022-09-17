c1715798c3ec8414a02e8abb2f91f1d25e5dd918
bun913 <imanau03@gmail.com>
2022-09-18 06:59:50 +0900

    feat: solve abc269a

---
+++ abc269/a/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Atcoderの問題解く用
+
+全ての組み合わせを列挙する方法
+list(0...8)から2つを抜き出す
+list(combinations(l, 2))
+
+bit全探索でフラグが立っているかチェックする
+if ((i >> j) & 1)
+"""
+a, b, c, d = list(map(int, input().split()))
+print((a + b) * (c - d))
+print("Takahashi")
