a0c684fae780f44c01098a90f83ec9226fb00cf0
bun913 <imanau03@gmail.com>
2022-09-18 15:18:22 +0900

    refactor: fix abc269b

--- abc269/b/main.py
+++ abc269/b/main.py
@@ -3,18 +3,17 @@
 """
 S = [list(input()) for _ in range(10)]
 A = 11
-B = 11
+B = -1
 C = 11
-D = 11
+D = -1
 
 for i in range(10):
-    s = S[i]
-    if s.count("#") > 0:
-        A = min(A, i + 1)
-        B = max(A, i + 1)
-        for j in range(10):
-            if s[j] == "#":
-                C = min(C, j + 1)
-                D = max(C, j + 1)
+    for j in range(10):
+        s = S[i][j]
+        if s == "#":
+            A = min(A, i + 1)
+            B = max(B, i + 1)
+            C = min(C, j + 1)
+            D = max(D, j + 1)
 print(A, B)
 print(C, D)
