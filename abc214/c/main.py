e64a68f50eff99bd03cc378dfa2ce0db37f2f42e
bun913 <imanau03@gmail.com>
2022-09-19 05:45:15 +0900

    feat: solve abc214c

--- abc214/c/main.py
+++ abc214/c/main.py
@@ -1,21 +1,21 @@
 """
-Atcoderの問題解く用
+1以上N以下の人は時刻tに宝石をもらうとSi時間後にi+1の人に渡す
+高橋はTiにi番目のすぬけくんに宝石を渡す
 
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+つまり前の人から宝石をもらう時間か、高橋くんからもらう時間か早い方を答えるということ
+回した場合と高橋からもらう場合の最小値を求める
 """
+N = int(input())
+S = list(map(int, input().split()))
+T = list(map(int, input().split()))
 
+# 高橋くんから宝石がもらえる時間を初期値としてメモ
+results = [T[i] for i in range(N)]
+# 2周回して各人の最小値の結果を反映してあげる
+for i in range(2 * N):
+    cur = i % N
+    bef = (i + N - 1) % N
+    # 前の人から渡ってくる時間をcandとして、高橋くんからもらえる時間との最小値を取る
+    cand = results[bef] + S[bef]
+    results[cur] = min(cand, results[cur])
+print(*results, sep="\n")
