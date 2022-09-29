ccd825d9b662c989887444c49865575808abe08d
bun913 <imanau03@gmail.com>
2022-09-30 06:18:59 +0900

    feat: solve abc210c

--- abc210/c/main.py
+++ abc210/c/main.py
@@ -1,21 +1,21 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+N個のキャンディ
+色が最大10**9個
+連続して並んだキャンディをK個もらえる
 """
+from collections import defaultdict
 
+N, K = list(map(int, input().split()))
+C = list(map(int, input().split()))
+memo = defaultdict(int)
+for i in range(K):
+    memo[C[i]] += 1
+ans = len(set(memo))
+
+for i in range(K, N):
+    memo[C[i]] += 1
+    memo[C[i - K]] -= 1
+    if memo[C[i - K]] <= 0:
+        del memo[C[i - K]]
+    ans = max(ans, len(memo))
+print(ans)
