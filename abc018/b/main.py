fd5d0d732d13069d082551957bfe57e675b037bb
bun913 <imanau03@gmail.com>
2022-10-05 05:24:45 +0900

    feat: solve abc004b

--- abc018/b/main.py
+++ abc018/b/main.py
@@ -5,3 +5,14 @@
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+
+S = input()
+N = int(input())
+
+ans = list(S)
+for _ in range(N):
+    l, r = list(map(int, input().split()))
+    t = ans[l - 1 : r]
+    t = t[::-1]
+    ans = ans[: l - 1] + t + ans[r:]
+print(*ans, sep="")
