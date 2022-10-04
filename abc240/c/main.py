3d0cfe85b02b923bf6d485f5159255496e604210
bun913 <imanau03@gmail.com>
2022-10-05 05:51:24 +0900

    feat: solve abc240c

--- abc240/c/main.py
+++ abc240/c/main.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+N回のジャンプの後に座標Xの位置にいるようにすることはできるか
+Nが100なのでbit全探索は無理
+座標Xを限界としたDPの表を作る
 """
-from functools import reduce
-from itertools import combinations
-import math
+N, X = list(map(int, input().split()))
+dp = [[False for _ in range(X + 1)] for _ in range(N + 1)]
+dp[0][0] = True
+AB = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(1, N + 1):
+    a, b = AB[i - 1]
+    for j in range(X + 1):
+        if j >= a:
+            if dp[i - 1][j - a] is True:
+                dp[i][j] = True
+        if j >= b:
+            if dp[i - 1][j - b] is True:
+                dp[i][j] = True
+print("Yes" if dp[-1][-1] is True else "No")
