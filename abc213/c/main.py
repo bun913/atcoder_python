ec60fa6f83a48e7bc985ad1d03cb165542a9bd77
bun913 <imanau03@gmail.com>
2022-10-06 06:36:15 +0900

    feat: solve abc213c

--- abc213/c/main.py
+++ abc213/c/main.py
@@ -1,21 +1,21 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+Nは最低でも10**5ある
+数の書かれたカードを含まない行があるとき、その行のカードを消して、上へ詰める
+数の書かれたカードを含まない列があるとき、その列のカードをのぞき、左へ詰める
+座表圧縮とかいう奴と同じ結果になるのでは
 """
+H, W, N = list(map(int, input().split()))
+AB = [list(map(int, input().split())) for _ in range(N)]
+A = list(map(lambda ab: ab[0], AB))
+B = list(map(lambda ab: ab[1], AB))
 
+a_temp = sorted(set(A))
+ad = {v: i for i, v in enumerate(a_temp)}
+ans_a = list(map(lambda v: ad[v] + 1, A))
+
+b_temp = sorted(set(B))
+bd = {v: i for i, v in enumerate(b_temp)}
+ans_b = list(map(lambda v: bd[v] + 1, B))
+
+for i in range(N):
+    print(ans_a[i], ans_b[i])
