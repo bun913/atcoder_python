ddb2d6b02f976562d3aa22d29b0918b064625a4b
bun913 <imanau03@gmail.com>
2022-10-06 06:16:49 +0900

    feat: solve abc011b

--- abc011/b/main.py
+++ abc011/b/main.py
@@ -2,6 +2,8 @@
 """
 解く前のメモ用
 """
-from sys import setrecursionlimit
-
-setrecursionlimit(10**7)
+S = input()
+L = list(S)
+L[0] = L[0].upper()
+ans = [L[0]] + [s.lower() for s in L[1:]]
+print(*ans, sep="")
