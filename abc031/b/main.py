4f7eb2ad8dae96e62131c5438b3be22a0009f270
bun913 <imanau03@gmail.com>
2022-10-07 05:21:39 +0900

    feat: solve abc031b

--- abc031/b/main.py
+++ abc031/b/main.py
@@ -5,3 +5,12 @@
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+L, H = list(map(int, input().split()))
+N = int(input())
+A = [int(input()) for _ in range(N)]
+
+for a in A:
+    if a > H:
+        print(-1)
+        continue
+    print(max(0, L - a))
