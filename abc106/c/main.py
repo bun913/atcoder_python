5578e329a0f59497869b8a4975807c9213ce5b8c
bun913 <imanau03@gmail.com>
2022-10-07 05:50:01 +0900

    feat: solve abc106c

--- abc106/c/main.py
+++ abc106/c/main.py
@@ -1,14 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+5000兆日語と決まっている
+2以上の5000兆乗は当然Kの限界である10**18より大きいはず
+では最初の文字が1以上か2以上かで場合分けすれば良いのでは
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+S = input()
+K = int(input())
+
+# 最初の文字列が1意外ならその数字で決まり
+if not S.startswith("1"):
+    print(S[0])
+    exit()
+# 1だけで構成されている場合1
+if len(set(S)) == 1:
+    print(1)
+    exit()
+# # SがK文字以下の場合
+# if K <= len(S):
+#     print(S[K - 1])
+#     exit()
+# 1から始まるけどそれ以降に2以上の文字が存在する
+for i, s in enumerate(S):
+    if i + 1 == K:
+        print(s)
+        exit()
+    # K文字目以下で1以外の数字が見つかった場合
+    if s != "1":
+        print(s)
+        exit()
