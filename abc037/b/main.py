005e528cfec8a3f217bb9ada1d97ee1b69a4e0c7
bun913 <imanau03@gmail.com>
2022-10-10 06:43:10 +0900

    feat: solve abc037b

--- abc037/b/main.py
+++ abc037/b/main.py
@@ -5,3 +5,12 @@
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+
+N, Q = list(map(int, input().split()))
+A = [0 for _ in range(N)]
+
+for _ in range(Q):
+    L, R, T = list(map(int, input().split()))
+    for i in range(L - 1, R):
+        A[i] = T
+print(*A, sep="\n")
