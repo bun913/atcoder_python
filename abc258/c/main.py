3af884ee4db7cf34b8cd1137e2d53b3bd0df5b59
bun913 <imanau03@gmail.com>
2022-10-10 06:33:35 +0900

    feat: solve abc258c

--- abc258/c/main.py
+++ abc258/c/main.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+queryが10**5で操作回数のxが10**5あるので、普通にqueでやると間に合わない
+操作は末尾を削除して先頭に挿入するだけなので、ローテーションしている
+実際に動かさなくても何回操作したか分かれば答えを導ける
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+N, Q = list(map(int, input().split()))
+S = input()
+
+start = 0
+for _ in range(Q):
+    t, x = list(map(int, input().split()))
+    if t == 1:
+        # 添字のスタート位置を左にずらしていしくイメージ
+        start -= x
+        # N以上はローテーションするためmodを取る
+        start %= N
+        continue
+    # スタート位置がrだけ右にずれている
+    i = (start + (x - 1)) % N
+    print(S[i])
