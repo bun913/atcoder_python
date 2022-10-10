a820c3a781bee16fdc2051ffbb634aa4655ac7bd
bun913 <imanau03@gmail.com>
2022-10-11 05:46:52 +0900

    feat: solve abc076b

--- abc076/b/main.py
+++ abc076/b/main.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
 """
 解く前のメモ用
+Nが10しかないので全探索できそう
 """
 from sys import setrecursionlimit
+from itertools import product
 
 setrecursionlimit(10**7)
+
+N = int(input())
+K = int(input())
+
+ans = 10**19
+for opes in product(["A", "B"], repeat=N):
+    cur = 1
+    for o in opes:
+        if o == "A":
+            cur *= 2
+            continue
+        cur += K
+    ans = min(ans, cur)
+print(ans)
