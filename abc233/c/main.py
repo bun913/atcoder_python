b1107f2c8fc85f63d6951b575732e0a0886e89ab
bun913 <imanau03@gmail.com>
2022-10-11 05:57:01 +0900

    feat: solve abc233c

--- abc233/c/main.py
+++ abc233/c/main.py
@@ -1,75 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input()
-#int型で受け取るとき
-s = int(input())
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+取り出したボールの総積がXになるような取り出し方
+書かれた数が同じでも区別する
+袋に入っているボールの数は10**5を越えない
 """
-from functools import reduce
-from itertools import combinations
-import math
+N, X = list(map(int, input().split()))
+ans_lis = [1]
 
-# 素数判定
-
-
-def is_prime(n: int) -> bool:
-    """
-    素数判定(LogN)
-    """
-    for i in range(2, int(math.sqrt(n))+1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def permutation(n, r) -> int:
-    """
-    順列の計算
-    """
-    return math.factorial(n) // math.factorial(n-r)
-
-
-def combination(n, r) -> int:
-    """
-    組み合わせ数の計算
-    """
-    return permutation(n, r) // math.factorial(r)
-
-
-def rot_n(s, n) -> str:
-    """
-    アルファベットsをn文字分後にずらす
-    """
-    answer = ''
-    for letter in s:
-        answer += chr(ord('A') + (ord(letter)-ord('A')+n) % 26)
-    return answer
-
-
-def derot_n(s, n) -> str:
-    """
-    アルファベットsをn文字分戻す
-    """
-    answer = ''
-    for letter in s:
-        answer += chr(ord('Z') - (ord('Z')-ord(letter)+n) % 26)
-
-    return answer
+for _ in range(N):
+    A = list(map(int, input().split()))
+    L = A.pop(0)
+    ans_lis = [x * y for x in ans_lis for y in A]
+print(ans_lis.count(X))
