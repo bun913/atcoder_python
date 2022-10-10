0577b64486cbde6bf32bca55248efed125e64232
bun913 <imanau03@gmail.com>
2022-10-10 09:34:46 +0900

    feat: solve abc272b

--- abc272/b/main.py
+++ abc272/b/main.py
@@ -3,5 +3,21 @@
 解く前のメモ用
 """
 from sys import setrecursionlimit
+from itertools import combinations
 
 setrecursionlimit(10**7)
+
+N, M = list(map(int, input().split()))
+memo = dict((i + 1, set()) for i in range(N))
+
+for _ in range(M):
+    L = list(map(int, input().split()))
+    n, L = L[0], L[1:]
+    for l, r in combinations(L, 2):
+        memo[l].add(r)
+        memo[r].add(l)
+for k, v in memo.items():
+    if len(v) != N - 1:
+        print("No")
+        exit()
+print("Yes")
