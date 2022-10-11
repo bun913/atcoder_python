b4f482796f6977196907daedd050af279a53a60b
bun913 <imanau03@gmail.com>
2022-10-12 05:48:41 +0900

    feat: solve abc238c(AC with commentary)

--- abc238/c/main.py
+++ abc238/c/main.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+f(x) = x以下の正の整数でxと桁数が同じものの数
+f(1) + f(2) + .. + f(N)を998244353で割ったあまりを求める
 """
-from functools import reduce
-from itertools import combinations
-import math
+N = int(input())
+
+
+def s(A: int, B: int) -> int:
+    """
+    AからBまでの和を求める(和の公式)
+    """
+    return (B - A + 1) * (A + B) // 2
+
+
+ans = 0
+for x in range(1, 19):
+    if 10**x <= N:
+        ans += s(1, 9 * 10 ** (x - 1))
+        ans %= 998244353
+    else:
+        ans += s(1, N - 10 ** (x - 1) + 1)
+        ans %= 998244353
+        break
+print(ans)
