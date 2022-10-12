453225b2ab486b46053f56ce4d7eeb38cbec4068
bun913 <imanau03@gmail.com>
2022-10-13 07:49:56 +0900

    feat: solve abc211c

--- abc211/c/main.py
+++ abc211/c/main.py
@@ -1,21 +1,24 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+このうち8文字を選んで、左から順にchokudaiとする方法の全通り数
+Sが10**5なのでcombinationsとかは無理
+DPでi文字めまでを使ってchokudaiのk文字目までを作る方法が何通りあるか
 """
-
+S = input()
+N = len(S)
+S = "?" + S
+T = "?chokudai"
+# 表を作成する
+dp = [[0 for _ in range(9)] for _ in range(N + 1)]
+mod = 10**9 + 7
+# 初期化(すぐにわかるところを埋める)
+for i in range(N + 1):
+    dp[i][0] = 1
+# 表の小さい方から答えに辿り着くまで埋める
+for i in range(1, N + 1):
+    for k in range(1, 9):
+        if S[i] == T[k]:
+            dp[i][k] = dp[i - 1][k] + dp[i][k - 1]
+        else:
+            dp[i][k] = dp[i - 1][k]
+        dp[i][k] %= mod
+print(dp[-1][-1])
