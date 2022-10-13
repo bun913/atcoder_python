6084e70dda714763df107a870bdf4793e906f4e6
bun913 <imanau03@gmail.com>
2022-10-14 06:33:37 +0900

    feat: solve abc227c(with commentary)

--- abc227/c/main.py
+++ abc227/c/main.py
@@ -1,45 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+答えは2**63以下
 
-1行1列データ
-
-#str型で受け取るとき
-s = input()
-#int型で受け取るとき
-s = int(input())
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+A**3<=Nが絶対条件（Aが最小だから)
 """
-from functools import reduce
-from itertools import combinations
-import math
+N = int(input())
 
-# 素数判定
+a = 1
+ans = 0
 
-
-def is_prime(n: int) -> bool:
-    for i in range(2, int(math.sqrt(n))+1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def permutation(n, r):
-    return math.factorial(n) // math.factorial(n-r)
-
-
-def combination(n, r):
-    return permutation(n, r) // math.factorial(r)
+while a**3 <= N:
+    # 初期値
+    b = a
+    while a * (b**2) <= N:
+        # cはb以上である必要があるので-bして+1
+        ans += N // (a * b) - b + 1
+        b += 1
+    a += 1
+print(ans)
