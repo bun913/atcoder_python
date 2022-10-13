69e07f8dfc2bca3c31eef4777569b8b0dcffc993
bun913 <imanau03@gmail.com>
2022-10-14 06:06:23 +0900

    feat: solve abc032a

--- abc032/a/main.py
+++ abc032/a/main.py
@@ -2,6 +2,19 @@
 """
 解く前のメモ用
 """
+from numpy import lcm, ceil
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+
+a = int(input())
+b = int(input())
+n = int(input())
+
+lc = lcm(a, b)
+if n <= lc:
+    print(lc)
+    exit()
+mul = int(ceil(n / lc))
+ans = lc * mul
+print(ans)
