897e6984e7bcdbef26e585e07517505626a18475
bun913 <imanau03@gmail.com>
2022-10-15 06:48:34 +0900

    feat: solve abc064c

--- abc064/c/main.py
+++ abc064/c/main.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+400で割った時の商でレートを決めることができそう
+3200以上は最後にまとめて計算するのが良さそう
+3200以上は書いてある色以外も選ぶことができるとのこと
 """
-from functools import reduce
-from itertools import combinations
-import math
+N = int(input())
+A = list(map(int, input().split()))
+
+below_reds = list(filter(lambda x: x <= 3199, A))
+over_reds = list(filter(lambda x: x > 3199, A))
+
+min_set = set()
+# 好きな色を選ぶことは考えない
+for r in below_reds:
+    c = r // 400
+    min_set.add(c)
+# 3200以上で好きに選ぶ
+min_ans = len(min_set)
+max_ans = min_ans
+for r in over_reds:
+    # 3199以下が一人もいない場合時だけ最小値を増加させる
+    if min_ans == 0:
+        min_ans += 1
+    max_ans += 1
+print(min_ans, max_ans)
