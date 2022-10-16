1194dc94171fe4c45343da265cd7439ff4355332
bun913 <imanau03@gmail.com>
2022-10-17 06:26:39 +0900

    feat: solve arc046a

--- arc046/a/main.py
+++ arc046/a/main.py
@@ -5,3 +5,11 @@
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+N = int(input())
+
+for i in range(1, 8):
+    for n in range(1, 10):
+        N -= 1
+        if N == 0:
+            print(str(n) * i)
+            exit()
