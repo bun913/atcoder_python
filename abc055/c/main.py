e606080a0ba307af77f97c99467a63556023cb2b
bun913 <imanau03@gmail.com>
2022-10-17 06:42:29 +0900

    feat: solve abc055c

--- abc055/c/main.py
+++ abc055/c/main.py
@@ -1,21 +1,18 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+cが2つでSが作れる
+Sccという組みを最大で何個作れるか
 """
+S, C = list(map(int, input().split()))
+ans = 0
 
+# まずSが1個でもある場合その数だけ作る
+set_cnt = min(S, C // 2)
+ans += set_cnt
+S -= set_cnt
+C -= 2 * set_cnt
+if C < 2:
+    print(ans)
+    exit()
+# あとはCを4個消費すれば1個Sが作れる
+ans += C // 4
+print(ans)
