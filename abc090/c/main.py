5c474212341182976278891bdbd8ba94ec640448
bun913 <imanau03@gmail.com>
2022-10-17 06:26:27 +0900

    feat: solve abc090c

--- abc090/c/main.py
+++ abc090/c/main.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+無限に広がる空間
+一旦手で作成してみてヒントを探す
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+
+N, M = list(map(int, input().split()))
+n = N - 2
+m = M - 2
+ans = abs(n * m)
+print(ans)
