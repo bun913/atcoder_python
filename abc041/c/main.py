c077be9a8e3eb2e9a725049e336455f2c5be04ee
bun913 <imanau03@gmail.com>
2022-10-18 06:01:50 +0900

    feat: solve abc041c(use normal sort)

--- abc041/c/main.py
+++ abc041/c/main.py
@@ -2,9 +2,8 @@
 """
 解く前のメモ用
 """
-from numpy import argsort
-
 N = int(input())
 A = list(map(int, input().split()))
-ans_list = argsort(A)
-[print(a + 1) for a in reversed(ans_list)]
+ad = [(t, i + 1) for i, t in enumerate(A)]
+ad = sorted(ad, reverse=True)
+[print(i) for _, i in ad]
