5ecb012dc8ea1c54c29c6f493543e9b10e5ccb47
bun913 <imanau03@gmail.com>
2022-10-19 06:31:18 +0900

    feat: solve abc043b

--- abc043/b/main.py
+++ abc043/b/main.py
@@ -2,6 +2,16 @@
 """
 解く前のメモ用
 """
-from sys import setrecursionlimit
+from collections import deque
 
-setrecursionlimit(10**7)
+q = deque([])
+S = input()
+
+for s in S:
+    if s == "B":
+        if q:
+            q.pop()
+        continue
+    q.append(s)
+ans = "".join(list(q))
+print(ans)
