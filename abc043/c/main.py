ef9a7996666c4a679ea1eee3151332b8faed6671
bun913 <imanau03@gmail.com>
2022-10-19 06:25:31 +0900

    feat: solve abc043c

--- abc043/c/main.py
+++ abc043/c/main.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+答えの値から離れ場離れるほどコストは大きくなる
+普通に答えの幅が小さいので全探索で間に合いそう
 """
-from sys import setrecursionlimit
+N = int(input())
+A = list(map(int, input().split()))
+ad = sorted(A)
 
-setrecursionlimit(10**7)
+ans = float("inf")
+for n in range(-100, 101):
+    s = 0
+    for a in ad:
+        s += (a - n) ** 2
+    ans = min(ans, s)
+
+print(ans)
