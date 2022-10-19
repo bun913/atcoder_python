210142acc3172b7c18b7976fb82d0482c356710d
bun913 <imanau03@gmail.com>
2022-10-20 07:30:45 +0900

    feat: solve past2 cd

--- past202004-open/c/main.py
+++ past202004-open/c/main.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
 """
 解く前のメモ用
+Xの書かれている黒いますはX
+2<=j<=2N-2に対してXは書かれていない黒います、左上、上、右上にXが書かれていればます
 """
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+
+N = int(input())
+S = [list(input()) for _ in range(N)]
+
+for i in range(N - 1, 0, -1):
+    for j in range(2, 2 * N - 1):
+        x = i - 1
+        y = j - 1
+        s = set([S[x + 1][y - 1], S[x + 1][y], S[x + 1][y + 1]])
+        should_ope = "X" in s and S[x][y] == "#"
+        if should_ope is True:
+            S[x][y] = "X"
+# 答えの出力
+[print("".join(S[i])) for i in range(N)]
--- past202004-open/d/main.py
+++ past202004-open/d/main.py
@@ -1,7 +1,28 @@
 # -*- coding: utf-8 -*-
 """
 解く前のメモ用
+
+bit全探索で求められそう
+3文字以下の部分文字列を全てつくればいけそうだけど
 """
 from sys import setrecursionlimit
+from itertools import product
 
 setrecursionlimit(10**7)
+
+S = input()
+limit = len(S)
+
+ans_set = set()
+for l in range(limit):
+    for r in range(l, l + 3):
+        part = S[l : r + 1]
+        for comb in product([True, False], repeat=len(part)):
+            s = ["" for _ in range(len(part))]
+            for i, bl in enumerate(comb):
+                if bl is True:
+                    s[i] = "."
+                else:
+                    s[i] = part[i]
+            ans_set.add("".join(s))
+print(len(ans_set))
