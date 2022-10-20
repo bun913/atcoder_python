2b756225acf9675a3f81edd95b538baadcf8f3a3
bun913 <imanau03@gmail.com>
2022-10-21 06:24:57 +0900

    feat: solve abc026b

--- abc026/b/main.py
+++ abc026/b/main.py
@@ -3,5 +3,17 @@
 解く前のメモ用
 """
 from sys import setrecursionlimit
+from math import pi
 
 setrecursionlimit(10**7)
+
+N = int(input())
+R = sorted([int(input()) for _ in range(N)], reverse=True)
+mul = 0
+for i in range(N):
+    if i % 2 == 0:
+        mul += R[i] ** 2
+        continue
+    mul -= R[i] ** 2
+ans = mul * pi
+print(ans)
