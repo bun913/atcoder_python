f4958ab1a28d3bcbddee802b5597227725a9f002
bun913 <imanau03@gmail.com>
2022-10-21 06:47:44 +0900

    feat: solve abc198c

--- abc198/c/main.py
+++ abc198/c/main.py
@@ -1,21 +1,12 @@
 """
 Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+from math import sqrt, ceil
 
+R, X, Y = list(map(int, input().split()))
+abs_dis = sqrt(X**2 + Y**2)
+ans = ceil(abs_dis / R)
+# コーナーケース
+if ans == 1 and abs_dis != R:
+    ans += 1
+print(ans)
