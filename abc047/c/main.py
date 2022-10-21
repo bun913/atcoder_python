9fbd87fb80d4edcff409f88713c277d63892262f
bun913 <imanau03@gmail.com>
2022-10-22 06:38:46 +0900

    feat:solve abc047c

--- abc047/c/main.py
+++ abc047/c/main.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 解く前のメモ用
+何個同じ色のグループが続いているかという問題では
+ランレングス圧縮してみるか
 """
-from sys import setrecursionlimit
+from itertools import groupby
 
-setrecursionlimit(10**7)
+S = input()
+compressed = [k for k, g in groupby(S)]
+print(len(compressed) - 1)
