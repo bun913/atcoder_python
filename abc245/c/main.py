28732e3bbcece34e98ad2d9d4e4bf8c3c9bb765a
bun913 <imanau03@gmail.com>
2022-10-23 07:09:26 +0900

    chore: tle abc245c

--- abc245/c/main.py
+++ abc245/c/main.py
@@ -1,14 +1,42 @@
 # -*- coding: utf-8 -*-
 """
 Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+これは単純にその時々の最適解（できれば差が小さい方）を選び続けるだけでは
 """
-from functools import reduce
-from itertools import combinations
-import math
+N, K = list(map(int, input().split()))
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+cands = []
+for i in range(N - 1):
+    a = A[i]
+    b = B[i]
+    nexa = A[i + 1]
+    nexb = B[i + 1]
+    # 初回のみ
+    if i == 0:
+        if min([abs(a - nexa), abs(a - nexb), abs(b - nexa), abs(b - nexb)]) > K:
+            print("No")
+            exit()
+        if abs(a - nexa) <= K:
+            cands.append((a, nexa))
+        if abs(a - nexb) <= K:
+            cands.append((a, nexb))
+        if abs(b - nexa) <= K:
+            cands.append((b, nexa))
+        if abs(b - nexb) <= K:
+            cands.append((b, nexb))
+        continue
+    # 前からみた全ての候補を確認する
+    next_cands = []
+    for prev, cur in cands:
+        if abs(cur - nexa) <= K:
+            next_cands.append((cur, nexa))
+        if abs(cur - nexb) <= K:
+            next_cands.append((cur, nexb))
+    cands = next_cands
+    # ここで次の候補がなくなっていれば終了
+    if len(cands) == 0:
+        print("No")
+        exit()
+print("Yes")
