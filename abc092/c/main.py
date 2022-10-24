cecd8c52ce38af335f5c65edbcb96462d1ee6606
bun913 <imanau03@gmail.com>
2022-10-25 06:00:57 +0900

    feat: solve abc092c

--- abc092/c/main.py
+++ abc092/c/main.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+本来の予定と使う項目が似通っているから
+そこから差分を式で一般化すれば良さそう
+前の結果とかから差分で導き出すパターンのやつか
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+差分は以下になるので、これを本来の予定から引いてやれば良いはず
+abs(Ai-1 + Ai) + abs(Ai-Ai+1) - abs(Ai-1-Ai+1)
 """
-from functools import reduce
-from itertools import combinations
-import math
+N = int(input())
+A = list(map(int, input().split()))
+A.insert(0, 0)
+A.append(0)
+org = 0
+for i in range(1, N + 2):
+    org += abs(A[i - 1] - A[i])
+
+for i in range(1, N + 1):
+    dif = abs(A[i - 1] - A[i]) + abs(A[i] - A[i + 1]) - abs(A[i - 1] - A[i + 1])
+    print(org - dif)
