4fd41642ce859ccb7201aeefee5946ac118b1cb8
bun913 <imanau03@gmail.com>
2022-10-26 06:16:53 +0900

    feat: solve abc003c

--- abc003/c/main.py
+++ abc003/c/main.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 """
 解く前のメモ用
+全部のビデオは見れないからi番目に大きいビデオを見続ければ良いのでは
 """
-from sys import setrecursionlimit
+from decimal import Decimal
 
-setrecursionlimit(10**7)
+N, K = list(map(int, input().split()))
+R = list(map(int, input().split()))
+rd = sorted(R, reverse=True)
+# 1の場合は最大のものを選ぶ
+rates = rd[:K]
+ans = Decimal("0")
+for r in reversed(rates):
+    ans = (ans + Decimal(str(r))) / Decimal("2")
+print(ans)
