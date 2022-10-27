02ae4d89fbbe139b4d66f390a533a8101798e698
bun913 <imanau03@gmail.com>
2022-10-28 06:24:01 +0900

    feat: solve abc245c

--- abc245/c/main.py
+++ abc245/c/main.py
@@ -1,42 +1,32 @@
 # -*- coding: utf-8 -*-
 """
 Atcoderの問題解く用
-これは単純にその時々の最適解（できれば差が小さい方）を選び続けるだけでは
 """
-N, K = list(map(int, input().split()))
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+a.insert(0, 0)
+b.insert(0, 0)
+ap = [False for _ in range(n + 1)]  # (X1, X2...Xi)まで考慮したとき、Xi = Aiとしてよいか
+bp = [False for _ in range(n + 1)]  # (X1, X2...Xi)まで考慮したとき、Xi = Biとしてよいか
 
-cands = []
-for i in range(N - 1):
-    a = A[i]
-    b = B[i]
-    nexa = A[i + 1]
-    nexb = B[i + 1]
-    # 初回のみ
-    if i == 0:
-        if min([abs(a - nexa), abs(a - nexb), abs(b - nexa), abs(b - nexb)]) > K:
-            print("No")
-            exit()
-        if abs(a - nexa) <= K:
-            cands.append((a, nexa))
-        if abs(a - nexb) <= K:
-            cands.append((a, nexb))
-        if abs(b - nexa) <= K:
-            cands.append((b, nexa))
-        if abs(b - nexb) <= K:
-            cands.append((b, nexb))
-        continue
-    # 前からみた全ての候補を確認する
-    next_cands = []
-    for prev, cur in cands:
-        if abs(cur - nexa) <= K:
-            next_cands.append((cur, nexa))
-        if abs(cur - nexb) <= K:
-            next_cands.append((cur, nexb))
-    cands = next_cands
-    # ここで次の候補がなくなっていれば終了
-    if len(cands) == 0:
-        print("No")
-        exit()
-print("Yes")
+ap[1] = True
+bp[1] = True
+
+for i in range(2, n + 1):
+    if ap[i - 1]:
+        if abs(a[i - 1] - a[i]) <= k:
+            ap[i] = True
+        if abs(a[i - 1] - b[i]) <= k:
+            bp[i] = True
+
+    if bp[i - 1]:
+        if abs(b[i - 1] - a[i]) <= k:
+            ap[i] = True
+        if abs(b[i - 1] - b[i]) <= k:
+            bp[i] = True
+
+if ap[n] or bp[n]:
+    print("Yes")
+else:
+    print("No")
