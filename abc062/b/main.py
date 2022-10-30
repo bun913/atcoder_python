a727b59525f4b13def8119d2cfc8dc24883e2e5a
bun913 <imanau03@gmail.com>
2022-10-31 05:45:06 +0900

    feat: solve abc062b

--- abc062/b/main.py
+++ abc062/b/main.py
@@ -2,6 +2,12 @@
 """
 解く前のメモ用
 """
-from sys import setrecursionlimit
-
-setrecursionlimit(10**7)
+H, W = list(map(int, input().split()))
+A = [list(input()) for _ in range(H)]
+for i in range(H):
+    A[i].insert(0, "#")
+    A[i].append("#")
+A.append(["#"] * (W + 2))
+A.insert(0, ["#"] * (W + 2))
+for i in range(len(A)):
+    print(*A[i], sep="")
