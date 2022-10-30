bac0d7a9e667e2a5c6d7b5b3530057c49ccb3c3e
bun913 <imanau03@gmail.com>
2022-10-31 06:21:16 +0900

    feat: solve abc275c

--- abc275/c/main.py
+++ abc275/c/main.py
@@ -2,9 +2,7 @@
 """
 解く前のメモ用
 """
-import numpy as np
 from itertools import combinations
-from math import isclose
 
 S = [list(input()) for _ in range(9)]
 
@@ -17,25 +15,10 @@ for x in range(9):
 # あとはこの4点が正方形か判定する
 ans = 0
 for pattern in combinations(porns, 4):
-    # 4つの角が全て等しく、4つの変が全て等しい
-    a = np.array(pattern[0])
-    b = np.array(pattern[1])
-    c = np.array(pattern[2])
-    d = np.array(pattern[3])
-    # どれかのベクトルの大きさが等しい
-    abcd = isclose(np.linalg.norm(b - a), np.linalg.norm(d - c))
-    acbd = isclose(np.linalg.norm(c - a), np.linalg.norm(d - b))
-    adbc = isclose(np.linalg.norm(d - a), np.linalg.norm(c - b))
-    # 角度が等しい
-    ab = b - a
-    cd = d - c
-    tan1 = isclose(np.arctan2(ab[0], ab[1]), np.arctan2(cd[0], cd[1]))
-    ac = c - a
-    bd = d - b
-    tan2 = isclose(np.arctan2(ac[0], ac[1]), np.arctan2(bd[0], bd[1]))
-    ad = d - a
-    bc = c - b
-    tan3 = isclose(np.arctan2(ad[0], ad[1]), np.arctan2(bc[0], bc[1]))
-    if (abcd and tan1) or (acbd and tan2) or (adbc and tan3):
+    # 4辺の長さが等しく対角線の長さも等しいと正方形
+    dis_of_sides = set()
+    for p1, p2 in combinations(pattern, 2):
+        dis_of_sides.add((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+    if len(dis_of_sides) == 2:
         ans += 1
 print(ans)
