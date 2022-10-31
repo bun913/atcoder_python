1ac8e309ccffe2d83e94e60b5231c55cd6b0729f
bun913 <imanau03@gmail.com>
2022-11-01 05:30:24 +0900

    feat: solve abc057b

--- abc057/b/main.py
+++ abc057/b/main.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+N,Mが小さいので二部探索は作らなくてもO(N**2)で十分
 """
-from sys import setrecursionlimit
+N, M = list(map(int, input().split()))
+students = [list(map(int, input().split())) for _ in range(N)]
+points = [list(map(int, input().split())) for _ in range(M)]
 
-setrecursionlimit(10**7)
+for a, b in students:
+    ans = float("inf")
+    min_dis = float("inf")
+    for i, cd in enumerate(points):
+        c, d = cd
+        dis = abs(c - a) + abs(d - b)
+        if dis < min_dis:
+            min_dis = dis
+            ans = i + 1
+    print(ans)
