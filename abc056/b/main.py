fb2221701b311e5631c569f13f9f9d731c1a3587
bun913 <imanau03@gmail.com>
2022-11-03 15:55:23 +0900

    feat: solve abc056b

--- abc056/b/main.py
+++ abc056/b/main.py
@@ -2,6 +2,14 @@
 """
 解く前のメモ用
 """
-from sys import setrecursionlimit
-
-setrecursionlimit(10**7)
+W, a, b = list(map(int, input().split()))
+a_end = a + W
+b_start = b
+b_end = b + W
+# bが右に離れている
+if b_start >= a_end:
+    print(b_start - a_end)
+elif a >= b_end:
+    print(a - b_end)
+else:
+    print(0)
