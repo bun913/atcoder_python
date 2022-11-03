95e1487f836f8a511b272f7e20f59f2729521047
bun913 <imanau03@gmail.com>
2022-11-03 16:12:29 +0900

    wip: abc069c

--- abc069/c/main.py
+++ abc069/c/main.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+Aの要素を自由に並び替える
+a[i]とa[i+1]の積が4の倍数になる
+- 4の倍数には何をかけてもOK
+- 2の倍数と2の倍数のペアはOK
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from functools import reduce
+
+N = int(input())
+A = list(map(int, input().split()))
+cnt_four, cnt_even, others = 0, 0, 0
+for a in A:
+    if a % 2 == 0 and a % 4 != 0:
+        cnt_even += 1
+    elif a % 4 == 0:
+        cnt_four += 1
+    else:
+        others += 1
+if cnt_four + cnt_even >= N // 2:
+    print("Yes")
+else:
+    print("No")
