47e40e258838ccb7fb9b81e28b2a90f163de5151
bun913 <imanau03@gmail.com>
2022-11-05 06:32:28 +0900

    feat: solve abc052b

--- abc052/b/main.py
+++ abc052/b/main.py
@@ -2,6 +2,14 @@
 """
 解く前のメモ用
 """
-from sys import setrecursionlimit
-
-setrecursionlimit(10**7)
+N = int(input())
+S = input()
+x = 0
+ans = -1000
+for s in S:
+    if s == "I":
+        x += 1
+    else:
+        x -= 1
+    ans = max(ans, x)
+print(max(ans, 0))
