73671fd2b2c550d53b49dc6bc7fabbc4ebef7bb4
bun913 <imanau03@gmail.com>
2022-11-12 21:40:14 +0900

    feat: solve abc021b

--- abc021/b/main.py
+++ abc021/b/main.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 """
 解く前のメモ用
+aから出発してK回寄り道してbに到着
+道路を通る回数が最小となる経路
 """
-from sys import setrecursionlimit
+from collections import Counter
 
-setrecursionlimit(10**7)
+N = int(input())
+a, b = list(map(int, input().split()))
+K = int(input())
+P = list(map(int, input().split()))
+P.insert(0, a)
+P.append(b)
+c = Counter(P)
+if c.most_common()[0][1] >= 2:
+    print("NO")
+    exit()
+print("YES")
