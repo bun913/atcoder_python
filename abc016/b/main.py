d4d0db738731a1d138c7698a353522252b599496
bun913 <imanau03@gmail.com>
2022-11-13 09:48:40 +0900

    feat: solve abc016b

--- abc016/b/main.py
+++ abc016/b/main.py
@@ -2,6 +2,14 @@
 """
 解く前のメモ用
 """
-from sys import setrecursionlimit
-
-setrecursionlimit(10**7)
+A, B, C = list(map(int, input().split()))
+can_plus = A + B == C
+can_minus = A - B == C
+if can_plus is True and can_minus is True:
+    print("?")
+elif can_plus is True:
+    print("+")
+elif can_minus is True:
+    print("-")
+else:
+    print("!")
