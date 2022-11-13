a8e586b773e95a9eb15f66fb1be4af119098481e
bun913 <imanau03@gmail.com>
2022-11-14 06:32:20 +0900

    feat: solve abc010b

--- abc010/b/main.py
+++ abc010/b/main.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 解く前のメモ用
+好き・嫌いのパターン
+好き・嫌い・大好きのパターン
 """
-from sys import setrecursionlimit
-
-setrecursionlimit(10**7)
+N = int(input())
+ans = 0
+A = list(map(int, input().split()))
+for a in A:
+    t = a
+    while t % 2 == 0 or t % 3 == 2:
+        t -= 1
+        ans += 1
+print(ans)
