cdf54751ab544ec956d19a33504c6b27e513852d
bun913 <imanau03@gmail.com>
2023-05-22 06:23:49 +0900

    feat: solve abc280b

--- abc280/b/main.py
+++ abc280/b/main.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+i番目までの和を保持しておき、それとの差を出していけば良い
 """
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+
+N = int(input())
+S = list(map(int, input().split()))
+A = [S[0]]
+
+for i in range(1, N):
+    dif = S[i] - S[i-1]
+    A.append(dif)
+print(*A)
