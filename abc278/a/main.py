65d82828b6c8fb843be85288d10978544c177184
bun913 <imanau03@gmail.com>
2023-05-22 06:03:08 +0900

    chore: solve abc280a

--- abc278/a/main.py
+++ abc278/a/main.py
@@ -5,3 +5,12 @@
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+
+N, K = list(map(int, input().split()))
+A = list(map(int, input().split()))
+ans = A[:]
+
+for _ in range(K):
+    ans.pop(0)
+    ans.append(0)
+print(*ans)
