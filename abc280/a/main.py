73be7c845e0e7a67e4d7579f2a74024fb9f8aa9e
bun913 <imanau03@gmail.com>
2023-05-22 06:23:41 +0900

    feat: solve abc280a

--- abc280/a/main.py
+++ abc280/a/main.py
@@ -5,3 +5,9 @@
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+H, W = list(map(int, input().split()))
+ans = 0
+for _ in range(H):
+    S = list(input())
+    ans += S.count("#")
+print(ans)
