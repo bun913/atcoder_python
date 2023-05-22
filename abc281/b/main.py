96ffc4582c2c4f893ba2d8899203d173ec542427
bun913 <imanau03@gmail.com>
2023-05-23 06:19:25 +0900

    feat: add abc281b

--- abc281/b/main.py
+++ abc281/b/main.py
@@ -5,3 +5,21 @@
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+S = input()
+N = len(S)
+
+if N != 8:
+    print("No")
+    exit()
+# 1文字目が大文字でない場合はNG
+if S[0].isupper() is False:
+    print("No")
+    exit()
+# 2から7文字目が数値に変換できるか
+if S[1:7].isdecimal() and S[1] != "0":
+    if S[-1].isupper():
+        print("Yes")
+        exit()
+    print("No")
+    exit()
+print("No")
