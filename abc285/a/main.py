b452a7753f80d988da51c10ae865c09a34774cbf
bun913 <imanau03@gmail.com>
2023-06-06 22:25:31 +0900

    feat: abc285 b

--- abc285/a/main.py
+++ abc285/a/main.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 解く前のメモ用
+直接つながっている数字は2*a or 2*a + 1
 """
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+a, b = list(map(int, input().split()))
+if 2 * a == b or 2 * a + 1 == b:
+    print("Yes")
+    exit()
+print("No")
--- abc285/b/main.py
+++ abc285/b/main.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
 """
 解く前のメモ用
+i=1,2,...N-1それぞれについて以下の条件を満たす最大の負ではない整数lを求める
+l +i <= N
+全ての 1 <= k <=lを満たす整数kについて Sk != Si+k
 """
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+N = int(input())
+S = input()
+
+# iはずらす数
+for i in range(1, N):
+    # lは1から
+    ans = 0
+    for l in range(0, N-i):
+        if S[l] != S[l+i]:
+            ans += 1
+        else:
+            break
+    print(ans)
