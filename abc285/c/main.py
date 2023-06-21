d37438d48e2ea3b97a99ba10bbc405012d7cb6b3
bun913 <imanau03@gmail.com>
2023-06-22 04:57:34 +0900

    feat: solve abc285

--- abc285/c/main.py
+++ abc285/c/main.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+SがAから始まる26進数と考えると計算できる
+Sの長さは高々16桁とわかっている
 """
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+
+
+def alpha2num(alpha):
+    num = 0
+    for index, item in enumerate(list(alpha)):
+        num += pow(26, len(alpha)-index-1)*(ord(item)-ord('A')+1)
+    return num
+
+
+S = input()
+e = 0
+ans = 0
+for s in S[::-1]:
+    # アルファベットを数字に変換する
+    n = alpha2num(s)
+    ans += n * (26 ** e)
+    e += 1
+print(ans)
