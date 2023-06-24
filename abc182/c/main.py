a4b3dd9ec859a976c28717344b418797c444e349
bun913 <imanau03@gmail.com>
2023-06-24 18:09:29 +0900

    feat: abc182c の時直し

--- abc182/c/main.py
+++ abc182/c/main.py
@@ -1,37 +1,23 @@
-# -*- coding: utf-8 -*-
 """
-Nは各桁に0が
-Nの桁数をkとする。Nの桁数を0以上k個未満消して、残った桁をそのままの順序で結合
-その数で3の倍数にしたい
-
-解答前のメモ
-3の倍数になるということは各桁が足して３になるということ
-各桁を3で割ったあまりを足し合わせれば良いのでは
-算数の知識で求めることもできそうだが、今回は最大でも18桁のNなのでギリギリ全探索でも間に合いそう
+0が含まれないNが与えられる
+Nの桁を任意の数を消して（消さないでもよい）3の倍数を作ることができるか
+たしか、各桁の数字を足して3の倍数になればよかったはず
+Nがたかだか10の18**なので全探索でも良さそう
 """
 from itertools import product
 
-N = int(input())
-L = list(map(int, list(str(N))))
-
-limit = 10**18 + 5
-ans = limit
-for flags in product([True, False], repeat=len(L)):
-    delete_cnt = 0
-    # 全部の桁を消せないのでスキップ
-    if flags.count(True) == len(flags):
+N = input()
+ans = 50
+for use_flags in product([True, False], repeat=len(N)):
+    num_list = []
+    for i, is_use in enumerate(use_flags):
+        if is_use is True:
+            num_list.append(int(N[i]))
+    if len(num_list) == 0:
         continue
-    # 残った数の各桁の和が3の倍数か調べる
-    rest_sum = 0
-    for i in range(len(L)):
-        is_delete = flags[i]
-        if is_delete is True:
-            delete_cnt += 1
-            continue
-        rest_sum += L[i] % 3
-    if rest_sum % 3 == 0:
-        ans = min(ans, delete_cnt)
-if ans == limit:
+    if sum(num_list) % 3 == 0:
+        ans = min(ans, len(N) - len(num_list))
+if ans == 50:
     print(-1)
     exit()
 print(ans)
