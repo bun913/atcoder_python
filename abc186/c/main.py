83e84d76f5459fe7f6038a557986c7a35d4ac1d8
bun913 <imanau03@gmail.com>
2023-06-24 13:17:42 +0900

    feat: 186c 別解

--- abc186/c/main.py
+++ abc186/c/main.py
@@ -1,14 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
-Nが10**5でかつせいぜい6桁くらいしかないので言われた通りにやっても間に合いそう
+次は10進数を8進数に変換するメソッドを自作した場合
 """
 N = int(input())
 ans = 0
+
+
+def get_oct(x: int) -> str:
+    n = x
+    s = ''
+    while n > 0:
+        first = n % 8
+        s = str(first) + s
+        n //= 8
+    return s
+
+
+def judge(s: str) -> bool:
+    if s.find("7") == -1:
+        return True
+
+
+ans = 0
 for i in range(1, N+1):
-    oct_num = oct(i)
-    oct_str = str(oct_num)[2:]
+    oc = get_oct(i)
     str_num = str(i)
-    if oct_str.find("7") == -1 and str_num.find("7") == -1:
+    if judge(oc) is True and judge(str_num) is True:
         ans += 1
 print(ans)
