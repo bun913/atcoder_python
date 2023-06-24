50c5ffbd088b5a58d6497f2fb6a6b4ba89f93d47
bun913 <imanau03@gmail.com>
2023-06-24 13:07:05 +0900

    feat: abc186c

--- abc186/c/main.py
+++ abc186/c/main.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 解く前のメモ用
+Nが10**5でかつせいぜい6桁くらいしかないので言われた通りにやっても間に合いそう
 """
-from sys import setrecursionlimit
-
-setrecursionlimit(10**7)
+N = int(input())
+ans = 0
+for i in range(1, N+1):
+    oct_num = oct(i)
+    oct_str = str(oct_num)[2:]
+    str_num = str(i)
+    if oct_str.find("7") == -1 and str_num.find("7") == -1:
+        ans += 1
+print(ans)
