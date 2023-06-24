70cb2717064117a1211cfd0659ddb3b36ed65c89
bun913 <imanau03@gmail.com>
2023-06-24 18:29:02 +0900

    feat: solve abc183c

--- abc183/c/main.py
+++ abc183/c/main.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+メタ的な読みとなるがNが8の段階で全探索だなと気づく
+これは全ての並び替えのパターンを持っておけば良さそう
 """
-from sys import setrecursionlimit
+from itertools import permutations
 
-setrecursionlimit(10**7)
+N, K = map(int, input().split())
+T = [list() for _ in range(N)]
+for i in range(N):
+    T[i] = list(map(int, input().split()))
+
+ans = 0
+for orders in permutations(list(range(N))):
+    if orders[0] != 0:
+        continue
+    circles = list(orders)
+    circles.append(0)
+    sum_time = 0
+    for cur_ind in range(N):
+        next_ind = cur_ind + 1
+        t = T[circles[cur_ind]][circles[next_ind]]
+        sum_time += t
+    if sum_time == K:
+        ans += 1
+print(ans)
