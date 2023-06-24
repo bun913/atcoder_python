815629450fae579c791456e2839bf10341be41a4
bun913 <imanau03@gmail.com>
2023-06-24 14:05:59 +0900

    feat: solve 最小公倍数

--- abc148/c/main.py
+++ abc148/c/main.py
@@ -1,19 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+AとBの最小公倍数を求める
 """
-from functools import reduce, lru_cache
-from itertools import combinations
 from math import gcd
-
-A, B = list(map(int, input().split()))
-_gcd = gcd(A, B)
-lcm = _gcd * (A // _gcd) * (B // _gcd)
+A, B = map(int, input().split())
+gc = gcd(A, B)
+lcm = A * B // gc
 print(lcm)
