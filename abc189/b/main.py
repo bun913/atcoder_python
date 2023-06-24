f7d65a04d72e2a295b0a53e58170ca39b7914c4b
bun913 <imanau03@gmail.com>
2023-06-24 14:43:14 +0900

    feat: 189b

--- abc189/b/main.py
+++ abc189/b/main.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+Pは全て%の整数値でくるので、整数で管理しが方が良さそう
 """
-from sys import setrecursionlimit
-
-setrecursionlimit(10**7)
+N, X = map(int, input().split())
+rest = X * 100
+for i in range(N):
+    v, p = map(int, input().split())
+    rest -= v * p
+    if rest < 0:
+        print(i+1)
+        exit()
+print(-1)
