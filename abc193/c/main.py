b1c339cc5d890ba1bd62023069336bb293e569e1
bun913 <imanau03@gmail.com>
2023-06-25 10:37:28 +0900

    feat: solve abc193c

--- abc193/c/main.py
+++ abc193/c/main.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+整数Nがくる。
+1以上Nの整数のうち、2以上の整数a,bを用いてa**bと表せないものの数
+これもa**bってことは少なくともbは2以上の整数であることがわかる。
+ということはどんなにあれでも N = a * aとなるはず（a=1は除く）
+であれば、これもNまで調べなくても、Nの平方根まで調べればいい。
+a,bは2以上の整数という制約がある
 """
-from sys import setrecursionlimit
-
-setrecursionlimit(10**7)
+N = int(input())
+limit = int(N**0.5) + 1
+can_pow = 0
+visited = set()
+for a in range(2, limit):
+    b = 2
+    while a**b <= N:
+        if a ** b not in visited:
+            can_pow += 1
+        visited.add(a**b)
+        b += 1
+print(N-can_pow)
