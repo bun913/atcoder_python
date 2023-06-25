36be212a540de6165350a1ef88d872e51af7ff13
bun913 <imanau03@gmail.com>
2023-06-25 11:23:01 +0900

    feat: solve abc175c

--- abc175/c/main.py
+++ abc175/c/main.py
@@ -1,30 +1,18 @@
-# -*- coding: utf-8 -*-
 """
-数直線の左か右にDだけK回移動する
-K回移動した座標の絶対値が最小になるようにしたい
-
-0に向かって近づける
-それでK回に回数が足りないなら、あとは+,-というのを繰り返しするだけでは
 """
-X, K, D = list(map(int, input().split()))
-# できるだけ0に近づけるように移動する
-dis_to_zero = abs(X)
-move_cnt = dis_to_zero // D
-dis = D * min(move_cnt, K)
-ans = X + dis
-if X > 0:
-    ans = X - dis
-# この時点で移動を使い切ってしまった場合
-if move_cnt >= K:
-    print(abs(ans))
+X, K, D = map(int, input().split())
+# Xを0に近づけるには何回移動すれば良いか
+c = abs(X) // D
+# 0に限りなく近づけるまでKが足りない場合
+if c > K:
+    print(abs(X) - K * D)
     exit()
-# 残りが2の倍数なら現時点の位置が答え
-rest = K - move_cnt
+# それ以外の場合K-cのあまりを考える
+rest = K - c
 if rest % 2 == 0:
-    print(abs(ans))
+    ans = abs(X) - c * D
+    print(ans)
     exit()
-if ans > 0:
-    ans -= D
-else:
-    ans += D
+# 奇数回の場合は1回余分に移動する
+ans = abs(X) - (c+1) * D
 print(abs(ans))
