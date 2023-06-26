b33b2a55d8282ed8babc01bc9a140e29351205f5
bun913 <imanau03@gmail.com>
2023-06-27 08:21:17 +0900

    feat: resolve 167c

--- abc167/c/main.py
+++ abc167/c/main.py
@@ -1,36 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ
-N,Mが最大12と良心的な数になっている
-ということはNを選ぶ組み合わせを全探索しても間に合うということ
-なので素直に全部のパターンを試して最小値を探すのみ
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+N,Mが非常に小さいのでbit全探索で利用する参考書を選べる
 """
+from itertools import product
 
-N, M, X = list(map(int, input().split()))
-memo = []
+N, M, X = map(int, input().split())
+C = []
+A = []
 for i in range(N):
-    L = list(map(int, input().split()))
-    memo.append({'c': L[0], 'A': L[1:]})
+    c, *a = map(int, input().split())
+    C.append(c)
+    A.append(a)
 
-ans = float('inf')
-for i in range(2**N):
-    costs = 0
-    understand_scores = [0 for _ in range(M)]
-    for j in range(N):
-        if ((i >> j) & 1):
-            # j番目の参考書を読む
-            dic = memo[j]
-            cost = dic['c']
-            scores = dic['A']
-            costs += cost
-            for a in range(M):
-                understand_scores[a] += scores[a]
-    is_score_overs = [True if s >= X else False for s in understand_scores]
-    if all(is_score_overs) is True:
-        ans = min(ans, costs)
-if ans == float('inf'):
-    ans = -1
+ans = 10**15
+for bools in product([True, False], repeat=N):
+    unds = [0] * M
+    cost = 0
+    for i, is_use in enumerate(bools):
+        if is_use is False:
+            continue
+        for j, a in enumerate(A[i]):
+            unds[j] += a
+        cost += C[i]
+    if all([und >= X for und in unds]):
+        ans = min(ans, cost)
+if ans == 10**15:
+    print(-1)
+    exit()
 print(ans)
