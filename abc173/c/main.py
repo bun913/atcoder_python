fbf98919c576d66396af5c2dd49cc66be573c2cd
bun913 <imanau03@gmail.com>
2023-06-28 08:31:47 +0900

    feat: solve 173c

--- abc173/c/main.py
+++ abc173/c/main.py
@@ -1,28 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-行を何行か選び、列を何列か選ぶ。選んだ行と列を全て赤く塗る
-黒いマスがK個残るようにできる行と列の選び方はあるか
-
-H,Wが6と非常に少ないのでそれぞれのbit全探索で間に合いそう
-"""
 from itertools import product
 from copy import deepcopy
 
-H, W, K = list(map(int, input().split()))
-A = [list("".join(input())) for _ in range(H)]
+H, W, K = map(int, input().split())
+C = [list(input()) for _ in range(H)]
 
+# H,Wが少ないのでbit全探索で求める
 ans = 0
-for row_bits in product([0, 1], repeat=H):
-    for col_bits in product([0, 1], repeat=W):
-        AD = deepcopy(A)
+for row_choices in product([False, True], repeat=H):
+    for col_choices in product([False, True], repeat=W):
+        c = deepcopy(C)
+        rows = [i for i, v in enumerate(row_choices) if v]
+        cols = [i for i, v in enumerate(col_choices) if v]
+        for row in rows:
+            c[row] = ['r'] * W
+        for col in cols:
+            for i in range(H):
+                c[i][col] = 'r'
         cnt = 0
-        for i in range(H):
-            for j in range(W):
-                if row_bits[i] == 1 or col_bits[j] == 1:
-                    AD[i][j] = "R"
-                # 行を黒くする
-                if AD[i][j] == "#":
-                    cnt += 1
+        for c_row in c:
+            cnt += c_row.count('#')
         if cnt == K:
             ans += 1
 print(ans)
