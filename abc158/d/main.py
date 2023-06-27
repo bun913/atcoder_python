71645139b52298c90a618b2836ad8f02b107b2b5
bun913 <imanau03@gmail.com>
2023-06-28 08:04:23 +0900

    feat: solve abc158d

--- abc158/d/main.py
+++ abc158/d/main.py
@@ -1,14 +1,33 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+これも今が反対の状態か管理しながら、文字列を追加する方向を考えれば良い
 """
-from functools import reduce
-from itertools import combinations
-import math
+from collections import deque
+
+S = input()
+Q = int(input())
+q = deque(list(S))
+
+is_reverse = False
+for _ in range(Q):
+    query = input().split()
+    # 反転させる場合
+    if query[0] == '1':
+        is_reverse = not is_reverse
+        continue
+    f = query[1]
+    c = query[2]
+    if f == '1':
+        if is_reverse is True:
+            q.append(c)
+            continue
+        q.appendleft(c)
+    else:
+        if is_reverse is True:
+            q.appendleft(c)
+            continue
+        q.append(c)
+if is_reverse is True:
+    print(*list(q)[::-1], sep='')
+    exit()
+print(*list(q), sep='')
