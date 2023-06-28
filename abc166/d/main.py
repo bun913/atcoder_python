cbc851e917e816881d5d829572c29cda851fa5a1
bun913 <imanau03@gmail.com>
2023-06-29 06:17:44 +0900

    feat: solve 166d

--- abc166/d/main.py
+++ abc166/d/main.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+A,Bはそれぞれだかだか10**2/5となる
 """
-from functools import reduce
-from itertools import combinations
-import math
+X = int(input())
+limit = int(pow(X, 2/5))
+for a in range(-limit, limit+1):
+    for b in range(-limit, limit+1):
+        if pow(a, 5) - pow(b, 5) == X:
+            print(a, b)
+            exit()
