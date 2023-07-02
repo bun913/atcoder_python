6c2fb4f954462b624d819b01eadf07bccc5c153e
bun913 <imanau03@gmail.com>
2023-07-02 15:16:04 +0900

    feat: 146c 二部探索

--- abc146/c/main.py
+++ abc146/c/main.py
@@ -1,35 +1,37 @@
-# -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+Xが非常に大きい数となるので全探索は無理
+Xをlogの計算に落とし込んで2分探索でとく
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+# 再帰関数の限界を上げる
+import sys
+sys.setrecursionlimit(10**9)
 
-"""
-考えたこと
-二部探索でいけばlogの計算で間に合うのでは？
-"""
+A, B, X = map(int, input().split())
 
-A, B, X = list(map(int, input().split()))
 
-# 2分探
-# 一番小さいのはまず0から
-# 最初に考えられる右端は10**9+1としておく
-l = 0
-r = 10 ** 9 + 1
-while r - l > 1:
-    c = (l + r) // 2
-    calced = A * c + B * len(str(c))
-    if calced <= X:
-        l = c
-    else:
-        r = c
-print(l)
+def is_ok(mid: int) -> bool:
+    cost = A * mid + B * len(str(mid))
+    if cost <= X:
+        return True
+    return False
+
+
+def meguru_bisect(ng, ok):
+    '''
+    初期値のng,okを受け取り,is_okを満たす最小(最大)のokを返す
+    まずis_okを定義すべし
+    ng ok は  とり得る最小の値-1 とり得る最大の値+1
+    最大最小が逆の場合はよしなにひっくり返す
+    '''
+    while (abs(ok - ng) > 1):
+        mid = (ok + ng) // 2
+        # print(ok, ng, mid)
+        if is_ok(mid):
+            ok = mid
+        else:
+            ng = mid
+    return ok
+
+
+ans = meguru_bisect(10**9+1, 0)
+print(ans)
