d52552da017c95262c5524fd6a06ff49e6456a5f
bun913 <imanau03@gmail.com>
2023-07-02 22:01:31 +0900

    feat: solve abc297c

--- abc297/c/main.py
+++ abc297/c/main.py
@@ -2,6 +2,18 @@
 """
 解く前のメモ用
 """
-from sys import setrecursionlimit
+from copy import deepcopy
 
-setrecursionlimit(10**7)
+H, W = map(int, input().split())
+# 普通にやるだけで良さそう
+S = [list(input()) for _ in range(H)]
+C = deepcopy(S)
+for i in range(H):
+    for j in range(W-1):
+        cur = C[i][j]
+        nxt = C[i][j+1]
+        if cur == 'T' and nxt == 'T':
+            C[i][j] = 'P'
+            C[i][j+1] = 'C'
+for c in C:
+    print(*c, sep='')
