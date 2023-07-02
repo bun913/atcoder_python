e28ea2390d886b390c5a6888796ced11d754e14f
bun913 <imanau03@gmail.com>
2023-07-02 13:38:38 +0900

    feat: solve abc169 d

--- abc169/d/main.py
+++ abc169/d/main.py
@@ -1,7 +1,40 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+まず素因数分解をする関数を作成
 """
-from sys import setrecursionlimit
 
-setrecursionlimit(10**7)
+
+def factorization(n):
+    arr = []
+    temp = n
+    for i in range(2, int(-(-n**0.5//1))+1):
+        if temp % i == 0:
+            cnt = 0
+            while temp % i == 0:
+                cnt += 1
+                temp //= i
+            arr.append([i, cnt])
+
+    if temp != 1:
+        arr.append([temp, 1])
+
+    if arr == []:
+        arr.append([n, 1])
+
+    return arr
+
+
+N = int(input())
+# エッジケースの排除
+if N == 1:
+    print(0)
+    exit()
+primes = factorization(N)
+ans = 0
+for prime, num in primes:
+    i = 1
+    while num >= i:
+        num -= i
+        ans += 1
+        i += 1
+print(ans)
