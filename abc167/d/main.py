3b86589943bc47122a5371bf41ba0b85ee2087db
bun913 <imanau03@gmail.com>
2023-07-02 16:01:27 +0900

    feat: solve abc167d

--- abc167/d/main.py
+++ abc167/d/main.py
@@ -1,14 +1,30 @@
-# -*- coding: utf-8 -*-
-"""
-Atcoderの問題解く用
+N, K = list(map(int, input().split()))
+A = list(map(int, input().split()))
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
-"""
-from functools import reduce
-from itertools import combinations
-import math
+rest = K
+# 各街に到達した回数を保持する配列
+visited = [-1] * N
+now_cnt = 0
+iter_cnt = 0
+pos = 0
+while True:
+    # ループより先に到達する場合
+    if rest == 0:
+        print(pos + 1)
+        exit()
+    # まだ未到達だった場合
+    if visited[pos] == -1:
+        visited[pos] = now_cnt
+        now_cnt += 1
+        rest -= 1
+        pos = A[pos] - 1
+        continue
+    # 2回目の到達の場合
+    iter_cnt = now_cnt - visited[pos]
+    break
+rest %= iter_cnt
+for i in range(N):
+    # 現在のポジションからrest回移動したポジションとひとしければ、その街が答え
+    if visited[i] == visited[pos] + rest:
+        print(i + 1)
+        exit()
