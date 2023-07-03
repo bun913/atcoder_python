39c876681c5dfb1e2840dcabe2460def0706bd99
bun913 <imanau03@gmail.com>
2023-07-04 06:28:17 +0900

    feat: solve abc197d

--- abc197/d/main.py
+++ abc197/d/main.py
@@ -1,21 +1,32 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+正N角系という情報が与えられる
+Nは偶数であることが保証されている
+多角形の頂点の一つを割り出す問題
+スタート地点との対角の頂点は与えられている
+つまり三角形の2点は最初からわかっている
+- 多角形の中心を求めて
+- 中心から点を回転させた後の座標を求めればよい
 """
+from math import radians, cos, sin
 
+
+def rotation_matrix(x, y, angle):
+    # 回転行列
+    rad = radians(angle)
+    ans = (x * cos(rad) - y * sin(rad), x * sin(rad) + y * cos(rad))
+    return ans
+
+
+N = int(input())
+x0, y0 = map(int, input().split())
+xx, yy = map(int, input().split())  # 対角の頂点
+# 中心を求める
+xo = (x0 + xx) / 2
+yo = (y0 + yy) / 2
+# 中心が原点に来る様に座標を移動させる
+x0 -= xo
+y0 -= yo
+# 点を回転させる方法は持っているのであとは角度を求める
+angle = 360 / N
+ans = rotation_matrix(x0, y0, angle)
+print(ans[0] + xo, ans[1] + yo)
