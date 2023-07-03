b96f264f2eb2d6c9794f3981a2e103efa9bca87c
bun913 <imanau03@gmail.com>
2023-07-03 09:49:06 +0900

    feat: bit全探索で仕切りを入れるという考え方

--- abc197/c/main.py
+++ abc197/c/main.py
@@ -1,21 +1,27 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+Nが最大で20しかないのでBit全探索で解けそう
+1つ以上のからではなく連続した区間に分ける
+つまり、Bit全探索で区間の境目を決められる
+ただし全部Falseは除く
 """
+from itertools import product
+from functools import reduce
 
+N = int(input())
+A = list(map(int, input().split()))
+
+ans = float("inf")
+# 仕切りをAiとAi+1の間に入れると考える
+for bools in product([True, False], repeat=N-1):
+    or_list = []
+    cur_or = 0
+    for i in range(N):
+        # orを取る
+        cur_or |= A[i]
+        # 仕切りがあったらorをリストに追加
+        # bools[i]がTrueの時に仕切りがあるので計算結果の格納と初期化
+        if i == N-1 or bools[i]:
+            or_list.append(cur_or)
+            x = 0
+    ans = min(ans, reduce(lambda x, y: x ^ y, or_list))
+print(ans)
