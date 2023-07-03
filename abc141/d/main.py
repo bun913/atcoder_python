22e25954845cd120da306f24f9ab87f78cb088b0
bun913 <imanau03@gmail.com>
2023-07-04 06:03:10 +0900

    feta: solve abc141d

--- abc141/d/main.py
+++ abc141/d/main.py
@@ -1,14 +1,43 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+m枚の割引があるので順番付きのデータ構造で純粋に出し入れすればよし
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+import heapq
+
+
+class priorityqueue:
+    """
+    最大値を常にpopしたい場合は、init時に-1をかける
+    """
+
+    def __init__(self, initial_data=None):
+        if initial_data:
+            data = [-1 * x for x in initial_data]
+            heapq.heapify(data)
+            self._heap = data
+        else:
+            init_data = []
+            heapq.heapfy(init_data)
+            self._heap = init_data
+
+    def is_empty(self):
+        return len(self._heap) == 0
+
+    def push(self, item):
+        heapq.heappush(self._heap, item * -1)
+
+    def pop(self):
+        return -1 * heapq.heappop(self._heap)
+
+    def get_list(self):
+        return list(self._heap)
+
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+hq = priorityqueue(a)
+for _ in range(m):
+    max_a = hq.pop()
+    hq.push(max_a // 2)
+ans = -sum(hq.get_list())
+print(ans)
