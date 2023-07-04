5cdeaa94c43dd6a4e2b378774a4fe6262226350b
bun913 <imanau03@gmail.com>
2023-07-05 07:01:06 +0900

    feat: solve abc284a,b

--- abc284/a/main.py
+++ abc284/a/main.py
@@ -5,3 +5,7 @@
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+
+N = int(input())
+S = [input() for _ in range(N)]
+print(*S[::-1], sep="\n")
--- abc284/b/main.py
+++ abc284/b/main.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 解く前のメモ用
+各テストケースについて、N個おん正の整数から奇数を数える
 """
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    A = list(map(int, input().split()))
+    ans = 0
+    for a in A:
+        if a % 2 == 1:
+            ans += 1
+    print(ans)
