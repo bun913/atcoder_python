547e6b732ba728473b98880d2af2b6f27ceae24f
bun913 <imanau03@gmail.com>
2023-07-05 06:51:54 +0900

    feat: solve abc178c,d

--- abc178/c/main.py
+++ abc178/c/main.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
 """
 解く前のメモ用
+0と9を含む数列の数を数える
+数列全部の数から条件を満たさないものを引いた方が簡単
 """
-from sys import setrecursionlimit
-
-setrecursionlimit(10**7)
+N = int(input())
+# Nが1のエッジケース
+if N == 1:
+    print(0)
+    exit()
+# N**10乗が全体の数だが
+mod = 10**9+7
+al = pow(10, N, mod)
+# 0または9を含まない数列の数を数える
+# 0を含まない数列の数
+dec0 = pow(9, N, mod)
+dec9 = pow(9, N, mod)
+# 0も9も含まない数列の数
+dec09 = pow(8, N, mod)
+ans = (al - dec0 - dec9 + dec09) % mod
+print(ans)
