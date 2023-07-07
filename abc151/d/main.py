963034626fcfa2a61fa18e1286140379c31f0c1e
bun913 <imanau03@gmail.com>
2023-07-08 06:24:35 +0900

    feat: solve abc151d

--- abc151/d/main.py
+++ abc151/d/main.py
@@ -1,14 +1,39 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+迷路の問題
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from collections import deque
+
+H, W = map(int, input().split())
+S = [list(input()) for _ in range(H)]
+# 迷路のつながっている道同士をグラフで表現する
+G = [list() for _ in range(H*W)]
+# 縦方向
+for i in range(H):
+    for j in range(W-1):
+        if S[i][j] == '.' and S[i][j+1] == '.':
+            G[i*W+j].append(i*W+j+1)
+            G[i*W+j+1].append(i*W+j)
+# 横方向
+for i in range(H-1):
+    for j in range(W):
+        if S[i][j] == '.' and S[i+1][j] == '.':
+            G[i*W+j].append((i+1)*W+j)
+            G[(i+1)*W+j].append(i*W+j)
+ans = -1
+# 幅優先探索
+for start in range(H*W):
+    q = deque([start])
+    dist = [-1] * (H*W)
+    dist[start] = 0
+    while q:
+        cur = q.popleft()
+        for nex in G[cur]:
+            # 訪問済みの場合
+            if dist[nex] != -1:
+                continue
+            # 訪問していない場合
+            dist[nex] = dist[cur] + 1
+            q.append(nex)
+    ans = max(ans, max(dist))
+print(ans)
