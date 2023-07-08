8e7a460ba72ec2801fdf9a2ee3c1cede2715002d
bun913 <imanau03@gmail.com>
2023-07-08 11:45:25 +0900

    feat: solve abc156 d

--- abc156/d/main.py
+++ abc156/d/main.py
@@ -1,14 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+全体の数 2**n -1 
+引く数 C(n, a) + C(n, b)
 """
-from functools import reduce
-from itertools import combinations
-import math
+
+
+def nCr_mod(n: int, r: int, mod: int) -> int:
+    # 分子
+    numerator = 1
+    for i in range(n-r+1, n+1):
+        numerator = numerator * i % mod
+    # 分母
+    demoninator = 1
+    for i in range(1, r+1):
+        demoninator = demoninator * i % mod
+    # 分子*分母の逆元
+    return numerator * pow(demoninator, mod-2, mod) % mod
+
+
+N, a, b = map(int, input().split())
+ans = pow(2, N, 10 ** 9 + 7) - 1
+mod = 10 ** 9 + 7
+ans -= nCr_mod(N, a, mod)
+ans -= nCr_mod(N, b, mod)
+ans %= mod
+print(ans)
