c92b9683171f45ec0382fcff7bfd4357b1f27b6b
bun913 <imanau03@gmail.com>
2023-07-09 06:23:42 +0900

    feat: solve abc305c

--- abc305/c/main.py
+++ abc305/c/main.py
@@ -1,7 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+自分の周りのクッキーが最大となる場所を探す
 """
-from sys import setrecursionlimit
+H, W = map(int, input().split())
+S = [list(input()) for _ in range(H)]
+dxy = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-setrecursionlimit(10**7)
+max_cookie = 0
+ans = (-1, -1)
+for i in range(H):
+    for j in range(W):
+        # 自分がクッキーなら違う
+        if S[i][j] == '#':
+            continue
+        cookie_cnt = 0
+        for dx, dy in dxy:
+            if 0 <= i + dx < H and 0 <= j + dy < W:
+                if S[i + dx][j + dy] == '#':
+                    cookie_cnt += 1
+        if max(max_cookie, cookie_cnt) == cookie_cnt:
+            max_cookie = cookie_cnt
+            ans = (i+1, j+1)
+print(ans[0], ans[1])
