f206b1ef469ba1771649f5b3fc43bff431d078f4
bun913 <imanau03@gmail.com>
2023-07-11 06:32:07 +0900

    feat: solve decimal abc308c

--- abc308/c/main.py
+++ abc308/c/main.py
@@ -1,16 +1,22 @@
-# -*- coding: utf-8 -*-
 """
-方向を変えてめっちゃでかい数を利用して整数演算をしてみる
+普通にやると Ai / (Ai+Bi)と少数を扱うことになる
+そのままだと誤差がでるので、とてつもなく大きい数をかけてやってみよう
+（Pythonだと大きい数も扱える）
 """
+from decimal import Decimal
+from operator import itemgetter
+
 N = int(input())
-success_rates = []
-for _ in range(N):
+score_list = []
+for i in range(N):
     a, b = map(int, input().split())
-    # めっちゃでかい数で整数演算を盛り込んでみる
-    success_rate = a * (10**20) // (a + b)
-    success_rates.append(success_rate)
+    score = Decimal(a) / Decimal(a+b)
+    score_list.append((score, i+1))
 
-people = list(range(1, N + 1))
-people.sort(key=lambda i: (-success_rates[i-1], i))
+score_list.sort(key=itemgetter(1))
+score_list.sort(key=itemgetter(0), reverse=True)
 
-print(*people, sep=' ')
+ans_list = []
+for _, ind in score_list:
+    ans_list.append(ind)
+print(*ans_list, sep=' ')
