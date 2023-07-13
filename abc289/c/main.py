6fa99392f18d5314c297640b8d4d23a3e48470ee
bun913 <imanau03@gmail.com>
2023-07-14 07:03:23 +0900

    feat: solve abc289c

--- abc289/c/main.py
+++ abc289/c/main.py
@@ -2,6 +2,29 @@
 """
 解く前のメモ用
 """
-from sys import setrecursionlimit
+from itertools import product
 
-setrecursionlimit(10**7)
+N, M = map(int, input().split())
+C = []
+for _ in range(M):
+    c = int(input())
+    A = set(map(int, input().split()))
+    C.append(A)
+
+# bit全探索で選ぶ集合を決めれば良い
+ans = 0
+cond_set = set([i for i in range(1, N + 1)])
+for bits in product([True, False], repeat=M):
+    # 全てFalseならスキップ
+    if not any(bits):
+        continue
+    s = set()
+    for i, bit in enumerate(bits):
+        if bit is False:
+            continue
+        cur_set = C[i]
+        # ここが違う
+        s = s.union(cur_set)
+    if s == cond_set:
+        ans += 1
+print(ans)
