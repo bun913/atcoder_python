d6be8217e603668c65df3b58999da9c106bf61d0
bun913 <imanau03@gmail.com>
2023-07-14 06:43:42 +0900

    feat: solve abc299c

--- abc299/c/main.py
+++ abc299/c/main.py
@@ -2,6 +2,32 @@
 """
 解く前のメモ用
 """
-from sys import setrecursionlimit
+from itertools import groupby
 
-setrecursionlimit(10**7)
+N = int(input())
+S = list(input())
+grouped = [(key, len(list(group))) for key, group in groupby(S)]
+
+ans = -1
+
+# まずは前からみる
+limit = len(grouped)
+for i in range(1, limit):
+    bef = grouped[i-1]
+    if bef[0] != '-':
+        continue
+    cur = grouped[i]
+    level = cur[1]
+    ans = max(ans, level)
+
+# 次に反転した配列を使って後ろからみる
+reversed_grouped = grouped[::-1]
+for i in range(1, limit):
+    bef = reversed_grouped[i-1]
+    if bef[0] != '-':
+        continue
+    cur = reversed_grouped[i]
+    level = cur[1]
+    ans = max(ans, level)
+
+print(ans)
