fd8618f8e4f0bc87c4a6b28c7265fbdda409d3ea
bun913 <imanau03@gmail.com>
2023-07-13 21:59:01 +0900

    feat: solve abc291c

--- abc291/c/main.py
+++ abc291/c/main.py
@@ -1,7 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+高橋が同じ座標にいたことがあるか判定する
 """
-from sys import setrecursionlimit
+N = int(input())
+S = list(input())
+cur = (0, 0)
+visited = set()
+visited.add(cur)
 
-setrecursionlimit(10**7)
+for s in S:
+    if s == 'R':
+        cur = (cur[0]+1, cur[1])
+    elif s == 'L':
+        cur = (cur[0]-1, cur[1])
+    elif s == 'U':
+        cur = (cur[0], cur[1]+1)
+    elif s == 'D':
+        cur = (cur[0], cur[1]-1)
+    if cur in visited:
+        print("Yes")
+        exit()
+    visited.add(cur)
+print("No")
