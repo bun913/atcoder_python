c5f361649ce5181b93ed8bbd1de5564bf69c1a26
bun913 <imanau03@gmail.com>
2023-07-16 21:19:40 +0900

    feat: solve abc209c

--- abc209/c/main.py
+++ abc209/c/main.py
@@ -1,9 +1,21 @@
 """
+長さNの整数列Cが与えられる
+以下の要素を全て満たす長さNの整数列Aの個数を求める
+1 <= Ai <= Ci
+Ai != Aj(1 <=i < j <= N)
 """
 N = int(input())
 C = list(map(int, input().split()))
-CD = sorted(C)
+mod = 10**9 + 7
+
+sort_c = sorted(C)
 ans = 1
-for i in range(N):
-    ans = ans * max(0, CD[i] - i) % 1000000007
+
+for i, c in enumerate(sort_c):
+    cnt = c - i
+    ans *= cnt
+    if ans == 0:
+        print(0)
+        exit()
+    ans %= mod
 print(ans)
