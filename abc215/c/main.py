57139c0194cc2fa2a1b7e640c3a2e520955c8468
bun913 <imanau03@gmail.com>
2023-07-16 20:39:36 +0900

    feat: solve abc215c

--- abc215/c/main.py
+++ abc215/c/main.py
@@ -1,30 +1,8 @@
-"""
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
-"""
 from itertools import permutations
 
 S, K = input().split()
 K = int(K)
-
-cands = list(permutations(S, len(S)))
-cands_to_s = [''.join(t) for t in cands]
-cands_to_s = list(set(cands_to_s))
-_sorted = sorted(cands_to_s)
-print(_sorted[K-1])
+cands = list(permutations(S))
+cands = list(set(cands))
+ans_list = sorted(cands)
+print("".join(ans_list[K-1]))
