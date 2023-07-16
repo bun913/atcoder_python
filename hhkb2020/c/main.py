b8d238661b82e7deba68d30c814fafcca537d828
bun913 <imanau03@gmail.com>
2023-07-16 22:09:51 +0900

    feat: solve hhkb2020c

--- hhkb2020/c/main.py
+++ hhkb2020/c/main.py
@@ -9,6 +9,20 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce
-from itertools import combinations
-import math
+from heapq import heappush, heappop, heapify
+
+N = int(input())
+P = list(map(int, input().split()))
+
+cant_use = set()
+cands = [i for i in range(0, N+1)]
+heapify(cands)
+
+for p in P:
+    cant_use.add(p)
+    while True:
+        cand = heappop(cands)
+        if cand not in cant_use:
+            print(cand)
+            heappush(cands, cand)
+            break
