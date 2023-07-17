b7ec55380b9241b1b2f3738288d2c4971b6109fa
bun913 <imanau03@gmail.com>
2023-07-17 11:48:20 +0900

    feat: resolve abc207c

--- abc207/c/main.py
+++ abc207/c/main.py
@@ -1,32 +1,33 @@
 """
-N個の区間
-1ならX以上Y以下の全ての実数からなる区間
-2ならX以上Y未満
-3ならXより大きくY以下
-4ならXより大きくY未満
-N以下の整数の組(i,j)が共通部分を持つ数
+全部の区間をl以上、r以下という形で持ち直す
+2重ループで区間を全探索する
+その区間について、lとrの大小関係によって場合分けをする
 """
 N = int(input())
-dists = []
+tlr = [list(map(int, input().split())) for _ in range(N)]
+intervals = []
 
-for _ in range(N):
-    t, l, r = list(map(int, input().split()))
-    # 1の状態
-    dist = [l, r]
-    if t == 2 or t == 4:
-        dist[1] -= 0.5
-    if t == 3 or t == 4:
-        dist[0] += 0.5
-    dists.append(dist)
+for t, l, r in tlr:
+    left = l
+    right = r
+    if t == 2:
+        right -= 0.5
+    if t == 3:
+        left += 0.5
+    if t == 4:
+        left += 0.5
+        right -= 0.5
+    intervals.append((left, right))
 
-# 総当たりで共通の個数を求める
+# 全探索で答えを求める
 ans = 0
 for i in range(N):
     for j in range(i + 1, N):
-        a = dists[i]
-        b = dists[j]
-        maxl = max(a[0], b[0])
-        minr = min(a[1], b[1])
-        is_count_up = not minr < maxl
-        ans += max(a[0], b[0]) <= min(a[1], b[1])
+        left_i, right_i = intervals[i]
+        left_j, right_j = intervals[j]
+        if right_i < left_j:
+            continue
+        if right_j < left_i:
+            continue
+        ans += 1
 print(ans)
