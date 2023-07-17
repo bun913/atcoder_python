b968fd08325f814279d5c239203b7db8285ea60e
bun913 <imanau03@gmail.com>
2023-07-17 11:28:16 +0900

    feat: solve abc221 c

--- abc221/c/main.py
+++ abc221/c/main.py
@@ -1,32 +1,42 @@
-# -*- coding: utf-8 -*-
 """
-整数N
-Nの各桁の数字を取り出して並べる
-0から始めることはできない
-適切にNを分離した時分離後の2数の積の最大値
-
-Nが最高でも9桁なのでbit全探索で左辺と右辺に分ければ良い
-あとはそれぞれを降順にソートして計算すればよし
+整数Nをどっかで分割して、好きに並べ替える
+分割後の数の積の最大値を求める
+bit全探索で使う数を決めようか、たかだか10桁だし
+であとはソートして使えばOK
+ただし0始りの数は許されないことに注意
 """
 from itertools import product
 
-N = int(input())
-S = str(N)
+
+def can_convert(str_list):
+    if str_list[0] == '0' and len(str_list) > 1:
+        return False
+    return True
+
+
+N = input()
+L = list(N)
 
 ans = -1
-for case_list in product([True, False], repeat=len(S)):
+for bits in product([True, False], repeat=len(N)):
+    # 全部Trueと全部Falseは飛ばす
     left = []
     right = []
-    for i, is_left in enumerate(case_list):
+    for i, is_left in enumerate(bits):
         if is_left is True:
-            left.append(S[i])
+            left.append(L[i])
             continue
-        right.append(S[i])
+        right.append(L[i])
     if len(left) == 0 or len(right) == 0:
         continue
-    if left[0] == "0" or right[0] == "0":
+    left_sorted = sorted(left, reverse=True)
+    right_sorted = sorted(right, reverse=True)
+    # 0始まりの場合はスキップ
+    if can_convert(left_sorted) is False:
         continue
-    ls = "".join(sorted(left, reverse=True))
-    rs = "".join(sorted(right, reverse=True))
-    ans = max(ans, int(ls) * int(rs))
+    if can_convert(right_sorted) is False:
+        continue
+    left_num = int(''.join(left_sorted))
+    right_num = int(''.join(right_sorted))
+    ans = max(ans, left_num * right_num)
 print(ans)
