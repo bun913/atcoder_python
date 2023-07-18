bb85d41f94fd757ab8808795ea2ba017ad82cc83
bun913 <nagasima.romance+@gmail.com>
2023-07-19 08:39:17 +0900

    feat: resolve abc237d

--- abc237/d/main.py
+++ abc237/d/main.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
 from collections import deque
 
 N = int(input())
-S = input()
-ans = deque([N])
-
-for i in range(N - 1, -1, -1):
+q = deque([N])
+S = list(input())
+# 逆順に処理していけば良い
+for i in range(N-1, -1, -1):
     s = S[i]
-    if s == "R":
-        ans.appendleft(i)
-    else:
-        ans.append(i)
-print(*ans)
+    if s == 'R':
+        q.appendleft(i)
+        continue
+    q.append(i)
+print(*q)
