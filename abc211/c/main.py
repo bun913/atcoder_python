8031ae81fe7fefb190e3a86016e159d998b688e4
bun913 <nagasima.romance+@gmail.com>
2023-07-20 07:10:57 +0900

    feat: solve abc211c

--- abc211/c/main.py
+++ abc211/c/main.py
@@ -1,24 +1,25 @@
 """
-このうち8文字を選んで、左から順にchokudaiとする方法の全通り数
-Sが10**5なのでcombinationsとかは無理
-DPでi文字めまでを使ってchokudaiのk文字目までを作る方法が何通りあるか
+chokudaiという部分文字列を何個作れるか
+部分文字列DPの考え方を使う
 """
 S = input()
-N = len(S)
-S = "?" + S
-T = "?chokudai"
-# 表を作成する
-dp = [[0 for _ in range(9)] for _ in range(N + 1)]
+L = list(S)
+S = '?' + S  # 適当に最初の文字を埋めておく
+T = '?chokudai'
 mod = 10**9 + 7
-# 初期化(すぐにわかるところを埋める)
-for i in range(N + 1):
+
+dp = [[0] * 9 for _ in range(len(S))]
+# すぐにわかるところを埋める
+for i in range(len(S)):
     dp[i][0] = 1
-# 表の小さい方から答えに辿り着くまで埋める
-for i in range(1, N + 1):
-    for k in range(1, 9):
-        if S[i] == T[k]:
-            dp[i][k] = dp[i - 1][k] + dp[i][k - 1]
+
+for i in range(1, len(S)):
+    for j in range(1, 9):
+        # 今見ている文字が作りたい文字と同じ場合
+        if S[i] == T[j]:
+            dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
         else:
-            dp[i][k] = dp[i - 1][k]
-        dp[i][k] %= mod
+            # 同じ文字じゃない場合これまでの結果を使うしかない
+            dp[i][j] = dp[i-1][j]
+        dp[i][j] %= mod
 print(dp[-1][-1])
