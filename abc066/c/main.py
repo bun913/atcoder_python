a832ef0aa7494f8d226b9c990e07cf1cfe0e992f
bun913 <nagasima.romance+@gmail.com>
2023-07-21 06:59:25 +0900

    feat: solve abc066c

--- abc066/c/main.py
+++ abc066/c/main.py
@@ -1,22 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-操作をn回行う
-数列のi番目の要素aiをbの末尾に追加する
-
-dequeで交互に入れ替え
-"""
 from collections import deque
 
 N = int(input())
 A = list(map(int, input().split()))
-B = deque()
 
-for i, a in enumerate(A):
-    if i % 2 == 0:
-        B.appendleft(a)
-        continue
-    B.append(a)
-if N % 2 != 0:
-    print(*B, sep=" ")
-    exit()
-print(*reversed(B), sep=" ")
+q = deque([])
+is_reverse = False
+
+for a in A:
+    if is_reverse:
+        q.appendleft(a)
+    else:
+        q.append(a)
+    is_reverse = not is_reverse
+
+b = list(q)
+if is_reverse == True:
+    b.reverse()
+print(*b)
