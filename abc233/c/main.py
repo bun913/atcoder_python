a4a111b050bc300ba6297aa21aba44846a0c37a7
bun913 <nagasima.romance+@gmail.com>
2023-07-21 06:39:04 +0900

    feat: solve abc233c

--- abc233/c/main.py
+++ abc233/c/main.py
@@ -1,14 +1,33 @@
-# -*- coding: utf-8 -*-
 """
-取り出したボールの総積がXになるような取り出し方
-書かれた数が同じでも区別する
-袋に入っているボールの数は10**5を越えない
+ボールの入った袋の個数をN
+ボールの総数をAとする
+今回の組み合わせの個数は ACNとなる
+で、今回ボールの個数の総積は10**5を超えないとある
+そのため全探索しても十分に間に合うはずである
+N個の袋からボールを一つ選ぶことを全探索する
 """
-N, X = list(map(int, input().split()))
-ans_lis = [1]
+from itertools import product
 
+N, X = map(int, input().split())
+L = []
+A = []
 for _ in range(N):
-    A = list(map(int, input().split()))
-    L = A.pop(0)
-    ans_lis = [x * y for x in ans_lis for y in A]
-print(ans_lis.count(X))
+    l, *a = map(int, input().split())
+    L.append(l)
+    A.append(a)
+# 全ての数字を掛け合わせる全探索を行う
+P = [1]
+# productで書くとこうなる
+for i in range(N):
+    P = [p * ai for p, ai in product(P, A[i])]
+ans = P.count(X)
+print(ans)
+# for i in range(N):
+#     tmp_product = []
+#     for ai in A[i]:
+#         for p in P:
+#             tmp_product.append(p * ai)
+#     P = tmp_product
+
+# ans = P.count(X)
+# print(ans)
