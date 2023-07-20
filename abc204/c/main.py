dd646157e1bd93944a8a309040f6c2497a7d9332
bun913 <nagasima.romance+@gmail.com>
2023-07-21 06:54:46 +0900

    feat: solve abc204c

--- abc204/c/main.py
+++ abc204/c/main.py
@@ -1,15 +1,11 @@
-"""
-"""
 from collections import deque
 
-N, M = map(int, input().split())
+N, M = list(map(int, input().split()))
 G = [[] for _ in range(N)]
-
 for _ in range(M):
-    a, b = map(int, input().split())
+    a, b = list(map(int, input().split()))
     G[a-1].append(b-1)
 
-# 答えには自分自身を含む
 ans = 0
 for start in range(N):
     q = deque([start])
@@ -17,11 +13,10 @@ for start in range(N):
     visited[start] = True
     while q:
         cur = q.popleft()
-        for to in G[cur]:
-            # すでに訪問済みの場合はスキップ
-            if visited[to] is True:
+        for nex in G[cur]:
+            if visited[nex] is True:
                 continue
-            visited[to] = True
-            q.append(to)
+            visited[nex] = True
+            q.append(nex)
     ans += sum(visited)
 print(ans)
