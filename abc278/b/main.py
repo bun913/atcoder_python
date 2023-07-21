8d6bb4db3ec185637921032adfaba496ba7b1205
bun913 <nagasima.romance+@gmail.com>
2023-07-22 07:01:08 +0900

    feta: abc278bc

--- abc278/b/main.py
+++ abc278/b/main.py
@@ -1,40 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
-見間違えやすい時刻・・・右上と左したを入れ替えても24時間時刻としてみれる
-現時点を含める見間違えやすい最初の時刻を求める
-Aは0,1,2のいずれか
-Bは0以上9以下までのいずれか（でもAが2の時には0以上3以下）
-Cは0以上5以下までのいずれか
-Dは0以上9以下
+時計の右上と左下を入れ替えても時刻として成立するのを間違いやすい時刻とする
+初めて訪れる見間違いやすい時刻を求める
 """
-from sys import setrecursionlimit
-setrecursionlimit(10**7)
+H, M = map(int, input().split())
 
-
-def is_confuse_time(h: int, m: int):
-    # 見間違いやすい時間の判定
+for i in range(0, 3601):
+    # 繰り上げる
+    m = M + i
+    h = H
+    if m >= 60:
+        h += m // 60
+        m %= 60
+    h %= 24
     a = h // 10
     b = h % 10
     c = m // 10
     d = m % 10
     b, c = c, b
     if a == 2:
-        if b > 3:
-            return False
-    if c > 5:
-        return False
-    return True
-
-
-H, M = list(map(int, input().split()))
-for i in range(60 * 60 * 24):
-    if is_confuse_time(H, M):
-        print("{} {}".format(H, M))
+        if b <= 3 and c <= 5 and d <= 9:
+            print(h, m)
+            exit()
+    if a <= 1 and b <= 9 and c <= 5 and d <= 9:
+        print(h, m)
         exit()
-    # 1秒足す
-    M += 1
-    M %= 60
-    if M == 0:
-        H += 1
-        H %= 24
--- abc278/c/main.py
+++ abc278/c/main.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
 """
 解く前のメモ用
+よく見ればNが10**9なので全員分の配列を作る段階でTLEになってしまう
+なのでdictで管理するのが良さそう
 """
-from sys import setrecursionlimit
+from collections import defaultdict
 
-setrecursionlimit(10**7)
+N, Q = map(int, input().split())
+relations = defaultdict(set)
+
+for _ in range(Q):
+    t, a, b = map(int, input().split())
+    if t == 1:
+        relations[a].add(b)
+        continue
+    if t == 2:
+        if b in relations[a]:
+            relations[a].remove(b)
+        continue
+    cand1 = b in relations[a]
+    cand2 = a in relations[b]
+    if cand1 and cand2:
+        print("Yes")
+        continue
+    print("No")
