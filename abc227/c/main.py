71cd2b867ad0e28a4e313d6f2e46b590656c1914
bun913 <nagasima.romance+@gmail.com>
2023-07-23 14:59:18 +0900

    feat: solve abc227c

--- abc227/c/main.py
+++ abc227/c/main.py
@@ -1,20 +1,21 @@
-# -*- coding: utf-8 -*-
 """
-答えは2**63以下
-
-A**3<=Nが絶対条件（Aが最小だから)
+A <= B <= Cかつ A * B* C <=N であるような正の整数の組みの数を求める
+Nが10**11以下であることは保証されている
+ということは、制約からAは10**4以下であることは保証されている
 """
 N = int(input())
-
-a = 1
 ans = 0
-
-while a**3 <= N:
-    # 初期値
+a = 1
+while a ** 3 <= N:
     b = a
-    while a * (b**2) <= N:
-        # cはb以上である必要があるので-bして+1
-        ans += N // (a * b) - b + 1
+    b_limit = int((N / a) ** (1/2)) + 1
+    while a * b ** 2 <= N:
+        c = N // (a*b)
+        if c < b:
+            break
+        cnt = c - b + 1
+        ans += cnt
         b += 1
     a += 1
+
 print(ans)
