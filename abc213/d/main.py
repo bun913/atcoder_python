0acc57974f66f53f42d42e9429780131ae9d3123
bun913 <nagasima.romance+@gmail.com>
2023-07-24 07:09:17 +0900

    feat: solve abc213d

--- abc213/d/main.py
+++ abc213/d/main.py
@@ -1,21 +1,34 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+import heapq
+import sys
 
+sys.setrecursionlimit(10**8)
+
+N = int(input())
+G = [[] for _ in range(N)]
+for _ in range(N-1):
+    a, b = map(int, input().split())
+    G[a-1].append(b-1)
+    G[b-1].append(a-1)
+
+# 小さい順に回るためソートする（これでnexが小さい順になる）
+for i in range(N):
+    G[i].sort()
+
+routes = []
+
+
+def dfs(now, pre):
+    routes.append(now+1)
+    for to in G[now]:
+        if to == pre:
+            continue
+        # さらに先に行く
+        dfs(to, now)
+        # これは今のところに戻ってきたので、ルートに戻ってきたことを記録する
+        routes.append(now+1)
+
+
+dfs(0, -1)
+print(*routes)
