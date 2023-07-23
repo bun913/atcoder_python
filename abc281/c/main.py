e79723e6fd8a908aed963f9e5900d15bd81b656c
bun913 <nagasima.romance+@gmail.com>
2023-07-24 07:09:33 +0900

    feat: solve abc281c

--- abc281/c/main.py
+++ abc281/c/main.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+T秒後に流れている音楽を求める
 """
 from sys import setrecursionlimit
+from itertools import accumulate
+from bisect import bisect_left
 
 setrecursionlimit(10**7)
+
+N, T = list(map(int, input().split()))
+A = list(map(int, input().split()))
+tt = T % sum(A)
+acc = list(accumulate(A))
+for i in range(N):
+    if acc[i] > tt:
+        rest = acc[i] - tt
+        print(i+1, A[i]-rest)
+        exit()
