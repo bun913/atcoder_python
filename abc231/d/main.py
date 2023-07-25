630d71d9b941aef35153886ea758cbc34de77f29
bun913 <nagasima.romance+@gmail.com>
2023-07-25 09:52:06 +0900

    feat: solve abc231d

--- abc231/d/main.py
+++ abc231/d/main.py
@@ -1,45 +1,60 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input()
-#int型で受け取るとき
-s = int(input())
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+- 一つのノードから経路が3本以上あればだめ？
+- だけでなくループしている場合もダメ
+    - ループしているかどうかはすでに同じ隣接成分になっているときにさらに辺同士を繋げるとなる
 """
-from functools import reduce
-from itertools import combinations
-import math
-
-# 素数判定
 
 
-def is_prime(n: int) -> bool:
-    for i in range(2, int(math.sqrt(n))+1):
-        if n % i == 0:
-            return False
-    return True
+class UnionFind:
+    def __init__(self, n):
+        self.n = n
+        self.parent_size = [-1]*n
+
+    def leader(self, a):
+        if self.parent_size[a] < 0:
+            return a
+        self.parent_size[a] = self.leader(self.parent_size[a])
+        return self.parent_size[a]
+
+    def merge(self, a, b):
+        x, y = self.leader(a), self.leader(b)
+        if x == y:
+            return
+        if abs(self.parent_size[x]) < abs(self.parent_size[y]):
+            x, y = y, x
+        self.parent_size[x] += self.parent_size[y]
+        self.parent_size[y] = x
+
+    def same(self, a, b):
+        return self.leader(a) == self.leader(b)
+
+    def size(self, a):
+        return abs(self.parent_size[self.leader(a)])
+
+    def groups(self):
+        result = [[] for _ in range(self.n)]
+        for i in range(self.n):
+            result[self.leader(i)].append(i)
+        return [r for r in result if r != []]
 
 
-def permutation(n, r):
-    return math.factorial(n) // math.factorial(n-r)
+N, M = map(int, input().split())
+G = [[] for _ in range(N)]
+un = UnionFind(N)
+for _ in range(M):
+    a, b = map(int, input().split())
+    G[a-1].append(b-1)
+    G[b-1].append(a-1)
+    if un.same(a-1, b-1):
+        # すでに同じ隣接成分になっているときさらに頂点を繋げるとループする
+        print("No")
+        exit()
+    un.merge(a-1, b-1)
 
-
-def combination(n, r):
-    return permutation(n, r) // math.factorial(r)
+ans = 'Yes'
+for i in range(N):
+    if len(G[i]) >= 3:
+        ans = 'No'
+        break
+print(ans)
