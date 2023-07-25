b89c3f35b15c972a86b4e03d5c0ab724c9ad68c6
bun913 <nagasima.romance+@gmail.com>
2023-07-26 06:33:43 +0900

    feat: abc229d

--- abc229/d/main.py
+++ abc229/d/main.py
@@ -1,45 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input()
-#int型で受け取るとき
-s = int(input())
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+文字列Sに対して.をXに置き換える操作をK回以下行うことができる
+Xを最大で何個連続させることができるか
+Sが2*10**5なのでbit全探索は無理
 """
-from functools import reduce
-from itertools import combinations
-import math
+from itertools import accumulate
 
-# 素数判定
+S = input()
+K = int(input())
+L = [0]
+for s in S:
+    if s == "X":
+        L.append(0)
+        continue
+    L.append(1)
 
+counter = list(accumulate(L))
 
-def is_prime(n: int) -> bool:
-    for i in range(2, int(math.sqrt(n))+1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def permutation(n, r):
-    return math.factorial(n) // math.factorial(n-r)
-
-
-def combination(n, r):
-    return permutation(n, r) // math.factorial(r)
+ans = 0
+r = 1
+for l in range(1, len(counter)):
+    while r < len(counter) and counter[r] - counter[l - 1] <= K:
+        r += 1
+    # 求めるものが区間の数なのでr-lで答えを求める
+    ans = max(ans, r-l)
+print(ans)
