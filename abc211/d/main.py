1b0db2f014b6865dbef14d3877f9036004dd996e
bun913 <nagasima.romance+@gmail.com>
2023-07-26 07:11:16 +0900

    feat: solve abc211d

--- abc211/d/main.py
+++ abc211/d/main.py
@@ -1,21 +1,38 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+from collections import deque
 
+N, M = map(int, input().split())
+G = [[] for _ in range(N+1)]
+for _ in range(M):
+    A, B = map(int, input().split())
+    G[A].append(B)
+    G[B].append(A)
+
+q = deque([1])
+inf = 10 ** 15
+time = [inf] * (N+1)
+time[1] = 0
+counter = [0] * (N+1)
+counter[1] = 1
+
+# 幅優先探索を行いながら最短経路と経路数をカウントする
+while q:
+    cur = q.popleft()
+    for nex in G[cur]:
+        if time[nex] == inf:
+            # もし行ける都市の移動時間が更新されていなければ更新
+            time[nex] = time[cur] + 1
+            # 経路数は今いる都市の経路数と同じ
+            counter[nex] = counter[cur]
+            q.append(nex)
+        else:
+            # 移動時間が更新済みなら
+            # 今いる年から+1の時間でいける年の場合は経路数をたす
+            if time[nex] == time[cur] + 1:
+                counter[nex] += counter[cur]
+                counter[nex] %= 10**9 + 7
+if time[N] == inf:
+    print(0)
+    exit()
+print(counter[N])
