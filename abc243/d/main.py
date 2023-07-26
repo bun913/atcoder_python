f133c1109fd4116632d6c60236c22f66f31b4c36
bun913 <nagasima.romance+@gmail.com>
2023-07-27 07:07:55 +0900

    feat: solve abc243d

--- abc243/d/main.py
+++ abc243/d/main.py
@@ -1,14 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+1件計算でなんとかできそうだがXが10**18と非常に大きい
+LUやRUは移動していないのと同じでるので入力から無駄な部分を消す
 """
-from functools import reduce
-from itertools import combinations
-import math
+
+N, X = map(int, input().split())
+S = input()
+memo = []
+
+for s in S:
+    if len(memo) == 0:
+        memo.append(s)
+        continue
+    if s == 'L' or s == 'R':
+        memo.append(s)
+        continue
+    # sがU確定
+    if memo[-1] == 'L' or memo[-1] == 'R':
+        memo.pop()
+        continue
+    if memo[-1] == 'U':
+        memo.append(s)
+        continue
+
+ans = X
+for s in memo:
+    if s == "L":
+        ans *= 2
+        continue
+    if s == "R":
+        ans *= 2
+        ans += 1
+        continue
+    ans //= 2
+print(ans)
