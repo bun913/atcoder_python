2ad841df95ff9fd00c7784ad1d1cb7ad61100263
bun913 <nagasima.romance+@gmail.com>
2023-07-28 06:40:29 +0900

    feat: abc248c

--- abc248/c/main.py
+++ abc248/c/main.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+長さNの数列であって
+Ai全てが1以上M以下かつAの総和がK以下である数列の個数をもとめる
 """
-from functools import reduce
-from itertools import combinations
-import math
+N, M, K = map(int, input().split())
+mod = 998244353
+dp = [[0] * (K+1) for _ in range(N+1)]
+# 1行目(長さ1の数列)の初期化(1以上M以下の数は1通り作れる)
+for a in range(1, M+1):
+    dp[1][a] = 1
+
+for i in range(2, N+1):
+    for x in range(1, K+1):
+        # j= 1 ~ (x-1)までの列挙
+        for j in range(1, x):
+            # M以下の数しか数列に使えないので、x-jがM以下の場合のみ足す
+            if x-j <= M:
+                dp[i][x] += dp[i-1][j]
+                dp[i][x] %= mod
+ans = 0
+
+for x in range(1, K+1):
+    ans += dp[N][x]
+    ans %= mod
+print(ans)
