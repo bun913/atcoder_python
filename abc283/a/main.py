cc48ebe767487c15cb8be8a134192b685cc8401e
bun913 <nagasima.romance+@gmail.com>
2023-08-01 08:24:00 +0900

    feat: add abc283ab

--- abc283/a/main.py
+++ abc283/a/main.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+Qが10**5でリスト操作はO(1)なので余裕
 """
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+
+A, B = list(map(int, input().split()))
+print(A**B)
--- abc283/b/main.py
+++ abc283/b/main.py
@@ -5,3 +5,14 @@
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+N = int(input())
+A = list(map(int, input().split()))
+ad = A[:]
+Q = int(input())
+
+for i in range(Q):
+    q = list(map(int, input().split()))
+    if q[0] == 1:
+        ad[q[1]-1] = q[2]
+        continue
+    print(ad[q[1]-1])
