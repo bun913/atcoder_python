cd86e3c91fb363c87043a88397667567fb1c91ae
bun913 <nagasima.romance+@gmail.com>
2023-08-14 08:00:23 +0900

    feat: abc314c

--- abc314/c/main.py
+++ abc314/c/main.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+Sの各文字について位置が変わるのは1回ずつ
+書く数字の出現位置を覚えておけばずらしていくだけで良さそう
 """
-from sys import setrecursionlimit
-
-setrecursionlimit(10**7)
+N, M = map(int, input().split())
+S = list(input())
+C = list(map(int, input().split()))
+# Sのi番目の位置を記録する配列
+indexes = [i for i in range(N)]
+# 1..Nまでの出現位置を記録する
+number_map = dict()
+for i, c in enumerate(C):
+    if c in number_map:
+        number_map[c].append(i)
+        continue
+    number_map[c] = [i]
+# indexをずらしていく
+for num, cur_index_list in number_map.items():
+    cnt = len(cur_index_list)
+    for i, ind in enumerate(cur_index_list):
+        indexes[ind] = cur_index_list[(i+1) % cnt]
+ans_list = S[:]
+for i, ind in enumerate(indexes):
+    ans_list[ind] = S[i]
+print(*ans_list, sep="")
