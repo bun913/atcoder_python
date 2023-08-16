524aa3146a3c577eb5d254643413490c4ff58522
bun913 <nagasima.romance+@gmail.com>
2023-08-17 06:52:13 +0900

    feat: solve abc288c

--- abc288/c/main.py
+++ abc288/c/main.py
@@ -1,7 +1,49 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+UnionFindですでに連結しているところを繋げないようにしてみる？
 """
-from sys import setrecursionlimit
 
-setrecursionlimit(10**7)
+
+class UnionFind:
+    def __init__(self, n):
+        self.n = n
+        self.parent_size = [-1]*n
+
+    def leader(self, a):
+        if self.parent_size[a] < 0:
+            return a
+        self.parent_size[a] = self.leader(self.parent_size[a])
+        return self.parent_size[a]
+
+    def merge(self, a, b):
+        x, y = self.leader(a), self.leader(b)
+        if x == y:
+            return
+        if abs(self.parent_size[x]) < abs(self.parent_size[y]):
+            x, y = y, x
+        self.parent_size[x] += self.parent_size[y]
+        self.parent_size[y] = x
+
+    def same(self, a, b):
+        return self.leader(a) == self.leader(b)
+
+    def size(self, a):
+        return abs(self.parent_size[self.leader(a)])
+
+    def groups(self):
+        result = [[] for _ in range(self.n)]
+        for i in range(self.n):
+            result[self.leader(i)].append(i)
+        return [r for r in result if r != []]
+
+
+N, M = map(int, input().split())
+un = UnionFind(N)
+ans = 0
+for _ in range(M):
+    a, b = map(int, input().split())
+    if un.same(a-1, b-1):
+        ans += 1
+        continue
+    un.merge(a-1, b-1)
+print(ans)
