6ba119dc58e0ddd70afcb3163c58dee9e0ad6b84
bun913 <nagasima.romance+@gmail.com>
2023-08-26 13:59:56 +0900

    feta: solve abc061c

--- abc061/c/main.py
+++ abc061/c/main.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+バケツソートの考え方を使う
+今回だとaがたかだか10**5であるのに注目
+aの値をインデックスとする配列を用意してあげて、それぞれの配列の要素数をカウントすれば良い
 """
-from sys import setrecursionlimit
+N, K = map(int, input().split())
+counts = [0 for _ in range(10**5+1)]
 
-setrecursionlimit(10**7)
+for _ in range(N):
+    a, b = map(int, input().split())
+    counts[a] += b
+
+cur_cnt = 0
+for num, cnt in enumerate(counts):
+    cur_cnt += cnt
+    if cur_cnt >= K:
+        print(num)
+        break
