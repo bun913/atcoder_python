68780b4036caf0a3c0395374fd7fe275b2739f9c
bun913 <nagasima.romance+@gmail.com>
2023-08-28 06:43:28 +0900

    feat: solve abc302c

--- abc302/c/main.py
+++ abc302/c/main.py
@@ -1,7 +1,29 @@
 # -*- coding: utf-8 -*-
 """
 解く前のメモ用
+問題を勘違いしていた
+Sの要素を並び替えるのではなく、Sの順番を並び替えるだけか
 """
-from sys import setrecursionlimit
+from itertools import permutations
 
-setrecursionlimit(10**7)
+N, M = map(int, input().split())
+S = [list(input()) for _ in range(N)]
+
+# 文字数が1の場合はYes
+if M == 1:
+    print("Yes")
+    exit()
+
+for T in permutations(S):
+    ok = 0
+    for i in range(N-1):
+        s1 = T[i]
+        s2 = T[i+1]
+        diff = sum(el1 != el2 for el1, el2 in zip(s1, s2))
+        if diff == 1:
+            ok += 1
+            continue
+    if ok == N-1:
+        print("Yes")
+        exit()
+print("No")
