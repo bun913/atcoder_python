6bbeb99a38c88274e1d7f801a3520125725b820d
bun913 <nagasima.romance+@gmail.com>
2023-08-29 06:59:23 +0900

    feat: 005c

--- abc005/c/main.py
+++ abc005/c/main.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+AもBもたかだか10**2なので、3重ループになっても大丈夫
 """
-from functools import reduce
-from itertools import combinations
-import math
+
+T = int(input())
+N = int(input())
+A = list(map(int, input().split()))
+M = int(input())
+B = list(map(int, input().split()))
+
+octs = sorted(A)
+customers = sorted(B)
+
+# そもそもたこ焼きが足らない
+if len(octs) < len(customers):
+    print("no")
+    exit()
+
+ok_custmor_list = [False for _ in range(len(customers))]
+for c_ind, c in enumerate(customers):
+    for o_ind in range(len(octs)):
+        oct = octs[o_ind]
+        if c >= oct and c - oct <= T:
+            ok_custmor_list[c_ind] = True
+            octs.pop(o_ind)
+            break
+if all(ok_custmor_list) is True:
+    print("yes")
+    exit()
+print("no")
