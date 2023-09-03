f410ff79b1c454e575cad03d96d14c6aa2d046bd
bun913 <nagasima.romance+@gmail.com>
2023-09-03 09:14:32 +0900

    feat: sumitrust2019ab

--- sumitrust2019/a/main.py
+++ sumitrust2019/a/main.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+2019年なのでうるう年は関係ない
+M1月D1日が月末か求める
+M2月D2日であることがわかっている
+どうもこうも、別にきにせずにD2が1か判定すれば良くね
 """
 from sys import setrecursionlimit
 
 setrecursionlimit(10**7)
+
+m1, d1 = list(map(int, input().split()))
+m2, d2 = list(map(int, input().split()))
+
+if d2 == 1:
+    print(1)
+    exit()
+print(0)
--- sumitrust2019/b/main.py
+++ sumitrust2019/b/main.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+税抜価格Xを忘れたので知りたい
+N円を支払っている
+Xが小数の場合は切り捨て
 """
 from sys import setrecursionlimit
+from math import ceil
 
 setrecursionlimit(10**7)
+N = int(input())
+cand1 = 100 * N // 108
+cand2 = cand1 + 1
+if cand1 * 108 // 100 == N:
+    print(cand1)
+    exit()
+if cand2 * 108 // 100 == N:
+    print(cand2)
+    exit()
+print(":(")
