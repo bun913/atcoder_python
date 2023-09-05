8462d4f995f68dd82ed2a38ef43b47f6ff5b4622
bun913 <nagasima.romance+@gmail.com>
2023-09-06 06:44:10 +0900

    feat: solve c

--- abc057/c/main.py
+++ abc057/c/main.py
@@ -2,6 +2,18 @@
 """
 解く前のメモ用
 """
-from sys import setrecursionlimit
+N = int(input())
 
-setrecursionlimit(10**7)
+
+def f(a, b):
+    return max(len(str(a)), len(str(b)))
+
+
+# 約数の列挙をlogNまでしていく
+limit = int(N**0.5) + 1
+ans = float("inf")
+for a in range(1, limit):
+    if N % a == 0:
+        b = N // a
+        ans = min(ans, f(a, b))
+print(ans)
