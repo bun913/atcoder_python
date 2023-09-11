7d80dc077d107575f1da668282acfa49c7491459
bun913 <nagasima.romance+@gmail.com>
2023-09-12 06:53:21 +0900

    feat: solve abc250c

--- abc250/c/main.py
+++ abc250/c/main.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
 """
-from functools import reduce
-from itertools import combinations
-import math
+N, Q = map(int, input().split())
+num_inds = dict([(i, i) for i in range(1, N+1)])
+ind_nums = dict([(i, i) for i in range(1, N+1)])
+
+for _ in range(Q):
+    left_num = int(input())
+    left_ind = num_inds[left_num]
+    # left_indがNの時だけright_indはleft_indの横の数字になる
+    right_ind = left_ind + 1 if left_ind < N else left_ind - 1
+    right_num = ind_nums[right_ind]
+    # 更新する
+    num_inds[left_num], num_inds[right_num] = right_ind, left_ind
+    ind_nums[left_ind], ind_nums[right_ind] = right_num, left_num
+
+# 答えを出力
+ans = []
+for ind, num in sorted(ind_nums.items()):
+    ans.append(str(num))
+print(*ans)
