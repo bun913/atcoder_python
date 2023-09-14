051ffbf9b1a02164e9913d33f30603876bb7b306
bun913 <nagasima.romance+@gmail.com>
2023-09-15 04:42:55 +0900

    feat: solve 249c

--- abc249/c/main.py
+++ abc249/c/main.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
 """
-from functools import reduce
-from itertools import combinations
-import math
+from itertools import product
+from collections import Counter
+
+N, K = map(int, input().split())
+S = [list(input()) for _ in range(N)]
+
+ans = 0
+for kumi in product([True, False], repeat=N):
+    c = Counter()
+    for i, is_use in enumerate(kumi):
+        if is_use is False:
+            continue
+        s = S[i]
+        to_set = set(s)
+        c.update(to_set)
+    k_cnts = [item for item, count in c.items() if count == K]
+    ans = max(ans, len(k_cnts))
+
+print(ans)
