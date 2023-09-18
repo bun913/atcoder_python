a48c3d44de870393cf031ca50a913fd3c6378d9f
bun913 <nagasima.romance+@gmail.com>
2023-09-18 09:28:54 +0900

    feat: solve pan2020

--- panasonic2020/c/main.py
+++ panasonic2020/c/main.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ用
+sqrt(a) + sqrt(b) < sqrt(c)かどうかを求める
+このままだと少数の計算となるため、整数の問題に帰結させる
 """
-from sys import setrecursionlimit
-
-setrecursionlimit(10**7)
+a, b, c = map(int, input().split())
+if 4 * a * b < (c - a - b)**2 and c - a - b > 0:
+    print("Yes")
+    exit()
+print("No")
