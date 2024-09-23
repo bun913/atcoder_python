ccc8838bfab3a7c864acc72330f111e4c7afe1fc
bun913 <nagasima.romance+@gmail.com>
2024-09-24 06:00:42 +0900

    feat: 026d

--- abc026/d/main.py
+++ abc026/d/main.py
@@ -3,5 +3,27 @@
 解く前のメモ用
 """
 from sys import setrecursionlimit
+from math import sin, pi
 
-setrecursionlimit(10**7)
+setrecursionlimit(10**8)
+
+
+def solve():
+    act()
+
+
+def act():
+    A, B, C = map(int, input().split())
+    ng = 0
+    ok = 200
+    while ok - ng > 10 ** -12:
+        mid = (ok + ng) / 2
+        x = A * mid + B * sin(C * mid * pi)
+        if x >= 100:
+            ok = mid
+        else:
+            ng = mid
+    print(ok)
+
+
+solve()
