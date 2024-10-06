46026317bd8d041cc47114a61ac17190acc2c3de
bun913 <nagasima.romance+@gmail.com>
2024-10-07 06:31:22 +0900

    feat: 106b

--- abc106/b/main.py
+++ abc106/b/main.py
@@ -1,20 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-解く前のメモ
-
-Nが最大で200しかないので全部数え上げでも間に合う
+解く前のメモ用
 """
-from math import sqrt
+from sys import setrecursionlimit
 
-N = int(input())
-ans = 0
+setrecursionlimit(10**8)
 
-for i in range(1, N+1):
-    div_set = set()
-    for j in range(1, int(sqrt(i) + 1)):
-        if i % j == 0:
-            div_set.add(j)
-            div_set.add(i // j)
-    if len(div_set) == 8 and i % 2 == 1:
-        ans += 1
-print(ans)
+
+def solve():
+    N = arrange()
+    act(N)
+
+
+def arrange():
+    return int(input())
+
+
+def act(N):
+    ans = 0
+    for i in range(1, N+1):
+        if i % 2 == 0:
+            continue
+        div = set()
+        for a in range(1, int(i**0.5)+1):
+            if i % a == 0:
+                div.add(i // a)
+                div.add(a)
+        if len(div) == 8:
+            ans += 1
+    print(ans)
+
+
+solve()
