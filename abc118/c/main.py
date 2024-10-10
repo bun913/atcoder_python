750dd3bb1c686f2595622ad907eb4d7740a1f4c9
bun913 <nagasima.romance+@gmail.com>
2024-10-11 06:13:06 +0900

    feat: 118c

--- abc118/c/main.py
+++ abc118/c/main.py
@@ -1,14 +1,48 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+解く前のメモ用
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from sys import setrecursionlimit
+
+setrecursionlimit(10**8)
+
+
+def solve():
+    N, A = arrange()
+    act(N, A)
+
+
+def arrange():
+    N = int(input())
+    A = list(map(int, input().split()))
+    return N, A
+
+
+def act(N, A):
+    m = min(A)
+    S = sum(A)
+    skip_ind = A.index(m)
+    while True:
+        if S == m:
+            break
+        next_min = m
+        for i in range(N):
+            # 一旦初期化
+            S = 0
+            a = A[i]
+            # 自分は無視
+            if i == skip_ind:
+                continue
+            A[i] %= m
+            S += A[i]
+            if a % m == 0:
+                continue
+            if a % m < next_min:
+                next_min = a % m
+        S += m
+        m = next_min
+        skip_ind = A.index(m)
+    print(sum(A))
+
+
+solve()
