1a2831b2b79932bb3b6c1329c1edd441b416df7a
bun913 <nagasima.romance+@gmail.com>
2024-10-18 07:27:16 +0900

    feat: 133c

--- abc133/c/main.py
+++ abc133/c/main.py
@@ -1,14 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+解く前のメモ用
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from sys import setrecursionlimit
+
+setrecursionlimit(10**8)
+
+
+def solve():
+    L, R = arrange()
+    act(L, R)
+
+
+def arrange():
+    return map(int, input().split())
+
+
+def act(L, R):
+    mod = 2019
+    # 2019で割った章が繰り上がるなら絶対どこかで0が出るので0を出力
+    ls = L // mod
+    rs = R // mod
+    if rs - ls >= 1:
+        print(0)
+        return
+    # 商が同じってことは素直に計算すればいい
+    ans = 2019
+    for i in range(L, R):
+        for j in range(i+1, R+1):
+            ans = min(ans, (i*j) % mod)
+    print(ans)
+
+
+solve()
