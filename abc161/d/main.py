f190a7a9b1839676b71f295e818cea2a3ddae4e8
bun913 <nagasima.romance+@gmail.com>
2024-10-19 20:40:37 +0900

    feat: 161d

--- abc161/d/main.py
+++ abc161/d/main.py
@@ -1,14 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+解く前のメモ用
 """
-from functools import reduce
-from itertools import combinations
-import math
+from sys import setrecursionlimit
+from collections import deque
+
+setrecursionlimit(10**8)
+
+
+def solve():
+    act()
+
+
+def act():
+    K = int(input())
+    q = deque([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    i = 1
+    while q:
+        if i == K:
+            print(q.popleft())
+            return
+        cur = q.popleft()
+        # 自分自身の一番右の桁を-1して次の数を作る(絶対値-1)
+        if cur % 10 != 0:
+            q.append(cur * 10 + cur % 10 - 1)
+        # 自分自身の一番右の桁を足して次の数を作る
+        q.append(cur * 10 + cur % 10)
+        # 自分自身の一番右の桁を+1して次の数を作る(絶対値+1)
+        if cur % 10 != 9:
+            q.append(cur * 10 + cur % 10 + 1)
+        i += 1
+
+
+solve()
