c0b954810e3124366af4c330da6c59cc526fef1f
bun913 <nagasima.romance+@gmail.com>
2024-10-23 06:51:35 +0900

    feat: 169c

--- abc169/c/main.py
+++ abc169/c/main.py
@@ -4,4 +4,25 @@
 """
 from sys import setrecursionlimit
 
-setrecursionlimit(10**7)
+setrecursionlimit(10**8)
+
+
+def solve():
+    A, B = arrange()
+    act(A, B)
+
+
+def arrange():
+    A, B = input().split()
+    return A, B
+
+
+def act(A, B):
+    a = int(A)
+    tmp = B.replace(".", "")
+    b = int(tmp)
+    ans = a * b // 100
+    print(ans)
+
+
+solve()
