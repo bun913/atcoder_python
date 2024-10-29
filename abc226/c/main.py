b029b81a41f8f73330c2412934cfb3f3cfb95619
bun913 <nagasima.romance+@gmail.com>
2024-10-30 06:42:13 +0900

    feat: 226c

--- abc226/c/main.py
+++ abc226/c/main.py
@@ -1,45 +1,48 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input()
-#int型で受け取るとき
-s = int(input())
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
+解く前のメモ用
 """
-from functools import reduce
-from itertools import combinations
-import math
+from sys import setrecursionlimit
+from collections import deque
 
-# 素数判定
-
-
-def is_prime(n: int) -> bool:
-    for i in range(2, int(math.sqrt(n))+1):
-        if n % i == 0:
-            return False
-    return True
+setrecursionlimit(10**8)
 
 
-def permutation(n, r):
-    return math.factorial(n) // math.factorial(n-r)
+def solve():
+    N, T, G = arrange()
+    act(N, T, G)
 
 
-def combination(n, r):
-    return permutation(n, r) // math.factorial(r)
+def arrange():
+    N = int(input())
+    T = []
+    G = [[] for _ in range(N)]
+    for i in range(N):
+        L = list(map(int, input().split()))
+        t = L[0]
+        T.append(t)
+        k = L[1]
+        if k == 0:
+            continue
+        for a in L[2:]:
+            G[i].append(a-1)
+    return N, T, G
+
+
+def act(N, T, G):
+    q = deque([N-1])
+    ans = T[N-1]
+    visited = [False] * N
+    visited[N-1] = True
+    while q:
+        cur = q.popleft()
+        for nex in G[cur]:
+            if visited[nex] is True:
+                continue
+            visited[nex] = True
+            ans += T[nex]
+            q.append(nex)
+    print(ans)
+
+
+solve()
