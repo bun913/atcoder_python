e59ad31d9b90644888e293cf9c5951a267b461e7
bun913 <nagasima.romance+@gmail.com>
2026-03-27 08:46:35 +0900

    feat: add b

---
+++ past201912-open/c/main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+解く前のメモ用
+"""
+
+list_S = map(int,input().split())
+sorted_list_S = sorted(list_S, reverse=True)
+ans = sorted_list_S[2]
+print(ans)
+
---
+++ past201912-open/h/main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+解く前のメモ用
+"""
+
+from sys import setrecursionlimit
+
+setrecursionlimit(10**8)
+
+
+def solve():
+    act()
+
+
+def act():
+    pass
+
+
+solve()
