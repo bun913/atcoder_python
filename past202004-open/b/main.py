5df027de42674ca1e456490bd591e714df4e8568
bun913 <nagasima.romance+@gmail.com>
2026-03-26 21:53:27 +0900

    feat: add past b

--- past202004-open/b/main.py
+++ past202004-open/b/main.py
@@ -3,5 +3,20 @@
 解く前のメモ用
 """
 from sys import setrecursionlimit
+from collections import defaultdict
 
 setrecursionlimit(10**7)
+
+
+S = list(input())
+memo = defaultdict(int)
+
+# create memo
+for s in S:
+    memo[s] += 1
+
+# create answer
+l_t_memo = sorted(memo.items(), key=lambda item:item[1])
+ans = l_t_memo[-1][0]
+print(ans)
+
